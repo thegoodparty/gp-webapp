4f284d30ee4a6bf5a9930ef53763d4945eb8a284
tomer-tgp <tomer@thegoodparty.org>
2022-11-13 11:20:06 -0800

    chore: placeholder for candidates.

---
+++ app/candidates.js/head.js
@@ -0,0 +1,11 @@
+import GpHead from '../shared/layouts/GpHead';
+
+export default function Head({ params }) {
+  return (
+    <GpHead
+      title="Candidates | GOOD PARTY"
+      description="Good Certified Candidates on GOOD PARTY are all Non-Partisan, Small Money and Anti-Corruption."
+      slug="candidates"
+    />
+  );
+}
--- app/head.js
+++ app/head.js
@@ -5,7 +5,7 @@ export default function Head({ params }) {
     <GpHead
       title="GOOD PARTY | Free tools to change the rules and disrupt the corrupt."
       description="Not a political party, we’re building tools to change the rules, empowering creatives to mobilize community & disrupt the corrupt two-party system. Join us!"
-      slug={params.slug}
+      slug="/"
     />
   );
 }
