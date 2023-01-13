04eeca9d6d54c4534b1773f325626d5b144515ba
tomer-tgp <tomer@thegoodparty.org>
2023-01-13 09:26:00 -0800

    fix: script issue

--- app/(company)/run-for-office/head.js
+++ app/(company)/run-for-office/head.js
@@ -1,4 +1,5 @@
 import GpHead from '@shared/layouts/GpHead';
+import Script from 'next/script';
 
 export default function Head({ params }) {
   return (
@@ -8,7 +9,7 @@ export default function Head({ params }) {
         description="We've made it simple and free like democracy should be."
         slug="/run"
       />
-      <script src="https://www.googleoptimize.com/optimize.js?id=OPT-WLTK9ST"></script>
+      <Script src="https://www.googleoptimize.com/optimize.js?id=OPT-WLTK9ST"></Script>
     </>
   );
 }
