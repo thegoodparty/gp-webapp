65b2933a9410e78babfb08941d89579bc52e2d27
tomer-tgp <tomer@thegoodparty.org>
2023-01-12 21:54:01 -0800

    chore: optimize

--- app/(company)/run-for-office/head.js
+++ app/(company)/run-for-office/head.js
@@ -2,10 +2,13 @@ import GpHead from '@shared/layouts/GpHead';
 
 export default function Head({ params }) {
   return (
-    <GpHead
-      title="Run as an Indie or 3rd Party. | Good Party"
-      description="We've made it simple and free like democracy should be."
-      slug="/run"
-    />
+    <>
+      <GpHead
+        title="Run as an Indie or 3rd Party. | Good Party"
+        description="We've made it simple and free like democracy should be."
+        slug="/run"
+      />
+      <script src="https://www.googleoptimize.com/optimize.js?id=OPT-WLTK9ST"></script>
+    </>
   );
 }
