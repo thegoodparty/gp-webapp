151d92dc3c705599da4ca56a14e42c9a1a399841
tomer-tgp <tomer@thegoodparty.org>
2023-01-16 11:06:34 -0800

    fix: blog ssg

--- app/candidate/[slug]/page.js
+++ app/candidate/[slug]/page.js
@@ -50,12 +50,12 @@ export default async function Page({ params }) {
   );
 }
 
-export async function generateStaticParams() {
-  const { candidates } = await fetchCandidates();
+// export async function generateStaticParams() {
+//   const { candidates } = await fetchCandidates();
 
-  return candidates.map((candidate) => {
-    return {
-      slug: candidate.slug,
-    };
-  });
-}
+//   return candidates.map((candidate) => {
+//     return {
+//       slug: candidate.slug,
+//     };
+//   });
+// }
