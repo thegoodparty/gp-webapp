70541b6cc2d373e92b650d218ff9cab5923796b7
tomer-tgp <tomer@thegoodparty.org>
2023-02-09 17:07:51 -0800

    feat: add grid to autocomplete

--- app/political-terms/components/TermsHomePage.js
+++ app/political-terms/components/TermsHomePage.js
@@ -15,18 +15,28 @@ export default function TermsHomePage(props) {
   return (
     <MaxWidth>
       <div className="my-9 lg:my-16">
-        <h1 className="font-black text-4xl lg:text-5xl mb-4">Terms Glossary</h1>
-        <div className="text-lg">
-          Good Party's Terms Glossary is a list of definitions of words from the
-          political and elections world. These terms are from an independent's
-          perspective with an eye toward reform. If you have a suggestion for a
-          new definition, send it to{' '}
-          <a href="mailto:ask@goodparty.org" rel="noopener noreferrer nofollow">
-            ask@goodparty.org.
-          </a>
+        <div className="grid grid-cols-12 gap-3">
+          <div className="col-span-12 lg:col-span-9">
+            <h1 className="font-black text-4xl lg:text-5xl mb-4">
+              Terms Glossary
+            </h1>
+            <div className="text-lg">
+              Good Party's Terms Glossary is a list of definitions of words from
+              the political and elections world. These terms are from an
+              independent's perspective with an eye toward reform. If you have a
+              suggestion for a new definition, send it to{' '}
+              <a
+                href="mailto:ask@goodparty.org"
+                rel="noopener noreferrer nofollow"
+              >
+                ask@goodparty.org.
+              </a>
+            </div>
+          </div>
+          <div className="col-span-12 lg:col-span-3">
+            <TermsSearch glossaryItems={glossaryItems} />
+          </div>
         </div>
-
-        <TermsSearch glossaryItems={glossaryItems} />
         {/* {desktopMode ? <TermsSearch glossaryItems={glossaryItems} /> : <></>} */}
 
         {recentGlossaryItems && recentGlossaryItems.length > 0 ? (
--- app/political-terms/components/TermsSearch.js
+++ app/political-terms/components/TermsSearch.js
@@ -12,52 +12,48 @@ export default function TermsSearch(props) {
   return (
     <>
       {glossaryItems && glossaryItems.length > 0 ? (
-        <div className="grid grid-cols-3 gap-3 lg:grid-cols-4 items-center mt-4">
-          <div className="col-span-2 lg:col-span-1">
-            <Autocomplete
-              sx={{ '& fieldset': { borderRadius: 33 } }}
-              options={glossaryItems}
-              getOptionLabel={(option) => (option ? option : '')}
-              fullWidth
-              // value={value}
-              variant="outlined"
-              renderInput={(renderInputParams) => (
-                <div
-                  ref={renderInputParams.InputProps.ref}
-                  style={{
-                    alignItems: 'center',
-                    width: '100%',
-                    display: 'flex',
-                    flexDirection: 'row',
-                  }}
-                >
-                  <TextField
-                    style={{ flex: 1 }}
-                    //   {...params}
-                    InputProps={{
-                      ...renderInputParams.InputProps,
-                      startAdornment: (
-                        <InputAdornment position="start">
-                          <FaSearch />
-                        </InputAdornment>
-                      ),
-                    }}
-                    placeholder="Search"
-                    inputProps={{
-                      ...renderInputParams.inputProps,
-                    }}
-                    InputLabelProps={{ style: { display: 'none' } }}
-                  />
-                </div>
-              )}
-              onChange={(event, item) => {
-                if (item != undefined && item != '') {
-                  router.push(termLinkByTitle(item));
-                }
+        <Autocomplete
+          sx={{ '& fieldset': { borderRadius: 33 } }}
+          options={glossaryItems}
+          getOptionLabel={(option) => (option ? option : '')}
+          fullWidth
+          // value={value}
+          variant="outlined"
+          renderInput={(renderInputParams) => (
+            <div
+              ref={renderInputParams.InputProps.ref}
+              style={{
+                alignItems: 'center',
+                width: '100%',
+                display: 'flex',
+                flexDirection: 'row',
               }}
-            />
-          </div>
-        </div>
+            >
+              <TextField
+                style={{ flex: 1 }}
+                //   {...params}
+                InputProps={{
+                  ...renderInputParams.InputProps,
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <FaSearch />
+                    </InputAdornment>
+                  ),
+                }}
+                placeholder="Search"
+                inputProps={{
+                  ...renderInputParams.inputProps,
+                }}
+                InputLabelProps={{ style: { display: 'none' } }}
+              />
+            </div>
+          )}
+          onChange={(event, item) => {
+            if (item != undefined && item != '') {
+              router.push(termLinkByTitle(item));
+            }
+          }}
+        />
       ) : (
         <></>
       )}
