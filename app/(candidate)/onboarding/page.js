3b24230acb4e55cfbbc663b708a6ab0ff2e48747
tomer-tgp <tomer@thegoodparty.org>
2023-02-23 12:41:54 -0800

    feat: create campaign page

--- app/(candidate)/onboarding/page.js
+++ app/(candidate)/onboarding/page.js
@@ -1,16 +1,13 @@
 import gpApi from 'gpApi';
 import gpFetch from 'gpApi/gpFetch';
 import { getServerToken } from 'helpers/userServerHelper';
-import OnboardingPage from './components/OnboardingPage';
+import OnboardingStartPage from './components/OnboardingStartPage';
 
 export default async function Page() {
   const childProps = {
-    title:
-      "Hi! I'm Jared. I just need a little bit of information so we can help you get started...",
-    description:
-      'Good Party  will be with you every step of the way so you can run a successful campaign.',
+    title: "Hi! I'm Jared. Let's get your campaign started...",
     self: '/onboarding',
     slug: '',
   };
-  return <OnboardingPage {...childProps} />;
+  return <OnboardingStartPage {...childProps} />;
 }
