80921f4ba3fee8ffcaede8d4b4161d07bf91cadb
tomer-tgp <tomer@thegoodparty.org>
2023-03-02 08:57:05 -0800

    chore: skip goals

--- app/(candidate)/onboarding/[slug]/goals/[step]/page.js
+++ app/(candidate)/onboarding/[slug]/goals/[step]/page.js
@@ -1,47 +1,48 @@
 export const dynamic = 'force-dynamic';
 
-import gpApi from 'gpApi';
-import gpFetch from 'gpApi/gpFetch';
-import { fetchContentByKey } from 'helpers/fetchHelper';
-import { getServerToken } from 'helpers/userServerHelper';
-import { redirect } from 'next/navigation';
-import DetailsStepPage from './components/DetailsStepPage';
-import goalsFields from './goalsFields';
+// import gpApi from 'gpApi';
+// import gpFetch from 'gpApi/gpFetch';
+// import { fetchContentByKey } from 'helpers/fetchHelper';
+// import { getServerToken } from 'helpers/userServerHelper';
+// import { redirect } from 'next/navigation';
+// import DetailsStepPage from './components/DetailsStepPage';
+// import goalsFields from './goalsFields';
 
 export default async function Page({ params }) {
-  const { slug, step } = params;
+  // const { slug, step } = params;
 
-  let stepInt = step ? parseInt(step, 10) : 1;
+  // let stepInt = step ? parseInt(step, 10) : 1;
 
-  let { campaign } = await fetchUserCampaignServer();
-  if (campaign?.slug !== slug) {
-    redirect('/onboarding');
-  }
+  // let { campaign } = await fetchUserCampaignServer();
+  // if (campaign?.slug !== slug) {
+  //   redirect('/onboarding');
+  // }
 
-  const stepFields = goalsFields[stepInt - 1];
-  const { pageType } = stepFields;
-  let positions = [];
-  if (pageType === 'issuesPage') {
-    ({ positions } = await fetchPositions());
-  }
+  // const stepFields = goalsFields[stepInt - 1];
+  // const { pageType } = stepFields;
+  // let positions = [];
+  // if (pageType === 'issuesPage') {
+  //   ({ positions } = await fetchPositions());
+  // }
 
-  let pledge;
-  if (pageType === 'pledgePage') {
-    const res = await fetchContentByKey('pledge');
-    pledge = res.content;
-  }
+  // let pledge;
+  // if (pageType === 'pledgePage') {
+  //   const res = await fetchContentByKey('pledge');
+  //   pledge = res.content;
+  // }
 
-  const childProps = {
-    title: stepFields.title,
-    subTitle: stepFields.subTitle,
-    slug,
-    campaign,
-    fields: stepFields.fields,
-    step: stepInt,
-    pathname: `/details/${stepInt}`,
-    pageType,
-    pledge,
-    positions,
-  };
-  return <DetailsStepPage {...childProps} />;
+  // const childProps = {
+  //   title: stepFields.title,
+  //   subTitle: stepFields.subTitle,
+  //   slug,
+  //   campaign,
+  //   fields: stepFields.fields,
+  //   step: stepInt,
+  //   pathname: `/details/${stepInt}`,
+  //   pageType,
+  //   pledge,
+  //   positions,
+  // };
+  // return <DetailsStepPage {...childProps} />;
+  return <div>WIP</div>;
 }
