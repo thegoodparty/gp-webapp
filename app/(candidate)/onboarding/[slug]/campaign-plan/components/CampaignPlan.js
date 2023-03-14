130572a70badafacc86d38b9a9da66b0bfc30989
tomer-tgp <tomer@thegoodparty.org>
2023-03-13 18:27:07 -0700

    feat: breadcrumbs

--- app/(candidate)/onboarding/[slug]/campaign-plan/components/CampaignPlan.js
+++ app/(candidate)/onboarding/[slug]/campaign-plan/components/CampaignPlan.js
@@ -1,11 +1,20 @@
 import MaxWidth from '@shared/layouts/MaxWidth';
+import Breadcrumbs from '@shared/utils/Breadcrumbs';
 import CampaignAccordion from './CampaignAccordion';
 import Hero from './Hero';
 
 export default function CampaignManager(props) {
+  const { slug } = props.campaign;
+  const breadcrumbsLinks = [
+    { href: `/onboarding/${slug}/dashboard`, label: 'Dashboard' },
+    {
+      label: 'Your Campaign Plan',
+    },
+  ];
   return (
-    <div className="bg-slate-100 py-8">
+    <div className="bg-slate-100 py-2">
       <MaxWidth>
+        <Breadcrumbs links={breadcrumbsLinks} />
         <Hero />
         <CampaignAccordion {...props} />
       </MaxWidth>
