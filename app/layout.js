70c33fb5fb94c362d6d333384d57603d712e4f3c
tomer-tgp <tomer@thegoodparty.org>
2023-04-03 21:44:54 -0700

    feat: WEB-510

--- app/layout.js
+++ app/layout.js
@@ -56,7 +56,7 @@ export default function RootLayout({ children }) {
       <Script
         strategy="afterInteractive"
         type="text/javascript"
-        id="fb"
+        id="gtm"
         dangerouslySetInnerHTML={{
           __html: `
         // GTM
@@ -67,6 +67,23 @@ export default function RootLayout({ children }) {
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
   })(window,document,'script','dataLayer','GTM-M53W2ZV');
          }
+        `,
+        }}
+      />
+      <Script
+        strategy="afterInteractive"
+        type="text/javascript"
+        id="usersnap"
+        dangerouslySetInnerHTML={{
+          __html: `
+        // usersnap.com
+        window.onUsersnapLoad = function(api) {
+          api.init();
+        }
+        var script = document.createElement('script');
+        script.defer = 1;
+        script.src = 'https://widget.usersnap.com/global/load/ffda1fce-d2f7-4471-b118-050ae883436b?onload=onUsersnapLoad';
+        document.getElementsByTagName('head')[0].appendChild(script);
         `,
         }}
       />
