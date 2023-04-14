f1be9a5b006dc9f2fba59aaea13cfbf3b54e1fb7
tomer-tgp <tomer@thegoodparty.org>
2023-04-14 13:40:33 -0400

    chore: deploy

--- app/shared/candidates/CandidateProgressBar.js
+++ app/shared/candidates/CandidateProgressBar.js
@@ -1,4 +1,4 @@
-'use client';
+e'use client';
 import React, { useState, useEffect } from 'react';
 import { IoIosCheckmark } from 'react-icons/io';
 import { useHookstate } from '@hookstate/core';
@@ -24,7 +24,7 @@ const CandidateProgressBar = ({
     didWin,
     votesReceived,
   } = candidate;
-  const followOffset = 0; //useHookstate(followOffsetState);
+  const followOffset = 0; // useHookstate(followOffsetState);
   const offsetFollow = 0;
 
   let people = overrideFollowers ? likelyVoters : peopleSoFar;
