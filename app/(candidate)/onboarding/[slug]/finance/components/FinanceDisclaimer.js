3cde188935e861add544f5311c30732368f52371
tomer-tgp <tomer@thegoodparty.org>
2023-05-03 22:21:25 -0700

    fix: updated design for disclaimer

--- app/(candidate)/onboarding/[slug]/finance/components/FinanceDisclaimer.js
+++ app/(candidate)/onboarding/[slug]/finance/components/FinanceDisclaimer.js
@@ -4,8 +4,8 @@ import { FaExclamation } from 'react-icons/fa';
 export default function FinanceDisclaimer() {
   return (
     <div className="mb-10 lg:mb-20 px-5 lg:px-2 text-center mx-auto">
-      <div className="flex max-w-2xl mx-auto justify-center">
-        <div className="text-4xl mr-8 text-red-700 pt-1">
+      <div className="flex max-w-2xl mx-auto justify-center items-center lg:items-start">
+        <div className="text-4xl mr-4 lg:mr-8 text-red-700 lg:pt-1">
           <FaExclamation />
         </div>
         <div className="text-left">
@@ -14,8 +14,8 @@ export default function FinanceDisclaimer() {
           services that charge fees for obtaining an EIN on your behalf.
         </div>
       </div>
-      <div className="flex max-w-2xl mx-auto bg-gp-yellow mt-14 py-6 px-8 rounded-xl">
-        <div className="text-4xl mr-8 text-red-700 pt-1">
+      <div className="flex max-w-2xl  mx-auto bg-gp-yellow mt-14 py-6 px-8 rounded-xl">
+        <div className="hidden lg:block text-4xl mr-8 text-red-700 pt-1">
           <Image
             src="/images/icons/party.svg"
             alt="party"
@@ -23,7 +23,7 @@ export default function FinanceDisclaimer() {
             height={62}
           />
         </div>
-        <div className="text-left">
+        <div className="text-left font-semibold">
           By completing this process, you&apos;ll be taking an essential step
           toward managing your campaign&apos;s finances and ensuring compliance
           with federal regulations.
