5e1dafa46bb9eae01fb16dec6213e205681f6cb7
Taylor Murray <taylor@goodparty.org>
2023-05-09 17:13:25 -0700

    fix: blog cta bg color

--- app/blog/components/SubscribeBlog.js
+++ app/blog/components/SubscribeBlog.js
@@ -4,7 +4,7 @@ import styles from '../shared/ArticleSnippet.module.scss';
 
 export default function SubscribeBlog() {
   return (
-    <article className={`${styles.wrapper} bg-teal-300`}>
+    <article className={`${styles.ctaWrapper} bg-teal-300`}>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
         <div className={'lg:col-span-3'}>
           <div className="font-black text-xl mt-8 mb-3 text-center">
