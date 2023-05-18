b356800886a1f13ef809bca5fce2819a3734716b
tomer-tgp <tomer@thegoodparty.org>
2023-05-18 09:30:06 -0700

    feat: profile link

--- app/(candidate)/onboarding/[slug]/dashboard/components/Banner.js
+++ app/(candidate)/onboarding/[slug]/dashboard/components/Banner.js
@@ -1,12 +1,16 @@
 import H1 from '@shared/typography/H1';
+import Link from 'next/link';
 
 export default function Banner({ campaign }) {
-  const { launchStatus } = campaign;
+  const { launchStatus, candidateSlug, slug } = campaign;
   return (
     <div className="pb-8">
       {launchStatus === 'launched' ? (
         <div className="p-2 text-lg text-center bg-green-600 text-white">
-          Your campaign is launched
+          Your campaign is launched.{' '}
+          <Link href={`/candidate/${candidateSlug || slug}`}>
+            Your Campaign
+          </Link>
         </div>
       ) : null}
       {launchStatus === 'pending' ? (
