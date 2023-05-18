2e61a2bb33ac083bfac9aebaf7c0b07609b5b332
tomer-tgp <tomer@thegoodparty.org>
2023-05-17 21:43:59 -0700

    feat: revalidatePath

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,10 +1,12 @@
+import { revalidatePath } from 'next/cache';
+
 export default async function revalidate(req, res) {
   const { path } = req.query;
 
   try {
     // This should be the actual path not a rewritten path
     // e.g. for "/blog/[slug]" this should be "/blog/post-1"
-    await res.revalidate(path);
+    revalidatePath(path);
     return res.json({ revalidated: true });
   } catch (err) {
     // If there was an error, Next.js will continue
