13d2ca2dc8d11419a56406e52782df0440a0410a
tomer-tgp <tomer@thegoodparty.org>
2023-05-19 23:38:44 -0700

    feat: disable mobile scroll

--- app/shared/layouts/navigation/TopDashboardMenu.js
+++ app/shared/layouts/navigation/TopDashboardMenu.js
@@ -1,7 +1,28 @@
+'use client';
 import DashboardMenu from 'app/(candidate)/dashboard/shared/DashboardMenu';
 import Hamburger from 'hamburger-react';
+import { useEffect } from 'react';
+
+function disableScroll() {
+  window.scrollTo(0, 0);
+  // if any scroll is attempted, set it to 0
+  window.onscroll = function () {
+    window.scrollTo(0, 0);
+  };
+}
+
+function enableScroll() {
+  window.onscroll = function () {};
+}
 
 export default function TopDashboardMenu({ open, toggleCallback, pathname }) {
+  useEffect(() => {
+    if (open) {
+      disableScroll();
+    } else {
+      enableScroll();
+    }
+  }, [open]);
   return (
     <div className="lg:hidden">
       <Hamburger toggled={open} toggle={toggleCallback} size={20} />
