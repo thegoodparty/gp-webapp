037c82f141ef98278f7c5eff325d4b68c0b4237e
tomer-tgp <tomer@thegoodparty.org>
2023-05-20 16:16:41 -0700

    feat: full width header

--- app/shared/layouts/navigation/Nav.js
+++ app/shared/layouts/navigation/Nav.js
@@ -1,33 +1,30 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
-import MaxWidth from '../MaxWidth';
 import RightSide from './RightSide';
 
 export default function Nav() {
   return (
     <div className="relative bg-slate-50 lg:block border-solid border-b border-zinc-200 lg:px-6 z-50 h-14">
-      <MaxWidth>
-        <div className="flex justify-between items-center h-14">
-          <div className="flex items-center">
-            <Link href="/" id="nav-logo">
-              <Image
-                src="/images/logo-hologram.svg"
-                data-cy="logo"
-                width={145}
-                height={32}
-                alt="GOOD PARTY"
-              />
-            </Link>
-            <div className="pl-4 text-sm hidden lg:block">
-              Helping real people run and win!
-            </div>
-          </div>
-          <div className="flex justify-end items-center">
-            <RightSide />
+      <div className="flex justify-between items-center h-14">
+        <div className="flex items-center">
+          <Link href="/" id="nav-logo">
+            <Image
+              src="/images/logo-hologram.svg"
+              data-cy="logo"
+              width={145}
+              height={32}
+              alt="GOOD PARTY"
+            />
+          </Link>
+          <div className="pl-4 text-sm hidden lg:block">
+            Helping real people run and win!
           </div>
         </div>
-      </MaxWidth>
+        <div className="flex justify-end items-center">
+          <RightSide />
+        </div>
+      </div>
     </div>
   );
 }
