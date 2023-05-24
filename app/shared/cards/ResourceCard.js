eafd950b0672dda2a90f96de810d517de6b01949
tomer-tgp <tomer@thegoodparty.org>
2023-05-23 21:10:06 -0700

    feat: resource card

---
+++ app/shared/cards/ResourceCard.js
@@ -0,0 +1,14 @@
+import Body1 from '@shared/typography/Body1';
+import Body2 from '@shared/typography/Body2';
+import Link from 'next/link';
+
+export default function ResourceCard({ title, description, link }) {
+  return (
+    <Link href={link}>
+      <div className="p-4 bg-primary rounded-xl transition-colors hover:bg-indigo-600">
+        <Body1 className="text-slate-50 h-12 line-clamp-2">{title}</Body1>
+        <Body2 className="text-indigo-100 mt-1">{description}</Body2>
+      </div>
+    </Link>
+  );
+}
