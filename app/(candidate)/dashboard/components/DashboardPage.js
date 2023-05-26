ecca3bdeabee6baf057d760a8e4365b8da06fb2d
tomer-tgp <tomer@thegoodparty.org>
2023-05-26 06:38:37 +0300

    fix: double title

--- app/(candidate)/dashboard/components/DashboardPage.js
+++ app/(candidate)/dashboard/components/DashboardPage.js
@@ -5,7 +5,6 @@ import TitleSection from '../shared/TitleSection';
 export default function DashboardPage(props) {
   return (
     <DashboardLayout {...props}>
-      <H1>Campaign Tracker</H1>
       <TitleSection
         title="Campaign Tracker - Coming Soon"
         subtitle="Good Party GPT can help you create high quality content for your campaign"
