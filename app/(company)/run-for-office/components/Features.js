11ea340542538252735fb4c1133d2d41ecced3b7
Taylor Murray <taylor@goodparty.org>
2023-06-05 11:09:15 -0700

    fix: change flexbox to grid.

--- app/(company)/run-for-office/components/Features.js
+++ app/(company)/run-for-office/components/Features.js
@@ -12,18 +12,27 @@ const features = [
     title: 'Get help from experts',
     description:
       'We cut out expensive consultants by providing you free access to our team of experienced political professionals in fundraising, digital, field operations, and more.',
+    icon: <RiTeamLine />,
+    bgfrom: 'from-[#4F15F4]',
+    bgto: 'to-[#13A9EA]',
   },
   {
     name: 'Tools',
     title: 'Get access to cutting-edge AI tools',
     description:
       'Our AI-powered tools make campaigning smarter, faster and cheaper than ever. This will give you a competitive edge over old-style, two-party politicians.',
+    icon: <RiLineChartLine />,
+    bgfrom: 'from-[#F48015]',
+    bgto: 'to-[#5DF415]',
   },
   {
     name: 'Track',
     title: 'Track your progress',
     description:
       'Keep your campaign on track by logging the most important activities of your campaign. Your free tracker shows you everything you need to do to get a win.',
+    icon: <LuStars />,
+    bgfrom: 'from-[#FF3AEB]',
+    bgto: 'to-[#3AF3FF]',
   },
 ];
 
@@ -31,7 +40,7 @@ export default function Features({ demoCallback }) {
   return (
     <section className="my-24">
       <MaxWidth>
-        <div className="flex justify-center items-center">
+        {/* <div className="flex justify-center items-center">
           <div className="flex flex-col md:flex-row items-center justify-between p-5 w-3/4">
             <div className="flex flex-col flex-auto items-center text-center">
               <h2 className="font-black text-8xl">100+</h2>
@@ -47,37 +56,42 @@ export default function Features({ demoCallback }) {
               <div className="text-lg mt-3">Cost to you</div>
             </div>
           </div>
+        </div> */}
+
+        <div className="grid grid-cols-12 gap-3 items-stretch text-center">
+          <div className="col-span-12 md:col-span-4">
+            <h2 className="font-black text-8xl">100+</h2>
+            <div className="text-lg mt-3">Candidates supported</div>
+          </div>
+          <div className="col-span-12 md:col-span-4">
+            <h2 className="font-black text-8xl">50+</h2>
+            <div className="text-lg mt-3">Years of Experience</div>
+          </div>
+          <div className="col-span-12 md:col-span-4">
+            <h2 className="font-black text-8xl">$0</h2>
+            <div className="text-lg mt-3">Cost to you</div>
+          </div>
         </div>
 
-        <div className="flex flex-col md:flex-row justify-center items-center mt-10 md:mt-20">
-          <div className="flex flex-col justify-center flex-auto md:w-1/3">
-            {features.map((step, index) => (
-              <div key={step.name}>
+        <div className="grid grid-cols-12 gap-3 justify-center items-center mt-10 md:mt-20">
+          <div className="col-span-12 md:col-span-4 justify-center">
+            {features.map((feature, index) => (
+              <div key={feature.name}>
                 <TogglePanel
-                  label={features[index].title}
+                  label={feature.title}
                   icon={
-                    features[index].name === 'Experts' ? (
-                      <div className="flex items-center justify-center rounded-full p-2 bg-gradient-to-br from-[#4F15F4] to-[#13A9EA] opacity-50">
-                        <RiTeamLine />
-                      </div>
-                    ) : features[index].name === 'Tools' ? (
-                      <div className="flex items-center justify-center rounded-full p-2 bg-gradient-to-br from-[#F48015] to-[#5DF415] opacity-50">
-                        <LuStars />
-                      </div>
-                    ) : features[index].name === 'Track' ? (
-                      <div className="flex items-center justify-center rounded-full p-2 bg-gradient-to-br from-[#FF3AEB] to-[#3AF3FF] opacity-50">
-                        <RiLineChartLine />
-                      </div>
-                    ) : (
-                      <></>
-                    )
+                    <div
+                      className={`flex items-center justify-center rounded-full p-2 bg-gradient-to-br ${feature.bgfrom} ${feature.bgto} opacity-50`}
+                    >
+                      {feature.icon}
+                    </div>
                   }
-                  children={features[index].description}
+                  children={feature.description}
                 />
               </div>
             ))}
           </div>
-          <div className="flex flex-col justify-center items-center text-center h-[500px] w-full md:w-2/3 mt-10 relative">
+          <div className="col-span-12 md:col-span-8 justify-center items-center text-center h-[500px] w-full mt-10 relative">
             <Image
               src="/images/run-page/platform.png"
               fill
