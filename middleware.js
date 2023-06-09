f21a563253bcb6515799aec9cbda329cb35cd0ad
Taylor Murray <taylor@goodparty.org>
2023-06-09 13:39:07 -0700

    feat: contentful redirects.

--- middleware.js
+++ middleware.js
@@ -1,6 +1,31 @@
 import { NextResponse } from 'next/server';
+import gpApi from 'gpApi';
+import gpFetch from 'gpApi/gpFetch';
 
-const Middleware = (req) => {
+export const fetchRedirects = async () => {
+  const api = gpApi.content.contentByKey;
+  const payload = {
+    key: 'redirects',
+  };
+  return await gpFetch(api, payload, 3600);
+};
+
+export default async function middleware(req) {
+  const { content } = await fetchRedirects();
+
+  // convert array to object for faster lookup
+  const redirects = content.reduce(
+    (acc, { fromPath, toPath }) => ({ ...acc, [fromPath]: toPath }),
+    {},
+  );
+
+  if (redirects.hasOwnProperty(req.nextUrl.pathname)) {
+    return NextResponse.redirect(
+      `${req.nextUrl.origin + redirects[req.nextUrl.pathname]}`,
+      { status: 301 },
+    );
+  }
+
   if (req.nextUrl.pathname === req.nextUrl.pathname.toLowerCase()) {
     return NextResponse.next();
   }
@@ -9,6 +34,4 @@ const Middleware = (req) => {
     `${req.nextUrl.origin + req.nextUrl.pathname.toLowerCase()}`,
     { status: 301 },
   );
-};
-
-export default Middleware;
+}
