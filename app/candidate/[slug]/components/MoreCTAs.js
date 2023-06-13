2d6c9d7641bd62ac3eeb4745a1c059638aa34a5b
tomer-tgp <tomer@thegoodparty.org>
2023-06-13 00:41:30 -0700

    feat: backdrop

--- app/candidate/[slug]/components/MoreCTAs.js
+++ app/candidate/[slug]/components/MoreCTAs.js
@@ -24,27 +24,35 @@ export default function MoreCTAs(props) {
         </SecondaryButton>
       </div>
       {open && (
-        <div className="absolute right-0 top-16 bg-primary text-gray-800 p-5 rounded-lg min-w-[270px] shadow-lg z-20">
-          <ShareButton>
-            <div className="flex items-center cursor-pointer py-3">
-              <RiShareForwardLine />
-              <div className="ml-2">Share</div>
-            </div>
-          </ShareButton>
-          {website && (
-            <a
-              href={website}
-              target="_blank"
-              rel="noopener noreferrer nofollow"
-              className="mt-1 block no-underline"
-            >
-              <div className="flex items-center cursor-pointer  py-3">
-                <RiProfileLine />
-                <div className="ml-2">Candidate website</div>
+        <>
+          <div
+            className="fixed h-screen w-screen top-14 left-0 "
+            onClick={() => {
+              setOpen(false);
+            }}
+          />
+          <div className="absolute right-0 top-16 bg-primary text-gray-800 p-5 rounded-lg min-w-[270px] shadow-lg z-20">
+            <ShareButton>
+              <div className="flex items-center cursor-pointer py-3">
+                <RiShareForwardLine />
+                <div className="ml-2">Share</div>
               </div>
-            </a>
-          )}
-        </div>
+            </ShareButton>
+            {website && (
+              <a
+                href={website}
+                target="_blank"
+                rel="noopener noreferrer nofollow"
+                className="mt-1 block no-underline"
+              >
+                <div className="flex items-center cursor-pointer  py-3">
+                  <RiProfileLine />
+                  <div className="ml-2">Candidate website</div>
+                </div>
+              </a>
+            )}
+          </div>
+        </>
       )}
     </div>
   );
