b96e2c81541685496e39253425051357b041b98d
tomer-tgp <tomer@thegoodparty.org>
2023-06-15 18:19:30 -0700

    fix: substring

--- app/candidate-old/[slug]/components/CandidatePage.js
+++ app/candidate-old/[slug]/components/CandidatePage.js
@@ -18,9 +18,9 @@ function pickTextColorBasedOnBg(
       ? bgColor.toString().substring(1, 7)
       : bgColor;
 
-  var r = parseInt(color.substring(0, 2), 16); // hexToR
-  var g = parseInt(color.substring(2, 4), 16); // hexToG
-  var b = parseInt(color.substring(4, 6), 16); // hexToB
+  var r = parseInt(color?.substring(0, 2), 16); // hexToR
+  var g = parseInt(color?.substring(2, 4), 16); // hexToG
+  var b = parseInt(color?.substring(4, 6), 16); // hexToB
   return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? darkColor : lightColor;
 }
 
