e530bf70e411517e03a55e6046def66b17b9c253
tomer-tgp <tomer@thegoodparty.org>
2023-06-19 11:05:21 -0700

    fix: landing pages bg

--- app/shared/landing-pages/LandingPageHero.js
+++ app/shared/landing-pages/LandingPageHero.js
@@ -1,27 +1,17 @@
 import MaxWidth from '@shared/layouts/MaxWidth';
 import Image from 'next/image';
 
-import bgImg from '/public/images/homepage/art.png';
+import bgImg from '/public/images/landing-pages/hero-bg.png';
 
 export default function LandingPageHero({ children, wideBg, ...props }) {
   return (
     <section className="relative pt-5 lg:pt-20" {...props}>
-      <div
-        className={`absolute h-full w-full max-h[200px] top-0 ${
-          wideBg
-            ? 'lg:w-[60%] lg:left-[40%]'
-            : 'left-[-50%] lg:w-[50%] lg:left-[50%]'
-        }`}
-      >
+      <div className="absolute h-full w-full max-h[200px] top-0 lg:w-[60%] lg:left-[40%]">
         <Image
           src={bgImg}
           sizes="100vw"
           fill
-          className={
-            wideBg
-              ? 'object-contain object-right-top'
-              : 'object-contain object-right-top lg:object-cover lg:object-left-top'
-          }
+          className="object-contain object-right-top"
           alt=""
           placeholder="blur"
           priority
