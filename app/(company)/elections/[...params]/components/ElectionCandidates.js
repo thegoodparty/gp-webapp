9245c3dbca82d61a09de2ebea1a7d81e853e25ac
tomer-tgp <tomer@thegoodparty.org>
2023-06-29 18:58:38 -0400

    fix: WEB-908

--- app/(company)/elections/[...params]/components/ElectionCandidates.js
+++ app/(company)/elections/[...params]/components/ElectionCandidates.js
@@ -4,9 +4,14 @@ import contentfulHelper from 'helpers/contentfulHelper';
 import AvatarWithTracker from '/app/candidate/[slug]/components/AvatarWithTracker';
 import CandidatePill from '/app/candidate/[slug]/components/CandidatePill';
 import styles from './Election.module.scss';
+import Link from 'next/link';
+import { candidateRoute } from 'helpers/candidateHelper';
 
 export default function ElectionCandidates(props) {
   const { content } = props;
+  if (content.candidates) {
+    return null;
+  }
 
   return (
     <section className="bg-primary h-auto pt-20 pb-40">
@@ -28,35 +33,36 @@ export default function ElectionCandidates(props) {
           <div className="col-span-12 lg:col-span-6"></div>
         </div>
         <div className="grid grid-cols-12 gap-3 mt-20 justify-items-center">
-          {content.candidates.map((candidate, index) => (
-            <div
-              key={index}
-              className="col-span-12 lg:col-span-3 justify-items-center"
-            >
-              <div className="flex flex-col items-center justify-center text-slate-50 w-full pl-2 pr-3">
-                <AvatarWithTracker
-                  candidate={candidate}
-                  color={candidate.color}
-                />
-                <span className=" text-slate-50 text-2xl p-3">
-                  {candidate.firstName} {candidate.lastName}
-                </span>
-                <CandidatePill
-                  text={`${candidate.office}, ${
-                    candidate.district ? candidate.district : candidate.state
-                  }`}
-                  color={candidate.color}
-                />
+          {content.candidates.map((candidate) => (
+            <Link key={candidate.slug} href={candidateRoute(candidate)}>
+              <div className="col-span-12 lg:col-span-3 justify-items-center">
+                <div className="flex flex-col items-center justify-center text-slate-50 w-full pl-2 pr-3">
+                  <AvatarWithTracker
+                    candidate={candidate}
+                    color={candidate.color}
+                  />
+                  <span className=" text-slate-50 text-2xl p-3">
+                    {candidate.firstName} {candidate.lastName}
+                  </span>
+                  <CandidatePill
+                    text={`${candidate.office}, ${
+                      candidate.district ? candidate.district : candidate.state
+                    }`}
+                    color={candidate.color}
+                  />
 
-                <ul className="font-sfpro text-[16px] font-normal max-w-[300px]">
-                  <li className="text-slate-50 pt-2">
-                    {candidate.topPosition}
-                  </li>
-                  <li className="text-slate-50 pt-2">{candidate.slogan}</li>
-                  <li className="text-slate-50 pt-2">{candidate.occupation}</li>
-                </ul>
+                  <ul className="font-sfpro text-[16px] font-normal max-w-[300px]">
+                    <li className="text-slate-50 pt-2">
+                      {candidate.topPosition}
+                    </li>
+                    <li className="text-slate-50 pt-2">{candidate.slogan}</li>
+                    <li className="text-slate-50 pt-2">
+                      {candidate.occupation}
+                    </li>
+                  </ul>
+                </div>
               </div>
-            </div>
+            </Link>
           ))}
         </div>
       </MaxWidth>
