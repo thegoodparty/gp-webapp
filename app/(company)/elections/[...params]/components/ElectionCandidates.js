9704b0130a47b47974ef623bc41dc7918958fc62
tomer-tgp <tomer@thegoodparty.org>
2023-07-11 21:50:23 -0400

    fix: alignment-3

--- app/(company)/elections/[...params]/components/ElectionCandidates.js
+++ app/(company)/elections/[...params]/components/ElectionCandidates.js
@@ -32,66 +32,72 @@ export default function ElectionCandidates(props) {
             </CmsContentWrapper>
           </div>
         </div>
-        <div className="flex flex-col lg:flex-row w-full flex-auto mt-20 justify-center items-center">
-          {content.candidates.map((candidate) => (
-            <div
-              className="flex w-auto justify-center justify-items-center mb-20 h-full"
-              key={candidate.slug}
-            >
-              <div className="flex flex-col items-center justify-center text-slate-50 w-full pl-2 pr-3">
-                <AvatarWithTracker
-                  candidate={candidate}
-                  color={candidate.color ?? colors[0]}
-                  candidateUrl={candidateRoute(candidate)}
-                />
+        <div className="mx-auto">
+          <div className="flex flex-col lg:flex-row w-full flex-auto mt-20 justify-stretch items-center">
+            {content.candidates.map((candidate) => (
+              <div
+                className="flex w-auto justify-center justify-items-center mb-20 h-full"
+                key={candidate.slug}
+              >
+                <div className="flex flex-col items-center justify-center text-slate-50 w-full pl-2 pr-3">
+                  <AvatarWithTracker
+                    candidate={candidate}
+                    color={candidate.color ?? colors[0]}
+                    candidateUrl={candidateRoute(candidate)}
+                  />
 
-                <Link href={candidateRoute(candidate)}>
-                  <span className=" text-slate-50 text-2xl p-3">
-                    {candidate.firstName} {candidate.lastName}
-                  </span>
-                </Link>
+                  <Link href={candidateRoute(candidate)}>
+                    <span className=" text-slate-50 text-2xl p-3">
+                      {candidate.firstName} {candidate.lastName}
+                    </span>
+                  </Link>
 
-                <CandidatePill
-                  text={`${candidate.office}, ${
-                    candidate.district ?? candidate.state
-                  }`}
-                  color={candidate.color ?? colors[0]}
-                  className="mt-3"
-                />
+                  <CandidatePill
+                    text={`${candidate.office}, ${
+                      candidate.district ?? candidate.state
+                    }`}
+                    color={candidate.color ?? colors[0]}
+                    className="mt-3"
+                  />
 
-                <ul className="font-sfpro text-[16px] font-normal max-w-[300px]">
-                  <li className="text-slate-50 pt-2">
-                    <div className="line-clamp-3">{candidate.topPosition}</div>
-                  </li>
-                  <li className="text-slate-50 pt-2">{candidate.slogan}</li>
-                  <li className="text-slate-50 pt-2">{candidate.occupation}</li>
-                </ul>
+                  <ul className="font-sfpro text-[16px] font-normal max-w-[300px]">
+                    <li className="text-slate-50 pt-2">
+                      <div className="line-clamp-3">
+                        {candidate.topPosition}
+                      </div>
+                    </li>
+                    <li className="text-slate-50 pt-2">{candidate.slogan}</li>
+                    <li className="text-slate-50 pt-2">
+                      {candidate.occupation}
+                    </li>
+                  </ul>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
 
-        <div className="flex flex-col text-center pb-5 lg:pl-20 p-10">
-          <div className="font-sfpro text-slate-50 font-semibold text-[32px] md:text-[54px] leading-[36px] md:leading-[64px] mt-2">
-            {content.districtTitle}
-          </div>
-          <div className="flex justify-center">
-            <Image
-              src={`https:${content?.districtImage?.url}`}
-              width={584}
-              height={524}
-              alt={`${city} district map`}
-            />
-          </div>
-          <div>
-            <a
-              id="district-link"
-              href={content.districtButtonLink}
-              target="_blank"
-              rel="noopener noreferrer nofollow"
-            >
-              <WarningButton>{content.districtButtonText}</WarningButton>
-            </a>
+          <div className="flex flex-col text-center pb-5 lg:pl-20 p-10">
+            <div className="font-sfpro text-slate-50 font-semibold text-[32px] md:text-[54px] leading-[36px] md:leading-[64px] mt-2">
+              {content.districtTitle}
+            </div>
+            <div className="flex justify-center">
+              <Image
+                src={`https:${content?.districtImage?.url}`}
+                width={584}
+                height={524}
+                alt={`${city} district map`}
+              />
+            </div>
+            <div>
+              <a
+                id="district-link"
+                href={content.districtButtonLink}
+                target="_blank"
+                rel="noopener noreferrer nofollow"
+              >
+                <WarningButton>{content.districtButtonText}</WarningButton>
+              </a>
+            </div>
           </div>
         </div>
       </MaxWidth>
