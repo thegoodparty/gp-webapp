0dc9b12c26e704d55ad3155eca2dc0b29ee88c94
tomer-tgp <tomer@thegoodparty.org>
2023-07-12 22:03:19 -0400

    fix: not found2

--- app/not-found.js
+++ app/not-found.js
@@ -1,18 +1,23 @@
+import H2 from '@shared/typography/H2';
+import H3 from '@shared/typography/H3';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function NotFound() {
   return (
     <div className="min-h-[calc(100vh-56px)] flex flex-col items-center justify-center">
-      <h2>Not Found</h2>
-      <p className="mb-20 mt-3">Could not find requested resource</p>
-      <Image
-        src="/images/logo-hologram.svg"
-        data-cy="logo"
-        width={145}
-        height={32}
-        alt="GOOD PARTY"
-        priority
-      />
+      <H2>Not Found</H2>
+      <H3 className="my-3">Could not find requested resource</H3>
+      <Link href="/">
+        <Image
+          src="/images/logo-hologram.svg"
+          data-cy="logo"
+          width={145}
+          height={32}
+          alt="GOOD PARTY"
+          priority
+        />
+      </Link>
     </div>
   );
 }
