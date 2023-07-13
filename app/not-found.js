e7ad67959c65d2fcde33c84e6505601fb8abfe3c
tomer-tgp <tomer@thegoodparty.org>
2023-07-12 21:54:22 -0400

    fix: 404

--- app/not-found.js
+++ app/not-found.js
@@ -1,8 +1,18 @@
+import Image from 'next/image';
+
 export default function NotFound() {
   return (
-    <>
+    <div className="min-h-[calc(100vh-56px)] flex flex-col items-center justify-center">
       <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-    </>
+      <p className="mb-20 mt-3">Could not find requested resource</p>
+      <Image
+        src="/images/logo-hologram.svg"
+        data-cy="logo"
+        width={145}
+        height={32}
+        alt="GOOD PARTY"
+        priority
+      />
+    </div>
   );
 }
