599fdfc904ad057ed414f73ae916909ca9430e09
tomer-tgp <tomer@thegoodparty.org>
2023-07-13 11:06:07 -0400

    chore: 500 test

---
+++ app/test-500/page.js
@@ -0,0 +1,16 @@
+import pageMetaData from 'helpers/metadataHelper';
+
+const meta = pageMetaData({
+  title: 'Blog | GOOD PARTY',
+  description: 'Good Party Blog',
+  slug: '/blog',
+});
+export const metadata = meta;
+
+export default async function Page({ params, searchParams }) {
+  return (
+    <div>
+      testing 500 <ThisShouldFail />
+    </div>
+  );
+}
