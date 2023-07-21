99918fa318e11eace945b6c9b2fb7a990cb87575
tomer-tgp <tomer@thegoodparty.org>
2023-07-21 14:27:21 -0500

    feat: WEB-982

--- app/(company)/elections/[...params]/components/ElectionCandidates.js
+++ app/(company)/elections/[...params]/components/ElectionCandidates.js
@@ -1,46 +1,55 @@
 import MaxWidth from '@shared/layouts/MaxWidth';
 import CmsContentWrapper from '@shared/content/CmsContentWrapper';
 import contentfulHelper from 'helpers/contentfulHelper';
-import AvatarWithTracker from '/app/candidate/[slug]/components/AvatarWithTracker';
-import CandidatePill from '/app/candidate/[slug]/components/CandidatePill';
 import styles from './Election.module.scss';
-import Link from 'next/link';
-import { candidateRoute } from 'helpers/candidateHelper';
-import { colors } from '/app/candidate/[slug]/components/CandidateColors';
 import Image from 'next/image';
 import InfoButton from '@shared/buttons/InfoButton';
 import WarningButton from '@shared/buttons/WarningButton';
 import ElectionCandidate from './ElectionCandidate';
+import dynamic from 'next/dynamic';
+const ScrollIntoView = dynamic(() => import('react-scroll-into-view'));
 
 export default function ElectionCandidates(props) {
   const { content, city } = props;
-  if (!content.candidates || content.candidates.length === 0) {
+  const {
+    candidates,
+    candidatesTitle,
+    candidatesSubTitle,
+    districtImage,
+    districtTitle,
+    districtButtonText,
+    districtButtonLink,
+    districtButton2Text,
+    districtButton2Link,
+    isButton2Scroll,
+  } = content || {};
+  if (!candidates || candidates.length === 0) {
     return null;
   }
 
   let startClass = '';
-  if (content.candidates.length === 2) {
+  if (candidates.length === 2) {
     startClass = 'lg:col-start-3';
   }
 
   return (
-    <section className="bg-primary h-auto pt-20 pb-40">
+    <section className="bg-primary h-auto pt-20 pb-40" id="candidate-section">
       <MaxWidth>
         <div className="flex flex-col text-center pb-5 lg:pl-20 p-10">
           <div className="font-sfpro text-slate-50 font-semibold text-[32px] md:text-[54px] leading-[36px] md:leading-[64px] mt-2">
-            {content.candidatesTitle}
+            {candidatesTitle}
           </div>
 
           <div
             className={`font-sfpro text-center text-slate-50 text-[18px] leading-6 mt-2 ${styles.hyperlink}`}
           >
             <CmsContentWrapper className="max-w-md">
-              {contentfulHelper(content.candidatesSubTitle)}
+              {contentfulHelper(candidatesSubTitle)}
             </CmsContentWrapper>
           </div>
         </div>
         <div className="md:flex justify-center flex-wrap">
-          {content.candidates.map((candidate) => (
+          {candidates.map((candidate) => (
             <div className="md:basis-1/2 xl:basis-1/4" key={candidate.slug}>
               <ElectionCandidate candidate={candidate} />
             </div>
@@ -48,14 +57,14 @@ export default function ElectionCandidates(props) {
         </div>
 
         <div className="flex flex-col text-center pb-5 lg:pl-20 p-10">
-          {content?.districtImage && (
+          {districtImage && (
             <>
               <div className="font-sfpro text-slate-50 font-semibold text-[32px] md:text-[54px] leading-[36px] md:leading-[64px] mt-2">
-                {content.districtTitle}
+                {districtTitle}
               </div>
               <div className="flex justify-center">
                 <Image
-                  src={`https:${content?.districtImage?.url}`}
+                  src={`https:${districtImage?.url}`}
                   width={584}
                   height={524}
                   alt={`${city} district map`}
@@ -64,32 +73,41 @@ export default function ElectionCandidates(props) {
             </>
           )}
           <div class="flex flex-col md:flex-row justify-center">
-            {content?.districtButtonText && (
+            {districtButtonText && (
               <div>
                 <a
                   id="district-link"
-                  href={content.districtButtonLink}
+                  href={districtButtonLink}
                   target="_blank"
                   rel="noopener noreferrer nofollow"
                 >
                   <InfoButton className="whitespace-nowrap">
-                    {content.districtButtonText}
+                    {districtButtonText}
                   </InfoButton>
                 </a>
               </div>
             )}
-            {content?.districtButton2Text && (
+            {districtButton2Text && (
               <div className="mt-4 md:mt-0 md:ml-4">
-                <a
-                  id="district-link2"
-                  href={content.districtButton2Link}
-                  target="_blank"
-                  rel="noopener noreferrer nofollow"
-                >
-                  <WarningButton className="whitespace-nowrap">
-                    {content.districtButton2Text}
-                  </WarningButton>
-                </a>
+                {isButton2Scroll ? (
+                  <ScrollIntoView selector="#candidate-section">
+                    {' '}
+                    <WarningButton className="whitespace-nowrap">
+                      {districtButton2Text}
+                    </WarningButton>
+                  </ScrollIntoView>
+                ) : (
+                  <a
+                    id="district-link2"
+                    href={districtButton2Link}
+                    target="_blank"
+                    rel="noopener noreferrer nofollow"
+                  >
+                    <WarningButton className="whitespace-nowrap">
+                      {districtButton2Text}
+                    </WarningButton>
+                  </a>
+                )}
               </div>
             )}
           </div>
