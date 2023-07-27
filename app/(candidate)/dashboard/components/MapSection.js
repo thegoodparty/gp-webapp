b206511c595c50eef5e00386f4600ebf5f020ab5
tomer-tgp <tomer@thegoodparty.org>
2023-07-26 19:47:41 -0600

    fix: map height

--- app/(candidate)/dashboard/components/MapSection.js
+++ app/(candidate)/dashboard/components/MapSection.js
@@ -4,8 +4,13 @@ export default function MapSection({ map }) {
   return (
     <section className="my-16">
       <H3>Door knocking map</H3>
-      <div className="h-96 rounded-2xl overflow-hidden mt-4">
-        <iframe src={map} width="100%" height="384px" />
+      <div className="h-[70vh] rounded-2xl overflow-hidden mt-4">
+        <iframe
+          src={map}
+          width="100%"
+          height="384px"
+          style={{ height: '70vh' }}
+        />
       </div>
     </section>
   );
