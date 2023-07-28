f09f819f435514bb85f213ad54194731ae38ddff
tomer-tgp <tomer@thegoodparty.org>
2023-07-28 17:51:33 -0600

    chore: bc13

--- app/faqs/[...titleId]/components/Links.js
+++ app/faqs/[...titleId]/components/Links.js
@@ -1,10 +1,12 @@
-import Link from 'next/link';
+// import Link from 'next/link';
 // import { TbSlash } from 'react-icons/tb';
 // import { JsonLd } from 'react-schemaorg';
 
 // import MuiBreadcrumbs from '@mui/material/Breadcrumbs';
 
 export default function Links({ links, withRefresh = false }) {
+  return <div>Tomer</div>;
+
   // const schema = [];
   // links.forEach((link, index) => {
   //   schema.push({
@@ -17,48 +19,48 @@ export default function Links({ links, withRefresh = false }) {
   //   });
   // });
 
-  return (
-    <div className="flex items-center whitespace-nowrap max-w-[100vw] overflow-x-auto py-6">
-      {/* <JsonLd
-        item={{
-          '@context': 'https://schema.org',
-          '@type': 'BreadcrumbList',
-          itemListElement: schema,
-        }}
-      /> */}
-      <nav className="flex" aria-label="Breadcrumb">
-        <ol className="inline-flex items-center space-x-1">
-          {links.map((link, index) => (
-            <li key={link.label}>
-              {index < links.length - 1 && (
-                <div className="flex items-center">
-                  {withRefresh ? (
-                    <a
-                      href={link.href}
-                      className="inline-flex items-center text-xs lg:text-base text-indigo-400 hover:text-indigo-800 hover:underline"
-                    >
-                      <div className="text-xs lg:text-base">{link.label}</div>
-                    </a>
-                  ) : (
-                    <Link
-                      href={link.href}
-                      className="inline-flex items-center text-xs lg:text-base text-indigo-400 hover:text-indigo-800 hover:underline"
-                    >
-                      <div className="text-xs lg:text-base">{link.label}</div>
-                    </Link>
-                  )}
-                  {/* <TbSlash className="ml-2" /> */}
-                </div>
-              )}
-            </li>
-          ))}
-          <li>
-            <div className="text-xs lg:text-base text-indigo-800">
-              {links[links.length - 1].label}
-            </div>
-          </li>
-        </ol>
-      </nav>
-    </div>
-  );
+  // return (
+  //   <div className="flex items-center whitespace-nowrap max-w-[100vw] overflow-x-auto py-6">
+  //     {/* <JsonLd
+  //       item={{
+  //         '@context': 'https://schema.org',
+  //         '@type': 'BreadcrumbList',
+  //         itemListElement: schema,
+  //       }}
+  //     /> */}
+  //     <nav className="flex" aria-label="Breadcrumb">
+  //       <ol className="inline-flex items-center space-x-1">
+  //         {links.map((link, index) => (
+  //           <li key={link.label}>
+  //             {index < links.length - 1 && (
+  //               <div className="flex items-center">
+  //                 {withRefresh ? (
+  //                   <a
+  //                     href={link.href}
+  //                     className="inline-flex items-center text-xs lg:text-base text-indigo-400 hover:text-indigo-800 hover:underline"
+  //                   >
+  //                     <div className="text-xs lg:text-base">{link.label}</div>
+  //                   </a>
+  //                 ) : (
+  //                   <Link
+  //                     href={link.href}
+  //                     className="inline-flex items-center text-xs lg:text-base text-indigo-400 hover:text-indigo-800 hover:underline"
+  //                   >
+  //                     <div className="text-xs lg:text-base">{link.label}</div>
+  //                   </Link>
+  //                 )}
+  //                 {/* <TbSlash className="ml-2" /> */}
+  //               </div>
+  //             )}
+  //           </li>
+  //         ))}
+  //         <li>
+  //           <div className="text-xs lg:text-base text-indigo-800">
+  //             {links[links.length - 1].label}
+  //           </div>
+  //         </li>
+  //       </ol>
+  //     </nav>
+  //   </div>
+  // );
 }
