ff8b5b6581111788587299ca9ba320448529fef5
tomer-tgp <tomer@thegoodparty.org>
2023-07-28 16:21:11 -0600

    chore: breadcrumbs8

--- app/shared/utils/Breadcrumbs.js
+++ app/shared/utils/Breadcrumbs.js
@@ -1,33 +1,31 @@
-// 'use client';
 import Link from 'next/link';
-import { MdOutlineChevronRight } from 'react-icons/md';
 import { TbSlash } from 'react-icons/tb';
-import { JsonLd } from 'react-schemaorg';
+// import { JsonLd } from 'react-schemaorg';
 
 // import MuiBreadcrumbs from '@mui/material/Breadcrumbs';
 
 export default function Breadcrumbs({ links, withRefresh = false }) {
-  const schema = [];
-  links.forEach((link, index) => {
-    schema.push({
-      '@type': 'ListItem',
-      position: index + 1,
-      item: {
-        '@id': link.href, // || router.asPath,
-        name: link.label,
-      },
-    });
-  });
+  // const schema = [];
+  // links.forEach((link, index) => {
+  //   schema.push({
+  //     '@type': 'ListItem',
+  //     position: index + 1,
+  //     item: {
+  //       '@id': link.href, // || router.asPath,
+  //       name: link.label,
+  //     },
+  //   });
+  // });
 
   return (
     <div className="flex items-center whitespace-nowrap max-w-[100vw] overflow-x-auto py-6">
-      <JsonLd
+      {/* <JsonLd
         item={{
           '@context': 'https://schema.org',
           '@type': 'BreadcrumbList',
           itemListElement: schema,
         }}
-      />
+      /> */}
       <nav class="flex" aria-label="Breadcrumb">
         <ol class="inline-flex items-center space-x-1">
           {links.map((link, index) => (
