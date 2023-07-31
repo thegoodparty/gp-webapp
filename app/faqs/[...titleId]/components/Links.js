52532978f3860ebb3bc7d4fff67c6b3018d94d05
tomer-tgp <tomer@thegoodparty.org>
2023-07-30 20:59:45 -0700

    fix: breadcrumbs

--- app/faqs/[...titleId]/components/Links.js
+++ app/faqs/[...titleId]/components/Links.js
@@ -1,94 +1,52 @@
-// import Link from 'next/link';
-// import { TbSlash } from 'react-icons/tb';
-// import { JsonLd } from 'react-schemaorg';
+import { TbSlash } from 'react-icons/tb';
+import { JsonLd } from 'react-schemaorg';
 
-import Link from 'next/link';
+export default function Links({ links }) {
+  const schema = [];
+  links.forEach((link, index) => {
+    schema.push({
+      '@type': 'ListItem',
+      position: index + 1,
+      item: {
+        '@id': link.href, // || router.asPath,
+        name: link.label,
+      },
+    });
+  });
 
-// import MuiBreadcrumbs from '@mui/material/Breadcrumbs';
-
-export default function Links({ links, withRefresh = false }) {
   return (
     <div className="flex items-center whitespace-nowrap max-w-[100vw] overflow-x-auto py-6">
-      <nav className="flex" aria-label="Breadcrumb">
-        <ol className="inline-flex items-center space-x-1">
+      <JsonLd
+        item={{
+          '@context': 'https://schema.org',
+          '@type': 'BreadcrumbList',
+          itemListElement: schema,
+        }}
+      />
+      <nav class="flex" aria-label="Breadcrumb">
+        <ol class="inline-flex items-center space-x-1">
           {links.map((link, index) => (
             <li key={link.label}>
-              {withRefresh ? (
-                <a
-                  href={link.href}
-                  className="inline-flex items-center text-xs lg:text-base text-indigo-400 hover:text-indigo-800 hover:underline"
-                >
-                  <div className="text-xs lg:text-base">{link.label}</div>
-                </a>
-              ) : (
-                <Link
-                  href={link.href}
-                  className="inline-flex items-center text-xs lg:text-base text-indigo-400 hover:text-indigo-800 hover:underline"
-                >
-                  {link.label}
-                </Link>
+              {index < links.length - 1 && (
+                <div class="flex items-center">
+                  <a
+                    href={link.href}
+                    class="inline-flex items-center text-xs lg:text-base text-indigo-400 hover:text-indigo-800 hover:underline"
+                  >
+                    <div className="text-xs lg:text-base">{link.label}</div>
+                  </a>
+                  <TbSlash className="ml-2" />
+                </div>
               )}
             </li>
           ))}
+          <li>
+            <div className="text-xs lg:text-base text-indigo-800">
+              {links[links.length - 1].label}
+            </div>
+          </li>
         </ol>
       </nav>
     </div>
   );
-
-  // const schema = [];
-  // links.forEach((link, index) => {
-  //   schema.push({
-  //     '@type': 'ListItem',
-  //     position: index + 1,
-  //     item: {
-  //       '@id': link.href, // || router.asPath,
-  //       name: link.label,
-  //     },
-  //   });
-  // });
-
-  // return (
-  //   <div className="flex items-center whitespace-nowrap max-w-[100vw] overflow-x-auto py-6">
-  //     {/* <JsonLd
-  //       item={{
-  //         '@context': 'https://schema.org',
-  //         '@type': 'BreadcrumbList',
-  //         itemListElement: schema,
-  //       }}
-  //     /> */}
-  //     <nav className="flex" aria-label="Breadcrumb">
-  //       <ol className="inline-flex items-center space-x-1">
-  //         {links.map((link, index) => (
-  //           <li key={link.label}>
-  //             {index < links.length - 1 && (
-  //               <div className="flex items-center">
-  //                 {withRefresh ? (
-  //                   <a
-  //                     href={link.href}
-  //                     className="inline-flex items-center text-xs lg:text-base text-indigo-400 hover:text-indigo-800 hover:underline"
-  //                   >
-  //                     <div className="text-xs lg:text-base">{link.label}</div>
-  //                   </a>
-  //                 ) : (
-  //                   <Link
-  //                     href={link.href}
-  //                     className="inline-flex items-center text-xs lg:text-base text-indigo-400 hover:text-indigo-800 hover:underline"
-  //                   >
-  //                     <div className="text-xs lg:text-base">{link.label}</div>
-  //                   </Link>
-  //                 )}
-  //                 {/* <TbSlash className="ml-2" /> */}
-  //               </div>
-  //             )}
-  //           </li>
-  //         ))}
-  //         <li>
-  //           <div className="text-xs lg:text-base text-indigo-800">
-  //             {links[links.length - 1].label}
-  //           </div>
-  //         </li>
-  //       </ol>
-  //     </nav>
-  //   </div>
-  // );
 }
