c5f9083dd07ccdb4895b387acb4c031650f24a30
tomer-tgp <tomer@thegoodparty.org>
2023-08-08 21:28:49 -0700

    fix: hero image

--- app/shared/landing-pages/LandingPageHero.js
+++ app/shared/landing-pages/LandingPageHero.js
@@ -13,8 +13,8 @@ export default function LandingPageHero({ children, wideBg, ...props }) {
           fill
           className="object-contain object-right-top"
           alt=""
-          placeholder="blur"
           priority
+          quality={50}
         />
       </div>
       <MaxWidth>
