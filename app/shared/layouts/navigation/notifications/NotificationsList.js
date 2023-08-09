14048e5fba91f7e1d65b6ac0bd9b2d0b31a3e61a
tomer-tgp <tomer@thegoodparty.org>
2023-08-09 12:25:28 -0700

    feat: empty notification message

--- app/shared/layouts/navigation/notifications/NotificationsList.js
+++ app/shared/layouts/navigation/notifications/NotificationsList.js
@@ -1,4 +1,5 @@
 'use client';
+import H3 from '@shared/typography/H3';
 import Notification from './Notification';
 
 export default function NotificationsList({ notifications }) {
@@ -7,6 +8,9 @@ export default function NotificationsList({ notifications }) {
       {notifications.map((notification) => (
         <Notification notification={notification} key={notification.id} />
       ))}
+      {notifications.length === 0 && (
+        <H3 className="mt-3 text-center">No notifications</H3>
+      )}
     </div>
   );
 }
