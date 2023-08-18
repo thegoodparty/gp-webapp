3f453e78141f7c9d914b5ca0fd518ebfd9adf513
taylorgp <124819672+taylorgp@users.noreply.github.com>
2023-08-17 18:40:03 -0700

    Web 1057 (#68)
    * chore: debug jodit.
    
    * feat: my content auto save feature.
    
    * fix: remove save button.

--- app/(candidate)/dashboard/content/[slug]/components/ContentEditor.js
+++ app/(candidate)/dashboard/content/[slug]/components/ContentEditor.js
@@ -1,78 +1,58 @@
 'use client';
-import { useEffect, useState } from 'react';
-import dynamic from 'next/dynamic';
-import { useHookstate } from '@hookstate/core';
-import { globalSnackbarState } from '@shared/utils/Snackbar';
+import { useEffect, useState, useRef } from 'react';
 import { updateCampaign } from 'app/(candidate)/onboarding/shared/ajaxActions';
 import PlanVersion from './PlanVersion';
 import PrimaryButton from '@shared/buttons/PrimaryButton';
 import LoadingAI from 'app/(candidate)/onboarding/[slug]/campaign-plan/components/LoadingAI';
 import BlackButton from '@shared/buttons/BlackButton';
 import AiModal from 'app/(candidate)/onboarding/[slug]/campaign-plan/components/AiModal';
-import Typewriter from 'typewriter-effect';
-import { FaPencilAlt, FaSave } from 'react-icons/fa';
+import { FaSave } from 'react-icons/fa';
 import SecondaryButton from '@shared/buttons/SecondaryButton';
 import { MdOutlineArrowBackIos } from 'react-icons/md';
 import Link from 'next/link';
 import Actions from '../../components/Actions';
-
-const RichEditor = dynamic(
-  () =>
-    import(
-      'app/(candidate)/onboarding/[slug]/campaign-plan/components/RichEditor'
-    ),
-  {
-    loading: () => (
-      <p className="p-4 text-center text-2xl font-bold">Loading Editor...</p>
-    ),
-  },
-);
-
-let aiCount = 0;
-let aiTotalCount = 0;
+import { debounce } from '/helpers/debounceHelper';
+import JoditEditor from 'jodit-react';
+// import Typewriter from 'typewriter-effect';
+// import { useHookstate } from '@hookstate/core';
+// import { globalSnackbarState } from '@shared/utils/Snackbar';
 
 export default function ContentEditor({
   section = '',
   campaign,
-  initialOpen,
   versions = {},
   updateVersionsCallback,
-  forceExpand,
   subSectionKey = 'aiContent',
 }) {
-  const [open, setOpen] = useState(initialOpen);
-  const [editMode, setEditMode] = useState(true);
-  const [isEdited, setIsEdited] = useState(false);
-  const [plan, setPlan] = useState(false);
   const [loading, setLoading] = useState(true);
   const [isTyped, setIsTyped] = useState(false);
   const [isFailed, setIsFailed] = useState(false);
-  const snackbarState = useHookstate(globalSnackbarState);
   const [documentName, setDocumentName] = useState('Untitled Document');
+  const [saved, setSaved] = useState('');
+  // const snackbarState = useHookstate(globalSnackbarState);
 
-  const campaignPlan = campaign[subSectionKey];
+  const editor = useRef(null);
+  const plan = useRef('');
   const key = section;
 
   useEffect(() => {
-    if (campaignPlan && campaignPlan[key]) {
-      setPlan(campaignPlan[key].content);
-      setDocumentName(campaignPlan[key].name);
+    if (campaign && campaign[subSectionKey][key]) {
+      plan.current = campaign[subSectionKey][key].content;
+      setDocumentName(campaign[subSectionKey][key].name);
+      setSaved('Saved');
       setLoading(false);
       setIsTyped(true);
     }
-  }, [campaignPlan]);
+  }, [campaign]);
 
-  const toggleSelect = () => {
-    setOpen(!open);
-  };
-
-  const setEdit = () => {
-    setIsEdited(true);
-    setEditMode(true);
-  };
-
-  const handleEdit = async (editedPlan) => {
-    setPlan(editedPlan);
+  // Function to be called when the user has finished typing
+  const handleTypingComplete = async () => {
+    if (campaign[subSectionKey][key].content === plan.current) {
+      // do not call save if the content has not changed from load.
+      return;
+    } else {
+      await handleSave();
+    }
   };
 
   const handleRegenerate = async (improveQuery) => {
@@ -80,11 +60,11 @@ export default function ContentEditor({
     let chat = [];
     if (improveQuery !== '') {
       chat = [
-        { role: 'system', content: plan },
+        { role: 'system', content: plan.current },
         { role: 'user', content: improveQuery },
       ];
     }
-    setPlan(false);
+    plan.current = '';
     aiCount = 0;
     aiTotalCount = 0;
     setIsTyped(false);
@@ -92,13 +72,7 @@ export default function ContentEditor({
   };
 
   const handleSave = async () => {
-    snackbarState.set(() => {
-      return {
-        isOpen: true,
-        message: 'Saving...',
-        isError: false,
-      };
-    });
+    setSaved('Saving...');
 
     const updated = campaign;
     let existing_name;
@@ -113,23 +87,31 @@ export default function ContentEditor({
     updated[subSectionKey][key] = {
       name: existing_name ? existing_name : key,
       updatedAt: updatedAt,
-      content: plan,
+      content: plan.current,
     };
 
-    // updated[subSectionKey][key] = plan;
-    setIsEdited(false);
-    setEditMode(false);
     await updateCampaign(updated, key, false, 'aiContent');
     await updateVersionsCallback();
-    // router.push(`/onboarding/${campaign.slug}/dashboard/1`);
   };
 
   const updatePlanCallback = (version) => {
-    setPlan(version.text);
-    setIsEdited(true);
+    console.log('setting plan - callback!');
+    plan.current = version.text;
   };
 
-  const expand = open || forceExpand;
+  const config = {
+    readonly: false, // all options from https://xdsoft.net/jodit/doc/
+    enableDragAndDropFileToEditor: false,
+    useSearch: false,
+    toolbar: false,
+    showCharsCounter: false,
+    showWordsCounter: false,
+    showXPathInStatusbar: false,
+    toolbarInlineForSelection: true,
+    showPlaceholder: false,
+    buttons:
+      'bold,italic,underline,strikethrough,ul,ol,fontsize,paragraph,copy,paste,hr,table,print',
+  };
 
   return (
     <div>
@@ -164,9 +146,9 @@ export default function ContentEditor({
             <div className="text-indigo-800 p-1 md:mt-2">{documentName}</div>
           </div>
 
-          {/* <div className="ml-3">
-            <div className="text-indigo-100 p-1 md:mt-2">Saved</div>
-          </div> */}
+          <div className="ml-3">
+            <div className="text-indigo-100 p-1 md:mt-2">{saved}</div>
+          </div>
         </div>
 
         <div className="flex w-full justify-end">
@@ -175,7 +157,7 @@ export default function ContentEditor({
             campaign={campaign}
             versions={versions ? versions[key] : {}}
             updatePlanCallback={updatePlanCallback}
-            latestVersion={campaignPlan ? campaignPlan[key] : false}
+            latestVersion={campaign ? campaign[subSectionKey][key] : false}
           />
 
           <Actions
@@ -209,48 +191,27 @@ export default function ContentEditor({
                         </div>
                       </div>
                     ) : (
-                      <>
-                        {editMode ? (
-                          <RichEditor
-                            initialText={plan}
-                            onChangeCallback={handleEdit}
-                            // sx={{'jd-color-border': '#ffffff'}}
-                          />
-                        ) : (
-                          <div
-                            className="relative pb-10 cursor-text"
-                            onClick={setEdit}
-                          >
-                            {initialOpen && !isTyped ? (
-                              <Typewriter
-                                options={{
-                                  delay: 1,
-                                }}
-                                onInit={(typewriter) => {
-                                  typewriter
-                                    .typeString(plan)
-                                    .callFunction(() => {
-                                      setIsTyped(true);
-                                    })
-
-                                    .start();
-                                }}
-                              />
-                            ) : (
-                              <div dangerouslySetInnerHTML={{ __html: plan }} />
-                            )}
-                            <div className="absolute bottom-2 right-2 rounded-full w-10 h-10 flex items-center justify-center bg-slate-500 cursor-pointer hidden-for-print">
-                              <FaPencilAlt />
-                            </div>
-                          </div>
-                        )}
-                      </>
+                      <JoditEditor
+                        ref={editor}
+                        value={plan.current}
+                        config={config}
+                        tabIndex={0}
+                        onChange={(newContent) => {
+                          if (typeof newContent === 'string') {
+                            plan.current = newContent;
+                            debounce(handleTypingComplete, undefined, 5000);
+                          } else {
+                            plan.current = newContent?.target?.innerHTML;
+                            debounce(handleTypingComplete, undefined, 5000);
+                          }
+                        }}
+                      />
                     )}
                   </div>
-                  <div className="flex items-center justify-center mt-6 py-6 hidden-for-print">
+                  {/* <div className="flex items-center justify-center mt-6 py-6 hidden-for-print">
                     <AiModal
                       submitCallback={handleRegenerate}
-                      showWarning={isEdited}
+                      showWarning={false}
                     />
                     <div onClick={handleSave}>
                       <PrimaryButton>
@@ -259,7 +220,7 @@ export default function ContentEditor({
                         </div>
                       </PrimaryButton>
                     </div>
-                  </div>
+                  </div> */}
                 </div>
               )}
             </div>
