6e0ff4ab8995d33b40ad41c970a7de628ebfbbe8
tomer-tgp <tomer@thegoodparty.org>
2023-09-13 17:57:24 -0700

    feat: WEB-1042

--- app/(candidate)/dashboard/components/ProgressPie.js
+++ app/(candidate)/dashboard/components/ProgressPie.js
@@ -8,12 +8,23 @@ import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 // import styles from './GoalsChart.module.scss';
 
 const COLORS = ['#DEE1E9', '#13161A'];
+const COLORS_OVER = ['#44F1A7', '#0EB66F'];
 
 export default function ProgressPie({ total, progress }) {
-  const data = [
-    { name: 'Total', value: total - progress >= 0 ? total - progress : 0 },
-    { name: 'So Far', value: progress },
-  ];
+  let overMode = total - progress < 0;
+
+  let data;
+  if (overMode) {
+    data = [
+      { name: 'Total', value: total },
+      { name: 'So Far', value: total / 5 },
+    ];
+  } else {
+    data = [
+      { name: 'Total', value: total - progress },
+      { name: 'So Far', value: progress },
+    ];
+  }
 
   return (
     <div className="relative">
@@ -31,17 +42,15 @@ export default function ProgressPie({ total, progress }) {
               cy="50%"
               innerRadius={60}
               outerRadius={80}
-              // startAngle={50}
-              // endAngle={310}
-              // fill="#13161A"
-              // labelLine={false}
-              // label={renderCustomizedLabel}
-              // isAnimationActive={false}
             >
               {data.map((entry, index) => (
                 <Cell
                   key={`cell-${index}`}
-                  fill={COLORS[index % COLORS.length]}
+                  fill={
+                    overMode
+                      ? COLORS_OVER[index % COLORS.length]
+                      : COLORS[index % COLORS.length]
+                  }
                   // fill="#EDEDED"
                 />
               ))}
@@ -51,8 +60,11 @@ export default function ProgressPie({ total, progress }) {
       </div>
       <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center text-lg">
         <div className="text-center">
-          <H2 className="text-4xl mb-1">
-            {total - progress > 0 ? kFormatter(total - progress) : 0}
+          <H2 className={`text-4xl mb-1 ${overMode ? 'text-green-500' : ''}`}>
+            {overMode && '+'}
+            {overMode
+              ? -1 * kFormatter(total - progress)
+              : kFormatter(total - progress)}
           </H2>
           <Body2>left this week</Body2>
         </div>
