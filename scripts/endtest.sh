3c7b52dcec6050ca2ff38fcda6efbfcbe2bbdd6c
Taylor Murray <taylor@goodparty.org>
2023-10-05 15:56:39 -0700

    fix: expand endtest options.

--- scripts/endtest.sh
+++ scripts/endtest.sh
@@ -8,7 +8,7 @@ RANDOM_PLATFORM="${platform_options[$RANDOM % ${#platform_options[@]}]}"
 if [ "$RANDOM_PLATFORM" == "ios" ]; then
   device_options=("f06" "f07" "a33" "a34" "a35" "a36" "a37" "a38")
   RANDOM_DEVICE="${device_options[$RANDOM % ${#device_options[@]}]}"
-  export ENDTEST_OPTIONS="&platform=$RANDOM_PLATFORM&os=$RANDOM_DEVICE&resolution=portrait"
+  export ENDTEST_OPTIONS="&platform=$RANDOM_PLATFORM&os=$RANDOM_DEVICE&resolution=portrait&geolocation=sanfrancisco&cases=all&notes="
   echo "ENDTEST_OPTIONS=$ENDTEST_OPTIONS" >> $GITHUB_ENV
   exit 0
 fi
@@ -17,7 +17,7 @@ fi
 if [ "$RANDOM_PLATFORM" == "android" ]; then 
   device_options=("a933" "a934" "a935" "a936" "a0167" "a0168" "a23" "a24" "a25")
   RANDOM_DEVICE="${device_options[$RANDOM % ${#device_options[@]}]}"
-  export ENDTEST_OPTIONS="&platform=$RANDOM_PLATFORM&os=$RANDOM_DEVICE&resolution=portrait"
+  export ENDTEST_OPTIONS="&platform=$RANDOM_PLATFORM&os=$RANDOM_DEVICE&resolution=portrait&geolocation=sanfrancisco&cases=all&notes="
   echo "ENDTEST_OPTIONS=$ENDTEST_OPTIONS" >> $GITHUB_ENV
   exit 0
 fi
@@ -26,7 +26,7 @@ fi
 if [ "$RANDOM_PLATFORM" == "windows" ]; then
   browser_options=("chrome" "firefox" "edge" "ie11")
   RANDOM_BROWSER="${browser_options[$RANDOM % ${#browser_options[@]}]}"
-  export ENDTEST_OPTIONS="&platform=$RANDOM_PLATFORM&os=windows10&browser=$RANDOM_BROWSER&browserVersion=latest"
+  export ENDTEST_OPTIONS="&platform=$RANDOM_PLATFORM&os=windows10&browser=$RANDOM_BROWSER&browserVersion=latest&geolocation=sanfrancisco&cases=all&notes="
   echo "ENDTEST_OPTIONS=$ENDTEST_OPTIONS" >> $GITHUB_ENV
   exit 0
 fi
@@ -35,7 +35,7 @@ fi
 if [ "$RANDOM_PLATFORM" == "mac" ]; then
   browser_options=("chrome" "firefox" "safari")
   RANDOM_BROWSER="${browser_options[$RANDOM % ${#browser_options[@]}]}"
-  export ENDTEST_OPTIONS="&platform=$RANDOM_PLATFORM&os=bigsur&browser=$RANDOM_BROWSER&browserVersion=latest"
+  export ENDTEST_OPTIONS="&platform=$RANDOM_PLATFORM&os=bigsur&browser=$RANDOM_BROWSER&browserVersion=latest&geolocation=sanfrancisco&cases=all&notes="
   echo "ENDTEST_OPTIONS=$ENDTEST_OPTIONS" >> $GITHUB_ENV
   exit 0
 fi
