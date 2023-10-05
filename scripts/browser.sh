b60d6a6b4f7419718fda0e0f47a4586e857d4c91
Taylor Murray <taylor@goodparty.org>
2023-10-05 12:24:22 -0700

    fix: load var into github_env

--- scripts/browser.sh
+++ scripts/browser.sh
@@ -2,4 +2,4 @@
 options=("chrome" "firefox" "edge" "safari")
 RANDOM_CHOICE="${options[$RANDOM % ${#options[@]}]}"
 export BROWSER="$RANDOM_CHOICE"
-echo "$BROWSER"
+echo "BROWSER=$BROWSER" >> $GITHUB_ENV
