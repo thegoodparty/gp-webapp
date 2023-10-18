76ac8086e3c178e6ab4689527a57a4116e9b5787
tomer-tgp <tomer@thegoodparty.org>
2023-10-18 12:24:17 -0700

    feat: graduate section

--- app/(landing)/academy-webinar/components/WebinarPage.js
+++ app/(landing)/academy-webinar/components/WebinarPage.js
@@ -23,7 +23,7 @@ export default function WebinarPage() {
       {/* <Sessions /> */}
       <Curriculum />
       <Dates />
-      {/* <Graduates /> */}
+      <Graduates />
       <GraduateSpotlight />
     </>
   );
