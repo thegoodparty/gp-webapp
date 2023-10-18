50d996d9d7e45ec38a14494acea6edf3a8d6d9ee
tomer-tgp <tomer@thegoodparty.org>
2023-10-18 08:49:34 -0700

    feat: graduate links

--- app/(landing)/academy-webinar/components/GraduateSpotlight.js
+++ app/(landing)/academy-webinar/components/GraduateSpotlight.js
@@ -4,22 +4,26 @@ import BreannaImg from 'public/images/landing-pages/breanna.png';
 import CarlosImg from 'public/images/landing-pages/carlos.png';
 import Image from 'next/image';
 import MaxWidth from '@shared/layouts/MaxWidth';
+import Link from 'next/link';
 
 const graduates = [
   {
     name: 'Carlos Rousselin',
     desc: 'Carlos Rousselin, a pharmaceutical scientist from Nashville, Tennessee, began wondering whether he should run for local office after learning about Good Party Academy.',
     img: CarlosImg,
+    link: 'https://goodparty.org/blog/article/gpa-grad-carlos-rousselin',
   },
   {
     name: 'Breanna Stott',
     desc: 'After discovering GPA on Instagram, Stott connected with the vision of Good Party — working to make people matter more than money in our democracy.',
     img: BreannaImg,
+    link: 'https://goodparty.org/blog/article/gpa-grad-breanna-stott',
   },
   {
     name: 'Victoria Masika',
     desc: 'Since announcing her run in early July, Masika has become one of the first candidates in North Carolina to affiliate with the Forward Party.',
     img: VictoriaImg,
+    link: 'https://goodparty.org/blog/article/race-update-victoria-masika',
   },
 ];
 
@@ -32,19 +36,30 @@ export default function GraduateSpotlight() {
         </MarketingH2>
         <div className="grid grid-cols-12 gap-6 md:gap-12">
           {graduates.map((graduate) => (
-            <div key={graduate.name} className="col-span-12 md:col-span-4">
-              <Image
-                src={graduate.img}
-                alt={graduate.name}
-                width={400}
-                height={400}
-                className="rounded-2xl w-full h-auto"
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              />
-              <div className="text-2xl font-medium mt-5 mb-2">
-                {graduate.name}
+            <div
+              key={graduate.name}
+              className="col-span-12 md:col-span-4 h-full flex flex-col justify-between"
+            >
+              <div>
+                <Image
+                  src={graduate.img}
+                  alt={graduate.name}
+                  width={400}
+                  height={400}
+                  className="rounded-2xl w-full h-auto"
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                />
+                <div className="text-2xl font-medium mt-5 mb-2">
+                  {graduate.name}
+                </div>
+                <div className="text-lg ">{graduate.desc}</div>
               </div>
-              <div className="text-lg ">{graduate.desc}</div>
+              <Link
+                href={graduate.link}
+                className="mt-4 text-purple-400 font-bold"
+              >
+                Read More
+              </Link>
             </div>
           ))}
         </div>
