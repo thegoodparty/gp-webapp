e0e8b39c7a4f58e9e59fb2c53bcf2e9e509b1c1b
tomer-tgp <tomer@thegoodparty.org>
2023-11-02 08:55:28 -0700

    chore: deploy

--- app/(company)/elections/[...params]/components/ElectionPage.js
+++ app/(company)/elections/[...params]/components/ElectionPage.js
@@ -14,7 +14,7 @@ export default function ElectionPage(props) {
   const [showModal, setShowModal] = useState(false);
 
   const { city } = props;
-  console.log('city', city);
+  console.log('city1', city);
 
   const handleOpenModal = () => {
     setShowModal(true);
