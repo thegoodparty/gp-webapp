106baf8c6c89adbaae3c467761b2cd2a8254d927
tomer-tgp <tomer@thegoodparty.org>
2023-11-22 11:23:10 -0500

    fix: tags vertical padding

--- app/blog/article/[slug]/components/ArticleTags.js
+++ app/blog/article/[slug]/components/ArticleTags.js
@@ -8,7 +8,7 @@ export default function ArticleTags({ tags }) {
   }
   return (
     <div>
-      <H3 className="my-3">Tags</H3>
+      <H3 className="my-3 mb-3">Tags</H3>
       {tags.map((tag) => (
         <Link href={`/blog/tag/${tag.slug}`} key={tag.slug}>
           <div className="bg-yellow px-3 py-2 text-sm rounded-full mr-2 inline-block font-medium cursor-pointer transition-shadow hover:shadow-md">
