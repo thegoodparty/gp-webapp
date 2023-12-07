2413fae3de4f29fa97d2fae9ac37b9542c1129a7
tomer-tgp <tomer@thegoodparty.org>
2023-12-06 22:22:50 -0800

    fix: metadata

--- app/(landing)/get-a-demo/page.js
+++ app/(landing)/get-a-demo/page.js
@@ -7,8 +7,8 @@ const meta = pageMetaData({
   description:
     "Book a brief 1:1 meeting with our team to learn about Good Party's free tools for independent and third-party candidates. We'll cover your campaign needs, our tool's functionality. and show you the tool in action.",
 
-  slug: '/info-session',
-  image: 'https://assets.goodparty.org/academy-intro.jpg',
+  slug: '/get-a-demo',
+  image: 'https://assets.goodparty.org/get-a-demo.png',
 });
 export const metadata = meta;
 
--- app/(landing)/info-session/page.js
+++ app/(landing)/info-session/page.js
@@ -7,7 +7,7 @@ const meta = pageMetaData({
   description:
     "Book a brief 1:1 meeting with our team to learn about getting involved with Good Party and our theory of change. We'll uncover how your unique skills, background, and passion can help advance the movement to elect more independent candidates!",
   slug: '/info-session',
-  image: 'https://assets.goodparty.org/academy-intro.jpg',
+  image: 'https://assets.goodparty.org/info-session.png',
 });
 export const metadata = meta;
 
