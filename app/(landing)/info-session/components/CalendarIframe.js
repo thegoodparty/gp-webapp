d117dae11ead2809300c634c0df4932eb600e5d7
tomer-tgp <tomer@thegoodparty.org>
2023-12-08 11:01:08 -0800

    chore: WEB-1373

--- app/(landing)/info-session/components/CalendarIframe.js
+++ app/(landing)/info-session/components/CalendarIframe.js
@@ -1,7 +1,7 @@
 export default function CalendarIframe() {
   return (
     <iframe
-      src="https://meetings.hubspot.com/colton-hess/volunteer-interest-chat?embed=true"
+      src="https://meetings.hubspot.com/quinn-mccully/volunteer-intro-chat?embed=true"
       width="100%"
       height="620"
       title="Book a Meeting"
