e88e9e217f423d7d9dabe8611fe6a0d94504fda1
tomer-tgp <tomer@goodparty.org>
2024-02-07 12:23:30 -0800

    fix: refresh from questions

--- app/(candidate)/dashboard/questions/components/Done.js
+++ app/(candidate)/dashboard/questions/components/Done.js
@@ -1,15 +1,14 @@
 import PrimaryButton from '@shared/buttons/PrimaryButton';
 import Confetti from './Confetti';
 import H1 from '@shared/typography/H1';
-import Link from 'next/link';
 
 export default function Done() {
   return (
     <div className="flex flex-col h-[calc(100vh-216px)] items-center justify-center">
       <H1 className="text-center">You&apos;re all set!</H1>
-      <Link href="/dashboard/plan" className="mt-10 block">
+      <a href="/dashboard/plan" className="mt-10 block">
         <PrimaryButton>Back to Dashboard</PrimaryButton>
-      </Link>
+      </a>
       <Confetti button={<PrimaryButton>Done</PrimaryButton>} />
     </div>
   );
