b44ce45846e94304a7ec8c9b1a1946fa4d770b6f
tomer-tgp <tomer@goodparty.org>
2024-02-17 07:47:27 -0800

    feat: WEB-1580

--- app/(candidate)/dashboard/questions/components/issues/IssuesSelector.js
+++ app/(candidate)/dashboard/questions/components/issues/IssuesSelector.js
@@ -4,7 +4,13 @@ import Tabs from '@shared/utils/Tabs';
 import { useState } from 'react';
 import IssuesList from './IssuesList';
 export default function IssuesSelector(props) {
-  const { completeCallback, standaloneMode, updatePositionsCallback } = props;
+  const {
+    completeCallback,
+    standaloneMode,
+    updatePositionsCallback,
+    candidatePositions,
+    campaign,
+  } = props;
   const [tab, setTab] = useState(0);
   const labels = [
     <H6 key="issue1">Issue One</H6>,
@@ -18,7 +24,13 @@ export default function IssuesSelector(props) {
         updatePositionsCallback();
       }
     } else {
-      completeCallback('issues');
+      const combinedIssuedCount =
+        (candidatePositions?.length || 0) +
+        (campaign?.customIssues?.length || 0);
+      updatePositionsCallback();
+      if (combinedIssuedCount > 2) {
+        completeCallback('issues');
+      }
     }
   };
 
