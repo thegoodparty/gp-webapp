60b69e0ef4dc86f218aab06c304db9c07af77182
tomer-tgp <tomer@goodparty.org>
2024-02-27 09:58:24 -0800

    chore: ppr config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const withPWA = require('next-pwa')({
 });
 
 const nextConfig = {
+  experimental: {
+    ppr: true, // https://nextjs.org/docs/app/api-reference/next-config-js/partial-prerendering
+  },
   reactStrictMode: true,
   transpilePackages: ['ui'],
   images: {
