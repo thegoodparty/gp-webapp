b73f27ae034a9f59aca4def0eaa07d07c1460e44
tomer-tgp <tomer@goodparty.org>
2024-02-29 22:11:44 -0800

    chore: WEB-1655

--- app/shared/layouts/CookiesSnackbar.js
+++ app/shared/layouts/CookiesSnackbar.js
@@ -3,12 +3,14 @@
 import ErrorButton from '@shared/buttons/ErrorButton';
 import { getCookie, setCookie } from 'helpers/cookieHelper';
 import { useEffect, useState } from 'react';
+import { isbot } from 'isbot';
 
 export default function CookiesSnackbar() {
   const [showBanner, setShowBanner] = useState(false);
   useEffect(() => {
     const cookie = getCookie('cookiesAccepted');
-    if (!cookie) {
+    const isBot = isbot(navigator.userAgent);
+    if (!cookie && !isBot) {
       setShowBanner(true);
     }
   }, []);
@@ -21,16 +23,14 @@ export default function CookiesSnackbar() {
   };
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-primary text-white p-4 text-center">
-      <p>
-        We use cookies to personalize content, analyze traffic, and provide you
-        with a better user experience. By continuing to browse this site, you
-        consent to the use of cookies.
-        <div className="mt-6" onClick={handleAccept}>
-          <ErrorButton className="ml-4" size="medium">
-            Accept
-          </ErrorButton>
-        </div>
-      </p>
+      We use cookies to personalize content, analyze traffic, and provide you
+      with a better user experience. By continuing to browse this site, you
+      consent to the use of cookies.
+      <div className="mt-6" onClick={handleAccept}>
+        <ErrorButton className="ml-4" size="medium">
+          Accept
+        </ErrorButton>
+      </div>
     </div>
   );
 }
