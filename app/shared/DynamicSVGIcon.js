a92e0b1926a1c185ed73bd685d5821d39e02ddc7
Matthew Marcus <lurchbulldog@gmail.com>
2024-03-19 13:44:30 -0500

    [WEB-1450] - A few more changes from https://github.com/thegoodparty/gp-webapp/pull/122#pullrequestreview-1945139940

--- app/shared/DynamicSVGIcon.js
+++ app/shared/DynamicSVGIcon.js
@@ -4,12 +4,7 @@ import React from 'react';
 export const DynamicSVGIcon = ({
   svgData,
   className,
-}) => {
-  console.log(`svgData =>`, svgData)
-  return <InlineSVG {...{
-    src: svgData,
-    className: `relative block w-[2.5rem] h-[2.5rem]${
-      className ? ` ${className}` : ''
-    }`,
-  }} />;
-};
+}) => <InlineSVG
+  src={svgData}
+  className={`relative block w-[2.5rem] h-[2.5rem]${className ? ` ${className}` : ''}`}
+/>
