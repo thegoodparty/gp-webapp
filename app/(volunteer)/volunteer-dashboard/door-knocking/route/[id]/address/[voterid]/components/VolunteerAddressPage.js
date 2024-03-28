22edbbf9cf826195ced7d8345f6f705794959ed5
tomer-tgp <tomer@goodparty.org>
2024-03-27 23:39:33 -0700

    fix: voterid

--- app/(volunteer)/volunteer-dashboard/door-knocking/route/[id]/address/[voterid]/components/VolunteerAddressPage.js
+++ app/(volunteer)/volunteer-dashboard/door-knocking/route/[id]/address/[voterid]/components/VolunteerAddressPage.js
@@ -1,19 +1,18 @@
+import TitleSection from './TitleSection';
+import VoterInfo from './VoterInfo';
+import TabsSection from './TabsSection';
 import VolunteerDashboardLayout from 'app/(volunteer)/volunteer-dashboard/shared/VolunteerDashboardLayout';
-// import TitleSection from './TitleSection';
-// import VoterInfo from './VoterInfo';
-// import TabsSection from './TabsSection';
 
 export default function VolunteerAddressPage(props) {
   return (
     <VolunteerDashboardLayout {...props}>
-      {/* <div className=" bg-white">
+      <div className=" bg-white">
         <TitleSection {...props} />
         <VoterInfo {...props} />
       </div>
       <div className="p-4">
         <TabsSection {...props} />
-      </div> */}
-      hi
+      </div>
     </VolunteerDashboardLayout>
   );
 }
