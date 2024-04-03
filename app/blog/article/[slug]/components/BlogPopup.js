f5a3e6f3a36fe3465b1c17df8a11b3e45ed160f0
Matthew Marcus <lurchbulldog@gmail.com>
2024-04-03 16:01:37 -0500

    [WEB-1879] - Removes the A/B testing logic and sets modal to center of display

--- app/blog/article/[slug]/components/BlogPopup.js
+++ app/blog/article/[slug]/components/BlogPopup.js
@@ -1,15 +1,13 @@
 'use client';
 import { useEffect, useState } from 'react';
-import HubSpotForm from '@shared/utils/HubSpotForm';
 import SignupForm from '@shared/inputs/SignupForm';
-import ModalCorner from '@shared/utils/ModalCorner';
 import Image from 'next/image';
 import { setCookie, getCookie } from 'helpers/cookieHelper.js';
 import Modal from '@shared/utils/Modal';
 
+const POPUP_DELAY = 30000;
 export default function BlogPopup() {
   const [showModal, setShowModal] = useState(false);
-  const [testVariant, setTestVariant] = useState(false);
 
   const handleOpenModal = () => {
     setShowModal(true);
@@ -17,39 +15,19 @@ export default function BlogPopup() {
 
   useEffect(() => {
     const cookie = getCookie('blogPopup');
-    const timerTest = setTimeout(() => {
-      if (window.gpBlogPopupTest) {
-        //this is setup as a js variable in blog test on VWO
-        setTestVariant(true);
-      }
-    }, 500);
     if (!cookie || cookie !== 'closed') {
-      const timer = setTimeout(handleOpenModal, 30000);
+      const timer = setTimeout(handleOpenModal, POPUP_DELAY);
       return () => {
         clearTimeout(timer);
-        clearTimeout(timerTest);
       };
     }
   }, []);
 
-  useEffect(() => {
-    if (window.gpBlogPopupTest) {
-      setTestVariant(true);
-    }
-  }, []);
-
-  const WrapperElement = (props) => {
-    if (testVariant) {
-      return (
-        <Modal {...props}>
-          <div className=" w-[90vw] max-w-[420px] mx-auto">
-            {props.children}
-          </div>
-        </Modal>
-      );
-    }
-    return <ModalCorner {...props}>{props.children}</ModalCorner>;
-  };
+  const WrapperElement = (props) => <Modal {...props}>
+    <div className=" w-[90vw] max-w-[420px] mx-auto">
+      {props.children}
+    </div>
+  </Modal>
 
   return (
     <WrapperElement
@@ -69,7 +47,6 @@ export default function BlogPopup() {
       <h2 className="text-2xl font-black my-1">
         Stay up to date with Good Party
       </h2>
-      {/* <HubSpotForm formId="5d84452a-01df-422b-9734-580148677d2c" /> */}
 
       <SignupForm
         formId="5d84452a-01df-422b-9734-580148677d2c"
