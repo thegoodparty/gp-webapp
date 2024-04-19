8439a12fb0bcdde0898dda7272b0b45992e95ada
Matthew Marcus <lurchbulldog@gmail.com>
2024-04-19 13:11:59 -0500

    [WEB-1855] - https://github.com/thegoodparty/gp-webapp/pull/156#discussion_r1572663938

--- app/(company)/about/components/BuildWithUsGridItems.js
+++ app/(company)/about/components/BuildWithUsGridItems.js
@@ -1,6 +1,5 @@
 import Image from 'next/image';
-import Link from 'next/link';
-import { BsFillArrowUpRightCircleFill } from 'react-icons/bs';
+import { BuildWithUsCard } from './BuildWithUsCard';
 
 const BUILD_WITH_US_CARDS = [
   {
@@ -22,31 +21,6 @@ const BUILD_WITH_US_CARDS = [
     linkText: 'Volunteer with us'
   }
 ]
-
-const BuildWithUsCard = ({
-  header,
-  blurb,
-  href,
-  linkText,
-}) => <section className="rounded-3xl
-  bg-tertiary-background
-  p-5
-  mb-4
-  last:mb-0
-  h-[252px]
-  md:mb-0
-  md:h-full">
-  <div className="relative h-full">
-    <h3
-      className="text-tertiary-dark text-2xl font-medium leading-8 mb-4">{header}</h3>
-    <p>{blurb}</p>
-    <Link className="flex items-center text-tertiary-dark absolute bottom-0"
-          href={href}>
-      <BsFillArrowUpRightCircleFill
-        className="inline mr-2 h-8 w-8" /><span>{linkText}</span>
-    </Link>
-  </div>
-</section>;
 
 const BuildWithUsImage = ({ src, alt, className }) => <Image
   className={`w-full rounded-3xl mb-4 last:mb-0 md:mb-0 ${className ? className : ''}`}
