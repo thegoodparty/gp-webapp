a4c2aa64f1dd773bba4d947779cf1e92fd54c96e
tomer-tgp <tomer@goodparty.org>
2024-05-14 18:40:25 -0400

    feat: temp script

--- app/(volunteer)/volunteer-dashboard/door-knocking/[dkslug]/route/[id]/address/[voterid]/components/TabsSection.js
+++ app/(volunteer)/volunteer-dashboard/door-knocking/[dkslug]/route/[id]/address/[voterid]/components/TabsSection.js
@@ -3,16 +3,19 @@ import Tabs from '@shared/utils/Tabs';
 import { useState } from 'react';
 import CandidateInfo from './CandidateInfo';
 import Surveys from './Surveys';
+import TempScript from './TempScript';
 
 export default function TabsSection(props) {
   const [tab, setTab] = useState(0);
   const labels = [
     <div key="issues">Voter Issues</div>,
+    <div key="script">Script</div>,
     <div key="info">Candidate Info</div>,
   ];
 
   const panels = [
     <Surveys {...props} key="issues-tab" />,
+    <TempScript {...props} key="script-tab" />,
     <CandidateInfo {...props} key="info-tab" />,
   ];
   const changeTabCallback = (index) => {
