369fd184d61d13956f11fbbf1e120c417fa09512
Matthew Marcus <lurchbulldog@gmail.com>
2024-05-22 17:19:38 -0500

    [WEB-2088] - Team page clean up (#176)
    * [WEB-2088] - fixed carousel
    
    * [WEB-2088] - Applied changes for widths and image shape
    
    * [WEB-2088] - Fixed remaining widths

--- app/(company)/team/components/LeadingTheMovement.js
+++ app/(company)/team/components/LeadingTheMovement.js
@@ -1,10 +1,10 @@
 import { LeadingActivityCards } from 'app/(company)/team/components/LeadingActivityCards';
 
 const LeadingTheMovement = () => (
-  <section className="px-4 py-8 lg:p-24">
-    <h2 className="text-4xl lg:text-6xl leading-tight mb-8 font-medium">
+  <section className="py-8 lg:py-24">
+    <h2 className="text-4xl md:text-6xl leading-tight mb-8 font-medium">
       <span>Leading the </span>
-      <br className="lg:hidden" />
+      <br className="md:hidden" />
       <span className="text-tertiary">movement</span>
     </h2>
     <LeadingActivityCards />
