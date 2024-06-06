e207335c0f56e6ac90074bf769f9571cf1065108
tomer-tgp <tomer@goodparty.org>
2024-06-06 11:55:04 -0700

    fix: move maxDuration to page

--- app/admin/campaign-statistics/page.js
+++ app/admin/campaign-statistics/page.js
@@ -5,6 +5,8 @@ import gpApi from 'gpApi';
 import { getServerToken } from 'helpers/userServerHelper';
 import gpFetch from 'gpApi/gpFetch';
 
+export const maxDuration = 60 * 5;
+
 const stripEmptyFilters = (filters) =>
   Object.keys(filters).reduce((acc, key) => {
     return {
