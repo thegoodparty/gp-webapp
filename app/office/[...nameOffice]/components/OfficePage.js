61b94085e11010427f6ec8dc567a24a2f9b3b56c
tomer-tgp <tomer@goodparty.org>
2024-06-09 17:46:47 -0400

    chore: what is happening??

--- app/office/[...nameOffice]/components/OfficePage.js
+++ app/office/[...nameOffice]/components/OfficePage.js
@@ -1,14 +1,14 @@
-import HelpBanner from './HelpBanner';
-// import HeroImg from './HeroImg';
+// import HelpBanner from './HelpBanner';
+import HeroImg from './HeroImg';
 // import CandidateCard from './CandidateCard';
 // import ContentSection from './ContentSection';
 
 export default function OfficePage(props) {
   return (
     <div className="bg-black text-white">
-      <HelpBanner {...props} />
-      {/* <HeroImg />
-      <div className="max-w-[1440px] mx-auto px-4 xl:p-0 mt-4">
+      {/* <HelpBanner {...props} /> */}
+      <HeroImg />
+      {/* <div className="max-w-[1440px] mx-auto px-4 xl:p-0 mt-4">
         <div className="grid grid-cols-12 gap-4">
           <div className="col-span-12 lg:col-span-3">
             <CandidateCard {...props} />
