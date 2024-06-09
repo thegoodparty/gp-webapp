68704ce44d4193ecf21c6ed3b2652ecbba5c6126
tomer-tgp <tomer@goodparty.org>
2024-06-09 18:02:21 -0400

    chore: no image

--- app/office/[...nameOffice]/components/OfficePage.js
+++ app/office/[...nameOffice]/components/OfficePage.js
@@ -1,14 +1,14 @@
 // import HelpBanner from './HelpBanner';
 import HeroImg from './HeroImg';
-// import CandidateCard from './CandidateCard';
-// import ContentSection from './ContentSection';
+import CandidateCard from './CandidateCard';
+import ContentSection from './ContentSection';
 
 export default function OfficePage(props) {
   return (
     <div className="bg-black text-white">
       {/* <HelpBanner {...props} /> */}
-      <HeroImg />
-      {/* <div className="max-w-[1440px] mx-auto px-4 xl:p-0 mt-4">
+      {/* <HeroImg /> */}
+      <div className="max-w-[1440px] mx-auto px-4 xl:p-0 mt-4">
         <div className="grid grid-cols-12 gap-4">
           <div className="col-span-12 lg:col-span-3">
             <CandidateCard {...props} />
@@ -21,7 +21,7 @@ export default function OfficePage(props) {
         </div>
         <div id="candidate-footer" className="mb-4"></div>
         <div>&nbsp;</div>
-      </div> */}
+      </div>
     </div>
   );
 }
