b7e9fdb8bcb05eff29ce9b9cd53409523478ceb8
tomer-tgp <tomer@goodparty.org>
2024-06-09 16:51:42 -0400

    chore: debug1

--- app/candidate/[...nameOffice]/components/CandidatePage.js
+++ app/candidate/[...nameOffice]/components/CandidatePage.js
@@ -1,13 +1,13 @@
 import HelpBanner from './HelpBanner';
-import HeroImg from './HeroImg';
-import CandidateCard from './CandidateCard';
-import ContentSection from './ContentSection';
+// import HeroImg from './HeroImg';
+// import CandidateCard from './CandidateCard';
+// import ContentSection from './ContentSection';
 
 export default function CandidatePage(props) {
   return (
     <div className="bg-black text-white">
       <HelpBanner {...props} />
-      <HeroImg />
+      {/* <HeroImg />
       <div className="max-w-[1440px] mx-auto px-4 xl:p-0 mt-4">
         <div className="grid grid-cols-12 gap-4">
           <div className="col-span-12 lg:col-span-3">
@@ -21,7 +21,7 @@ export default function CandidatePage(props) {
         </div>
         <div id="candidate-footer" className="mb-4"></div>
         <div>&nbsp;</div>
-      </div>
+      </div> */}
     </div>
   );
 }
