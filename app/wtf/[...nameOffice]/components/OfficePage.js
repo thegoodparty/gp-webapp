bb9a4dbaae23d2fb86a83b7a8323f82ac7d06580
tomer-tgp <tomer@goodparty.org>
2024-06-09 20:18:26 -0400

    chore: add candidate card

--- app/wtf/[...nameOffice]/components/OfficePage.js
+++ app/wtf/[...nameOffice]/components/OfficePage.js
@@ -1,6 +1,6 @@
 // import HelpBanner from './HelpBanner';
 // import HeroImg from './HeroImg';
-// import CandidateCard from './CandidateCard';
+import CandidateCard from './CandidateCard';
 // import ContentSection from './ContentSection';
 
 export default function OfficePage(props) {
@@ -11,7 +11,7 @@ export default function OfficePage(props) {
       <div className="max-w-[1440px] mx-auto px-4 xl:p-0 mt-4">
         <div className="grid grid-cols-12 gap-4">
           <div className="col-span-12 lg:col-span-3">
-            {/* <CandidateCard {...props} /> */}1
+            <CandidateCard {...props} />
           </div>
           <div className="col-span-12 lg:col-span-9">
             <div>{/* <ContentSection {...props} /> */}2</div>
