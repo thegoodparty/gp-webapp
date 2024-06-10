097467f4e7b00b27b0f702a15b9b761ad251e3f4
tomer-tgp <tomer@goodparty.org>
2024-06-09 22:50:38 -0400

    chore: img

--- app/wtf/[...nameOffice]/components/OfficePage.js
+++ app/wtf/[...nameOffice]/components/OfficePage.js
@@ -1,5 +1,5 @@
 // import HelpBanner from './HelpBanner';
-// import HeroImg from './HeroImg';
+import HeroImg from './HeroImg';
 import CandidateCard from './CandidateCard';
 // import ContentSection from './ContentSection';
 
@@ -7,7 +7,7 @@ export default function OfficePage(props) {
   return (
     <div className="bg-black text-white">
       {/* <HelpBanner {...props} /> */}
-      {/* <HeroImg /> */}
+      <HeroImg />
       <div className="max-w-[1440px] mx-auto px-4 xl:p-0 mt-4">
         <div className="grid grid-cols-12 gap-4">
           <div className="col-span-12 lg:col-span-3">
