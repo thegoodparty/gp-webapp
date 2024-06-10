e5a2b1f0b931c8fc5215f0fa1a901f2a2ffc10c7
tomer-tgp <tomer@goodparty.org>
2024-06-09 21:31:17 -0400

    chore: was is client issue

--- app/wtf/[...nameOffice]/components/CandidateCard.js
+++ app/wtf/[...nameOffice]/components/CandidateCard.js
@@ -12,8 +12,8 @@ import {
 import { IoPersonSharp } from 'react-icons/io5';
 import { MdEmail, MdStars } from 'react-icons/md';
 import TealButton from './TealButton';
-// import StickyCard from './StickyCard';
-// import CTA from './CTA';
+import StickyCard from './StickyCard';
+import CTA from './CTA';
 
 export default function CandidateCard(props) {
   const { candidate } = props;
@@ -25,61 +25,61 @@ export default function CandidateCard(props) {
   }
 
   return (
-    // <StickyCard>
-    <div className=" bg-primary-dark p-6 rounded-2xl border border-gray-700">
-      <div className="flex justify-between">
-        <div className="bg-primary inline-block border border-white rounded-2xl p-4">
-          <IoPersonSharp size={60} />
-        </div>
-        <div>
-          <div className="bg-secondary-light text-black py-2 px-4 rounded">
-            <Overline>UNCLAIMED</Overline>
+    <StickyCard>
+      <div className=" bg-primary-dark p-6 rounded-2xl border border-gray-700">
+        <div className="flex justify-between">
+          <div className="bg-primary inline-block border border-white rounded-2xl p-4">
+            <IoPersonSharp size={60} />
+          </div>
+          <div>
+            <div className="bg-secondary-light text-black py-2 px-4 rounded">
+              <Overline>UNCLAIMED</Overline>
+            </div>
           </div>
         </div>
-      </div>
-      <Overline className="my-4">{partyName}</Overline>
-      <H1 className="mb-4">
-        {firstName} {lastName}
-      </H1>
-      <div className="flex mb-3 items-center">
-        <MdStars className="text-secondary-light" size={20} />
-        <H5 className="ml-2">{party}</H5>
-      </div>
-      <div className="flex mb-3 items-center">
-        <IoPersonSharp className="text-secondary-light" size={20} />
-        <H5 className="ml-2">Running for {office}</H5>
-      </div>
-      <div className="flex mb-3 items-center">
-        <FaMapMarkerAlt className="text-secondary-light" size={20} />
-        <H5 className="ml-2">
-          {city}, {state}
-        </H5>
-      </div>
-      <Body1 className="mt-7 mb-8">
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium doloremque laudantium.
-      </Body1>
-
-      <div className=" flex justify-between items-center opacity-50 mb-8">
-        <MdEmail size={20} />
-        <FaGlobeAmericas size={20} />
-        <FaXTwitter size={20} />
-        <FaFacebookF size={20} />
-        <FaInstagram size={20} />
-        <FaTiktok size={20} />
-      </div>
-      <div className="p-3 text-center rounded border border-gray-300 font-medium cursor-pointer transition-colors hover:bg-white hover:text-primary mb-4">
-        Learn More About {firstName} {lastName}
-      </div>
-      {/* <CTA> */}
-      <TealButton>
-        <div className="flex items-center justify-center  ">
-          <div className="mr-1">Claim this profile</div>
-          <FaArrowRight />
+        <Overline className="my-4">{partyName}</Overline>
+        <H1 className="mb-4">
+          {firstName} {lastName}
+        </H1>
+        <div className="flex mb-3 items-center">
+          <MdStars className="text-secondary-light" size={20} />
+          <H5 className="ml-2">{party}</H5>
         </div>
-      </TealButton>
-      {/* </CTA> */}
-    </div>
-    // </StickyCard>
+        <div className="flex mb-3 items-center">
+          <IoPersonSharp className="text-secondary-light" size={20} />
+          <H5 className="ml-2">Running for {office}</H5>
+        </div>
+        <div className="flex mb-3 items-center">
+          <FaMapMarkerAlt className="text-secondary-light" size={20} />
+          <H5 className="ml-2">
+            {city}, {state}
+          </H5>
+        </div>
+        <Body1 className="mt-7 mb-8">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium.
+        </Body1>
+
+        <div className=" flex justify-between items-center opacity-50 mb-8">
+          <MdEmail size={20} />
+          <FaGlobeAmericas size={20} />
+          <FaXTwitter size={20} />
+          <FaFacebookF size={20} />
+          <FaInstagram size={20} />
+          <FaTiktok size={20} />
+        </div>
+        <div className="p-3 text-center rounded border border-gray-300 font-medium cursor-pointer transition-colors hover:bg-white hover:text-primary mb-4">
+          Learn More About {firstName} {lastName}
+        </div>
+        <CTA>
+          <TealButton>
+            <div className="flex items-center justify-center  ">
+              <div className="mr-1">Claim this profile</div>
+              <FaArrowRight />
+            </div>
+          </TealButton>
+        </CTA>
+      </div>
+    </StickyCard>
   );
 }
--- app/wtf/[...nameOffice]/components/OfficePage.js
+++ app/wtf/[...nameOffice]/components/OfficePage.js
@@ -1,20 +1,21 @@
-// import HelpBanner from './HelpBanner';
-// import HeroImg from './HeroImg';
+'use client';
+import HelpBanner from './HelpBanner';
+import HeroImg from './HeroImg';
 import CandidateCard from './CandidateCard';
-// import ContentSection from './ContentSection';
+import ContentSection from './ContentSection';
 
 export default function OfficePage(props) {
   return (
     <div className="bg-black text-white">
-      {/* <HelpBanner {...props} /> */}
-      {/* <HeroImg /> */}
+      <HelpBanner {...props} />
+      <HeroImg />
       <div className="max-w-[1440px] mx-auto px-4 xl:p-0 mt-4">
         <div className="grid grid-cols-12 gap-4">
           <div className="col-span-12 lg:col-span-3">
-            <CandidateCard {...props} />
+            <CandidateCard {...props} />1
           </div>
           <div className="col-span-12 lg:col-span-9">
-            <div>{/* <ContentSection {...props} /> */}2</div>
+            <ContentSection {...props} />
           </div>
         </div>
         <div id="candidate-footer" className="mb-4"></div>
