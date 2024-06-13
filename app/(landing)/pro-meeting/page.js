93a57c57649cec6205bea5aaa0e409dedc170d25
tomer-tgp <tomer@goodparty.org>
2024-06-13 11:05:31 -0400

    feat: WEB-2216 pro-meeting

---
+++ app/(landing)/pro-meeting/page.js
@@ -0,0 +1,15 @@
+import ProMeetingPage from './components/ProMeetingPage';
+import pageMetaData from 'helpers/metadataHelper';
+
+const meta = pageMetaData({
+  title: 'Upgrade to Pro',
+  description: 'Request to meet with our team to upgrade to GoodParty.org Pro',
+
+  slug: '/pro-consultation',
+  image: 'https://assets.goodparty.org/get-a-demo.png',
+});
+export const metadata = meta;
+
+export default async function Page() {
+  return <ProMeetingPage />;
+}
