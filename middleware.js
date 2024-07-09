b08d7d13a996160f083a3521325da1c42a3cbacc
tomer-tgp <tomer@goodparty.org>
2024-07-09 15:46:11 -0400

    chore: move redirects back to middleware

--- middleware.js
+++ middleware.js
@@ -1,8 +1,11 @@
 import { NextResponse } from 'next/server';
-import { getRedirects } from 'helpers/getRedirects';
 import { handlePathRedirect } from 'helpers/handlePathRedirect';
 // import { handlePathCapitalizationRedirect } from 'helpers/handlePathCapitalizationRedirect';
 import { handleApiRequestRewrite } from 'helpers/handleApiRequestRewrite';
+import gpFetch from 'gpApi/gpFetch';
+
+let dbRedirects;
+let dbFetchTime;
 
 export default async function middleware(req) {
   const redirectPaths = await getRedirects();
@@ -38,3 +41,45 @@ export const config = {
 // export const config = {
 //   matcher: ['/((?!api|_next/static|_next/image|images|favicon.ico).*)'],
 // }
+
+const getRedirects = async () => {
+  console.log('getRedirects');
+  if (!dbRedirects) {
+    console.log('getRedirects2');
+    // only call dbRedirect if it is not defined or once an hour
+    const res = await fetchRedirects();
+    dbRedirects = res.content;
+    dbFetchTime = Date.now();
+    console.log('getRedirects3', res);
+  } else {
+    if (!dbFetchTime || Date.now() - dbFetchTime > 10 * 3600000) {
+      dbFetchTime = Date.now();
+      console.log('getRedirects4');
+      const res = await fetchRedirects();
+      console.log('getRedirects5', res);
+      dbRedirects = res.content;
+    }
+  }
+  console.log('getRedirects6', dbRedirects);
+  return dbRedirects;
+};
+
+const fetchRedirects = async () => {
+  try {
+    const api = {
+      url: `${apiBase}/api/v1/content/content-by-key`,
+      method: 'GET',
+    };
+
+    console.log('fetchRedirects api', api);
+    const payload = {
+      key: 'redirects',
+    };
+    const res = await gpFetch(api, payload, 3600);
+    console.log('fetchRedirects', res);
+    return res;
+  } catch (e) {
+    console.log('fetchRedirects error', e);
+    return { content: {} };
+  }
+};
