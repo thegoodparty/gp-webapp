be5147e3acdebc8fcd8b25b59533544d85553cde
Matthew Marcus <lurchbulldog@gmail.com>
2024-07-16 22:02:36 -0500

    [WEB-2527] - Acknowledgement questions auto-scrolling (#205)
    * [WEB-2527] - Acknowledgement questions auto-scrolling
    
    * [WEB-2527] - cleanup

--- app/shared/acknowledgements/AcknowledgementQuestion.js
+++ app/shared/acknowledgements/AcknowledgementQuestion.js
@@ -1,9 +1,11 @@
+'use client';
 import CmsContentWrapper from '@shared/content/CmsContentWrapper';
 import SuccessButton from '@shared/buttons/SuccessButton';
 import { FaCheck } from 'react-icons/fa';
 import PrimaryButton from '@shared/buttons/PrimaryButton';
-import { AcknowledgementTitleBar } from '@shared/acknowledgements/AcknowledgementTitleBar';
+import AcknowledgementTitleBar from '@shared/acknowledgements/AcknowledgementTitleBar';
 import { AcknowledgementQuestionBody } from '@shared/acknowledgements/AcknowledgementQuestionBody';
+import { useEffect, useRef, useState } from 'react';
 
 export const AcknowledgementQuestion = ({
   emoticon = <></>,
@@ -13,36 +15,49 @@ export const AcknowledgementQuestion = ({
   show = false,
   acknowledged = false,
   onAcknowledge = () => {},
-}) => (
-  <>
-    <AcknowledgementTitleBar {...{ emoticon, title }} />
-    <AcknowledgementQuestionBody {...{ show }}>
-      <CmsContentWrapper>{body}</CmsContentWrapper>
+  disableScrollTo = false,
+}) => {
+  const [scrolledTo, setScrolledTo] = useState(disableScrollTo);
+  const titleBarRef = useRef(null);
 
-      <div className="flex justify-center mt-8">
-        {acknowledged ? (
-          <div
-            onClick={() => {
-              onAcknowledge(false);
-            }}
-          >
-            <SuccessButton>
-              <div className="flex items-center">
-                <FaCheck />
-                <div className="ml-2">{buttonTexts[1]}</div>
-              </div>
-            </SuccessButton>
-          </div>
-        ) : (
-          <div
-            onClick={() => {
-              onAcknowledge(true);
-            }}
-          >
-            <PrimaryButton>{buttonTexts[0]}</PrimaryButton>
-          </div>
-        )}
-      </div>
-    </AcknowledgementQuestionBody>
-  </>
-);
+  useEffect(() => {
+    if (show && !scrolledTo && titleBarRef.current) {
+      titleBarRef.current.scrollIntoView({ behavior: 'smooth' });
+      setScrolledTo(true);
+    }
+  }, [show]);
+
+  return (
+    <>
+      <AcknowledgementTitleBar {...{ emoticon, title, ref: titleBarRef }} />
+      <AcknowledgementQuestionBody {...{ show }}>
+        <CmsContentWrapper>{body}</CmsContentWrapper>
+
+        <div className="flex justify-center mt-8">
+          {acknowledged ? (
+            <div
+              onClick={() => {
+                onAcknowledge(false);
+              }}
+            >
+              <SuccessButton>
+                <div className="flex items-center">
+                  <FaCheck />
+                  <div className="ml-2">{buttonTexts[1]}</div>
+                </div>
+              </SuccessButton>
+            </div>
+          ) : (
+            <div
+              onClick={() => {
+                onAcknowledge(true);
+              }}
+            >
+              <PrimaryButton>{buttonTexts[0]}</PrimaryButton>
+            </div>
+          )}
+        </div>
+      </AcknowledgementQuestionBody>
+    </>
+  );
+};
