08e8d16ed24eca4cbc23000ec95bbbd4a906512f
tomer-tgp <tomer@goodparty.org>
2024-07-17 10:18:54 -0400

    fix: WEB-2427

--- app/(candidate)/dashboard/voter-records/components/NeedHelp.js
+++ app/(candidate)/dashboard/voter-records/components/NeedHelp.js
@@ -1,4 +1,4 @@
-import { InputLabel, MenuItem, Select } from '@mui/material';
+import { InputLabel, Link, MenuItem, Select } from '@mui/material';
 import PrimaryButton from '@shared/buttons/PrimaryButton';
 import SecondaryButton from '@shared/buttons/SecondaryButton';
 import TextField from '@shared/inputs/TextField';
@@ -80,14 +80,16 @@ export default function NeedHelp() {
             <form noValidate onSubmit={(e) => e.preventDefault()}>
               <div className=" text-center">
                 <H1 className="mb-4">Voter File Help</H1>
-                <Body2>
+                <Body2 className="mb-4">
                   Are you interested in creating a specific audience for your
                   voter file?
                   <br />
-                  Learn what GoodParty.org can help you create.
+                  <Link href="/blog">
+                    Learn what GoodParty.org can help you create.
+                  </Link>
                 </Body2>
               </div>
-              <InputLabel id="type">Voter File Type</InputLabel>
+              <InputLabel id="type">Voter File Type *</InputLabel>
               <Select
                 fullWidth
                 labelId="type"
@@ -97,8 +99,13 @@ export default function NeedHelp() {
                 onChange={(e) => {
                   handleChange('type', e.target.value);
                 }}
+                renderValue={(selected) => {
+                  if (selected.length === 0) {
+                    return <div>Select</div>;
+                  }
+                  return selected;
+                }}
               >
-                <MenuItem value="">Select</MenuItem>
                 {types.map((option) => (
                   <MenuItem value={option} key={option}>
                     {option}
@@ -112,6 +119,7 @@ export default function NeedHelp() {
                   rows={6}
                   placeholder="Tell us a bit about who you are trying to reach. Example: veterans ages 50-60"
                   label="Message"
+                  required
                   InputLabelProps={{
                     shrink: true,
                   }}
