43aef1e4a3ed8139d98f59ca6604c30105dc22bf
tomer-tgp <tomer@goodparty.org>
2024-07-20 11:45:37 -0400

    fix: WEB-2556

--- app/api/v1/entrance/logout/route.js
+++ app/api/v1/entrance/logout/route.js
@@ -2,4 +2,4 @@ import { NextResponse } from 'next/server';
 import { deleteTokenCookie } from 'helpers/tokenCookie';
 
 export const DELETE = async () =>
-  deleteTokenCookie(new NextResponse(null, { status: 204 }));
+  deleteTokenCookie(new NextResponse({ message: 'deleted' }, { status: 200 }));
