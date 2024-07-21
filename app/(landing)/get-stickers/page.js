4ecd55dafa8deca796cb04acdc0569ce34597a35
tomer-tgp <tomer@goodparty.org>
2024-07-20 22:14:12 -0400

    fix: desc

--- app/(landing)/get-stickers/page.js
+++ app/(landing)/get-stickers/page.js
@@ -4,7 +4,7 @@ import GetStickersPage from './components/GetStickersPage';
 const meta = pageMetaData({
   title: 'Get your FREE stickers for free',
   description:
-    "It's time to show that the Not a political party. We're building free tools to change the rules, so good independent candidates can run and win!",
+    "It's time to show that the MAJORITY of us are INDEPENDENTS. Get your FREE GoodParty.org Stickers to #BrightenAmerica",
 
   slug: '/get-stickers',
   image: 'https://assets.goodparty.org/landing/stickers3.png',
