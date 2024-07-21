52f82b70990a37ec200fafc4a9711c3fc00f5644
tomer-tgp <tomer@goodparty.org>
2024-07-20 22:08:59 -0400

    fix: form height

--- app/(landing)/get-stickers/components/FormSection.js
+++ app/(landing)/get-stickers/components/FormSection.js
@@ -8,7 +8,7 @@ export default function FormSection() {
         <h3 className="text-2xl md:text-3xl text-center font-medium mb-8">
           Get Your Stickers!
         </h3>
-        <div className="max-w-screen-sm mx-auto">
+        <div className="max-w-screen-sm mx-auto min-h-[560px]">
           <HubSpotForm formId="79c20f29-c2af-4e6c-9e89-f4d0eb6db166" />
         </div>
       </MaxWidth>
