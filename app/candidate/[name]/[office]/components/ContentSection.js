443dcf31167051a119657d4ac8ee19ba025ed600
tomer-tgp <tomer@goodparty.org>
2024-07-23 22:11:30 -0400

    fix: header

--- app/candidate/[name]/[office]/components/ContentSection.js
+++ app/candidate/[name]/[office]/components/ContentSection.js
@@ -12,11 +12,9 @@ export default function ContentSection(props) {
   const { projectedTurnout } = p2vData || {};
   return (
     <div className="grid grid-cols-12 gap-4 mb-4">
-      {p2vData && projectedTurnout && projectedTurnout !== 0 ? (
-        <div className="col-span-12 md:col-span-6">
-          <P2vCard {...props} />
-        </div>
-      ) : null}
+      <div className="col-span-12 md:col-span-6">
+        <P2vCard {...props} />
+      </div>
       {p2vData ? (
         <div className="col-span-12 md:col-span-6">
           <TargetCard {...props} />
