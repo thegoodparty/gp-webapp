563aff3a8334e55e4fc04ff0d3e31d8e5fc24ce5
tomer-tgp <tomer@goodparty.org>
2024-07-24 20:12:20 -0400

    fix: no p2vData

--- app/candidate/[name]/[office]/components/ContentSection.js
+++ app/candidate/[name]/[office]/components/ContentSection.js
@@ -9,7 +9,6 @@ export default function ContentSection(props) {
   const { candidate } = props;
   console.log('candidate', candidate);
   const { p2vData } = candidate;
-  const { projectedTurnout } = p2vData || {};
   return (
     <div className="grid grid-cols-12 gap-4 mb-4">
       <div className="col-span-12 md:col-span-6">
