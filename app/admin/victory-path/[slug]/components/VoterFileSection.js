3a8b558b2f6ed327146bb7cedc01aa4dbb5f4695
Taylor Murray <taylor@goodparty.org>
2024-07-31 10:26:42 -0700

    fix: auto save election type and election location.

--- app/admin/victory-path/[slug]/components/VoterFileSection.js
+++ app/admin/victory-path/[slug]/components/VoterFileSection.js
@@ -8,11 +8,14 @@ export default function VoterFileSection() {
     <div className="bg-indigo-50 rounded border border-slate-300 p-4 my-12">
       <div>
         {campaign.pathToVictory?.p2vStatus === 'Waiting' ? (
-          <div className="my-4">Path To Victory is processing...</div>
+          <div className="my-4">
+            Path To Victory is processing... Please wait a few minutes and
+            refresh the page.
+          </div>
         ) : (
           <div className="my-4">
             If you change the Election Type or Election Location you will need
-            to hit the Save button before you rerun the Path To Victory.
+            to rerun the Path To Victory.
           </div>
         )}
         <RerunP2V />
