aa448b4ee9b15985d9ab469e855e079eb690a6f8
tomer-tgp <tomer@goodparty.org>
2024-08-03 15:09:24 -0400

    feat: browsing

--- app/(candidate)/browsing/components/BrowsingPage.js
+++ app/(candidate)/browsing/components/BrowsingPage.js
@@ -12,13 +12,27 @@ import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
 const options = [
-  { key: 'running', label: 'Currently running for office' },
-  // { key: 'supporting', label: 'Supporting a candidate' },
-  { key: 'browsing', label: 'Just browsing' },
+  {
+    key: 'considering',
+    label: "I'm not running, but am actively considering to run in the future",
+  },
+  {
+    key: 'learning',
+    label:
+      "I'm interested in learning about GoodParty.org, but don't intend to run",
+  },
+  {
+    key: 'test',
+    label: 'I want to test out your tools before making a decision',
+  },
+  {
+    key: 'else',
+    label: 'Something else',
+  },
 ];
 
-export default function AccountTypePage() {
-  const [selected, setSelected] = useState('running');
+export default function BrowsingPage() {
+  const [selected, setSelected] = useState('considering');
   const router = useRouter();
 
   const handleNext = async () => {
@@ -35,10 +49,10 @@ export default function AccountTypePage() {
           <div className="grid py-6 max-w-2xl w-[75vw]">
             <Paper className="p-5 md:p-8 lg:p-12">
               <div className="text-center mb-8 pt-8">
-                <H1>Which best describes you?</H1>
+                <H1>We&apos;re excited to have you exploring GoodParty.org!</H1>
                 <Body2 className="mt-3">
-                  This helps us determine your account type. <br />
-                  (Don&apos;t worry, you can change this later.)
+                  To help us make your experience even better, could you let us
+                  know why you&apos;re just browsing?
                 </Body2>
               </div>
               <RadioList
@@ -48,9 +62,9 @@ export default function AccountTypePage() {
               />
 
               <div className="flex items-center justify-between mt-12">
-                <Link href="/">
+                <Link href="/account-type">
                   <SecondaryButton>
-                    <div className="min-w-[120px]">Cancel</div>
+                    <div className="min-w-[120px]">Back</div>
                   </SecondaryButton>
                 </Link>
                 <PrimaryButton onClick={handleNext}>
