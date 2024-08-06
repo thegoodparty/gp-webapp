a246e0d0542e59895f24ae737a45f6b796082960
Matthew Marcus <lurchbulldog@gmail.com>
2024-08-06 16:23:19 -0500

    [WEB-2640] - calendar url update

--- app/(landing)/info-session/components/CalendarIframe.js
+++ app/(landing)/info-session/components/CalendarIframe.js
@@ -1,7 +1,7 @@
 export default function CalendarIframe() {
   return (
     <iframe
-      src="https://meetings.hubspot.com/jack-nagel/volunteer-intro-chat?embed=true"
+      src="https://meetings.hubspot.com/katrina-vassallo/vol-1-1?embed=true"
       width="100%"
       height="620"
       title="Book a Meeting"
