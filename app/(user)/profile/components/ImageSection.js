a210147515e7cd29c3b8e402200ba78c98bb1834
tomer-tgp <tomer@goodparty.org>
2024-08-06 23:01:30 -0400

    feat: personal section

--- app/(user)/profile/components/ImageSection.js
+++ app/(user)/profile/components/ImageSection.js
@@ -11,6 +11,7 @@ import ImageUpload from '@shared/utils/ImageUpload';
 import { getUserCookie } from 'helpers/cookieHelper';
 import Body2 from '@shared/typography/Body2';
 import { useUser } from '@shared/hooks/useUser';
+import PrimaryButton from '@shared/buttons/PrimaryButton';
 
 function ImageSection() {
   const [user, setUser] = useUser();
@@ -26,15 +27,22 @@ function ImageSection() {
     }
   }, [uploadedImage]);
   return (
-    <div>
-      <div className="flex items-center flex-col">
+    <div className="flex mb-8">
+      <div className="p-4 bg-gray-200 rounded-2xl mr-4 border border-gray-300">
         <UserAvatar user={updatedUser} size="large" />
+      </div>
+      <div>
         <ImageUpload
-          customElement={<Body2 className="underline">Change Photo</Body2>}
+          customElement={
+            <div className="text-lg py-3 px-6 rounded-lg font-medium bg-primary-dark text-slate-50 inline-block">
+              Upload Image
+            </div>
+          }
           isUserImage
           uploadCallback={(image) => setUploadedImage(image)}
           maxFileSize={1000000}
         />
+        <Body2 className="mt-2">File size less than 1mb. JPG or PNG.</Body2>
       </div>
     </div>
   );
