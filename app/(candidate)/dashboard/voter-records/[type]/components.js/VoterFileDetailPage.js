8ef660e8a38f400bdeeb28a5c1f9c40bebe1fd73
Matthew Marcus <lurchbulldog@gmail.com>
2024-08-07 18:17:48 -0500

    [WEB-2662] - Fix voter file access issues (#236)
    * [WEB-2662] - Fix voter file access issues
    
    * [WEB-2662] - putting dead code back in

--- app/(candidate)/dashboard/voter-records/[type]/components.js/VoterFileDetailPage.js
+++ app/(candidate)/dashboard/voter-records/[type]/components.js/VoterFileDetailPage.js
@@ -1,7 +1,6 @@
 import DashboardLayout from 'app/(candidate)/dashboard/shared/DashboardLayout';
 import BackToAllFiles from './BackToAllFiles';
 import Hero from './Hero';
-import VideoSection from './VideoSection';
 import Paper from '@shared/utils/Paper';
 import ActionCards from './ActionCards';
 import VoterFileTypes from '../../components/VoterFileTypes';
@@ -10,32 +9,18 @@ import { slugify } from 'helpers/articleHelper';
 import H2 from '@shared/typography/H2';
 import Body2 from '@shared/typography/Body2';
 
+const getCustomVoterFileName = (customVoterFiles = [], type) =>
+  customVoterFiles.find((file) => `custom-${slugify(file.name, true)}` === type)
+    ?.name;
+
+const getDefaultVoterFileName = (type) =>
+  VoterFileTypes.find((file) => file.key.toLowerCase() === type)?.fields?.[0];
+
 export default function VoterFileDetailPage(props) {
   const { type, campaign, isCustom } = props;
-  const fileByKey = {};
-  VoterFileTypes.forEach((file) => {
-    fileByKey[file.key.toLowerCase()] = file;
-  });
-  if (
-    isCustom &&
-    campaign.data?.customVoterFiles &&
-    campaign.data?.customVoterFiles.length > 0 &&
-    Object.keys(fileByKey).length === 5
-  ) {
-    campaign.data?.customVoterFiles.forEach((file, i) => {
-      const key = `custom-${slugify(file.name, true)}`;
-      fileByKey[key] = {
-        key,
-        isCustom: true,
-        name: file.name,
-        fields: [file.channel, file.name, file.purpose || ''],
-      };
-    });
-  }
-
-  const file = fileByKey[type];
-  const { fields } = file || {};
-  const fileName = isCustom ? fields[1] : fields[0];
+  const fileName = isCustom
+    ? getCustomVoterFileName(campaign.data?.customVoterFiles, type)
+    : getDefaultVoterFileName(type);
 
   return (
     <DashboardLayout {...props}>
@@ -48,7 +33,6 @@ export default function VoterFileDetailPage(props) {
             <Body2 className="mt-1 mb-4 text-gray-600">
               Review the content below to get the most out of this voter file
             </Body2>
-            {/* <VideoSection {...props} /> */}
             <ActionCards {...props} fileName={fileName} />
           </Paper>
           <VendorCards {...props} type={type} />
--- app/(candidate)/dashboard/voter-records/page.js
+++ app/(candidate)/dashboard/voter-records/page.js
@@ -5,6 +5,7 @@ import VoterRecordsPage from './components/VoterRecordsPage';
 import { getServerToken, getServerUser } from 'helpers/userServerHelper';
 import gpApi from 'gpApi';
 import gpFetch from 'gpApi/gpFetch';
+import { redirect } from 'next/navigation';
 
 export async function fetchCanDownload() {
   try {
@@ -29,6 +30,9 @@ export default async function Page({ params, searchParams }) {
 
   const user = getServerUser(); // can be removed when door knocking app is not for admins only
   const { campaign } = await fetchUserCampaign();
+  if (!campaign?.isPro) {
+    return redirect('/dashboard/upgrade-to-pro', 'replace');
+  }
   const canDownload = await fetchCanDownload();
 
   const childProps = {
