56529b9d4c656edada9630a6e1c2985270945d78
tomer-tgp <tomer@goodparty.org>
2024-08-10 19:47:37 -0400

    feat: WEB-2689

--- app/shared/cards/CardPageWrapper.js
+++ app/shared/cards/CardPageWrapper.js
@@ -6,8 +6,10 @@ export default function CardPageWrapper({ children }) {
     <div className="bg-indigo-100 min-h-[calc(100vh-60px)]">
       <MaxWidth>
         <div className="flex items-center justify-center">
-          <div className="grid py-6 max-w-2xl w-[75vw]">
-            <Paper className="p-5 md:p-8 lg:p-12">{children}</Paper>
+          <div className="grid py-6 max-w-2xl w-[85vw]">
+            <Paper>
+              <div className="p-4 md:p-6 lg:p-8">{children}</div>
+            </Paper>
           </div>
         </div>
       </MaxWidth>
