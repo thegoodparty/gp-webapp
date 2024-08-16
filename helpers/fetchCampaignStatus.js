9ce5411f45ac1312fca61938da9dc433f6f338bc
tomer-tgp <tomer@goodparty.org>
2024-08-16 13:21:09 -0400

    fix: WEB-2689

--- helpers/fetchCampaignStatus.js
+++ helpers/fetchCampaignStatus.js
@@ -1,10 +1,10 @@
 import gpApi from 'gpApi';
 import gpFetch from 'gpApi/gpFetch';
 
-export async function fetchCampaignStatus() {
+export async function fetchCampaignStatus(serverToken = false) {
   try {
     const api = gpApi.user.campaignStatus;
-    return await gpFetch(api, false, 10);
+    return await gpFetch(api, false, 10, serverToken);
   } catch (e) {
     return { status: false };
   }
