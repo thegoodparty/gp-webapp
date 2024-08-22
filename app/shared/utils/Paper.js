6ff22a30ee675a0146d47becfc0f2e72753334a1
tomer-tgp <tomer@goodparty.org>
2024-08-21 21:30:46 -0400

    fix: WEB-2770

--- app/shared/utils/Paper.js
+++ app/shared/utils/Paper.js
@@ -1,7 +1,7 @@
 export default function Paper({ children, className, ...rest }) {
   return (
     <div
-      className={`bg-white border border-slate-300 py-4 px-4 md:py-5 lg:py-6 lg"px-8 rounded-xl ${
+      className={`bg-white border border-slate-300 py-4 px-4 md:py-5 lg:py-6 lg:px-8 rounded-xl ${
         className || ''
       }`}
       {...rest}
