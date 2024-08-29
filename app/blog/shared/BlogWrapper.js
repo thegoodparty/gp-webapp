6b2a4c8ab4daee693a867f4da7e895e2d8f1ecc0
Sanjeev Nayak <snayak00@gmail.com>
2024-08-29 12:01:11 -0400

    fix: remove front end sort

--- app/blog/shared/BlogWrapper.js
+++ app/blog/shared/BlogWrapper.js
@@ -42,11 +42,6 @@ export default function BlogWrapper({
     { label: sectionTitle },
   ];
 
-  // ensure sections are ordered correctly
-  const sortedSections = sections.sort(
-    (a, b) => Number(a.fields?.order) - Number(b.fields?.order),
-  );
-
   return (
     <>
       <StickersCallout />
