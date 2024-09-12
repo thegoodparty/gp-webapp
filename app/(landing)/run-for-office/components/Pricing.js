4874207c9f210143b4e25fd1df767c447bb1ec5b
Sanjeev Nayak <oldfashionedrobot@users.noreply.github.com>
2024-09-12 16:29:17 -0400

    WEB-2803: Run For Office Page 2.0 (#271)
    * WEB-2803: run for office pages updates
    
    * WEB-2803: remove unused image file
    
    * WEB-2803: fix build errors
    
    * WEB-2803: pr review cleanup
    
    * remove clsx
    
    ---------
    
    Co-authored-by: Sanjeev Nayak <snayak00@gmail.com>

--- app/(landing)/run-for-office/components/Pricing.js
+++ app/(landing)/run-for-office/components/Pricing.js
@@ -1,8 +1,11 @@
 import PrimaryButton from '@shared/buttons/PrimaryButton';
 import WarningButton from '@shared/buttons/WarningButton';
-import MaxWidth from '@shared/layouts/MaxWidth';
 import Link from 'next/link';
 import { IoMdCheckmark } from 'react-icons/io';
+import MarketingH3 from '@shared/typography/MarketingH3';
+import MarketingH4 from '@shared/typography/MarketingH4';
+import Body1 from '@shared/typography/Body1';
+import Button from '@shared/buttons/Button';
 
 const points1 = [
   'AI Campaign Content',
@@ -19,67 +22,64 @@ const points2 = [
   'Peer-to-peer texting and calling platform',
 ];
 
+function PricingCard({ heading, subheading, price, points, color }) {
+  const bgColor = color === 'lime' ? 'bg-lime-400' : 'bg-white';
+  const bulletColor =
+    color === 'lime'
+      ? 'bg-primary-main text-white'
+      : 'bg-lime-400 border-black/[0.12]';
+
+  return (
+    <div className={`flex flex-col p-6 md:p-12 rounded-md ${bgColor}`}>
+      <MarketingH4 className="pb-3 mb-6 border-b border-black/60">
+        {heading}
+      </MarketingH4>
+      {subheading && <Body1 className="mb-2">{subheading}</Body1>}
+      <ul className="grow p-0 m-0 list-inside list-none font-normal font-sfpro">
+        {points.map((point) => (
+          <li key={point} className="flex gap-3 mb-2">
+            <span>
+              <IoMdCheckmark
+                className={`p-[2px] rounded-full border ${bulletColor}`}
+                size={14}
+              />
+            </span>
+            <span>{point}</span>
+          </li>
+        ))}
+      </ul>
+      <div className="mt-14 mb-8 text-4xl font-medium">${price}/month</div>
+      <Link id="free-candidtate" href="/sign-up">
+        <Button className="w-full">Get Started</Button>
+      </Link>
+    </div>
+  );
+}
+
 export default function Pricing() {
   return (
-    <section
-      className="bg-primary-dark py-20 lg:py-28 text-white"
-      id="pricing-section"
-    >
-      <MaxWidth>
-        <h2 className="text-3xl lg:text-6xl font-semibold text-center">
+    <section id="pricing-section">
+      <hgroup className="text-white text-center mb-8">
+        <MarketingH3 className="mb-4 !text-4xl md:!text-5xl">
           Affordable and accessible
-        </h2>
-        <h3 className="text-xl lg:px-12 font-normal mt-10 mb-5 lg:mb-16 text-center">
+        </MarketingH3>
+        <Body1>
           Get free access to our core tools by agreeing to serve as an
           anti-corruption, independent, and people-powered representative for
           your community plus premium features for a small monthly fee.
-        </h3>
-        <div className="grid grid-cols-12 gap-4">
-          <div className="col-span-12 lg:col-span-6 lg:pr-8 lg:flex lg:justify-end">
-            <div className="shadow-md py-10 px-12 rounded-xl bg-[#FDFFF3] text-black">
-              <h3 className=" text-3xl font-medium pb-4 mb-8 border-b border-slate-300 min-w-[300px]">
-                Free
-              </h3>
-              {points1.map((point) => (
-                <div key={point} className="flex items-center mb-4">
-                  <div className="w-4 h-4 flex items-center justify-center rounded-full bg-lime-200 border border-primary mr-3">
-                    <IoMdCheckmark size={12} />
-                  </div>
-                  <div>{point}</div>
-                </div>
-              ))}
-              <div className="mt-14 mb-8 text-4xl font-light">$0/month</div>
-              <Link id="free-candidtate" href="/login">
-                <WarningButton fullWidth>Get Started</WarningButton>
-              </Link>
-            </div>
-          </div>
-          <div className="col-span-12 lg:col-span-6 lg:pl-8  lg:flex">
-            <div className="shadow-md py-10 px-12 rounded-xl bg-[#E4F47D] text-black flex flex-col justify-between">
-              <div>
-                <h3 className=" text-3xl font-medium pb-4 mb-8 border-b border-slate-300 min-w-[300px]">
-                  Pro
-                </h3>
-                <div className="font-bold mb-6">Everything in free plus…</div>
-                {points2.map((point) => (
-                  <div key={point} className="flex items-center mb-4">
-                    <div className="w-4 h-4 flex items-center justify-center rounded-full bg-lime-200 border border-primary mr-3">
-                      <IoMdCheckmark size={12} />
-                    </div>
-                    <div>{point}</div>
-                  </div>
-                ))}
-              </div>
-              <div>
-                <div className="mt-14 mb-8 text-4xl font-light">$10/month</div>
-                <Link id="pro-candidtate" href="/get-a-demo">
-                  <PrimaryButton fullWidth>Get Started</PrimaryButton>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-      </MaxWidth>
+        </Body1>
+      </hgroup>
+
+      <div class="flex-col md:flex-row flex justify-center mx-auto gap-4">
+        <PricingCard heading="Free" points={points1} price={0} />
+        <PricingCard
+          heading="Pro"
+          subheading="Everything in free plus…"
+          points={points2}
+          price={10}
+          color="lime"
+        />
+      </div>
     </section>
   );
 }
