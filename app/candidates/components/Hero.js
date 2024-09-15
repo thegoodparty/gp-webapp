689ea67eb05a8f49f8c78b953951c4de25c6e286
tomer-tgp <tomer@goodparty.org>
2024-09-14 22:08:11 -0400

    fix: hero title

--- app/candidates/components/Hero.js
+++ app/candidates/components/Hero.js
@@ -10,20 +10,17 @@ export default function Hero({ count = 0 }) {
   return (
     <div className="bg-primary-dark py-8 lg:py-24 text-white text-center ">
       <MaxWidth>
-        <MarketingH1 className="justify-center items-center">
-          <div className="flex  justify-center">
-            {count === 0 ? '' : numberFormatter(count)}{' '}
-            <Image
-              src="/images/heart.svg"
-              width={80}
-              height={80}
-              alt="gp.org"
-              className="mx-3"
-              priority
-            />{' '}
-            Independent
-          </div>
-          Civic Heroes Running in the U.S.
+        <MarketingH1 className="text-center">
+          {count === 0 ? '' : numberFormatter(count)}{' '}
+          <Image
+            src="/images/heart.svg"
+            width={80}
+            height={80}
+            alt="gp.org"
+            className="mx-3 static inline-block w-12 h-12 lg:w-20 lg:h-20"
+            priority
+          />
+          Independent Civic Heroes Running in the U.S.
         </MarketingH1>
         <MarketingH4 className="mt-8">
           Learn where Independent, People-Powered, and Anti-Corruption
