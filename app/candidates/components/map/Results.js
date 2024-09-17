9f44bb090fe07939df0a87ec55faa9ffefb10c9a
tomer-tgp <tomer@goodparty.org>
2024-09-16 21:11:36 -0400

    feat: WEB-2869 candidate stories

--- app/candidates/components/map/Results.js
+++ app/candidates/components/map/Results.js
@@ -8,7 +8,7 @@ import H5 from '@shared/typography/H5';
 export default function Results(props) {
   const { visibleMarkers } = useContext(MapContext);
   return (
-    <div className="md:w-[400px] lg:w-[500px]  md:h-[calc(100vh-56px-72px)] border-r border-gray-300 md:overflow-y-auto bg-indigo-100 overflow-auto">
+    <div className="md:w-[400px] lg:w-[500px]  md:h-[calc(100vh-56px-234px)] border-r border-gray-300 md:overflow-y-auto bg-indigo-100 overflow-auto">
       <H5 className="p-6">{visibleMarkers.length} candidates</H5>
       {visibleMarkers.map((campaign) => (
         <CampaignSnippet key={campaign.slug} campaign={campaign} />
