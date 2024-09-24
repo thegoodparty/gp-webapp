a9896cec262171b22451721f42be26b608834421
Matthew Marcus <lurchbulldog@gmail.com>
2024-09-24 13:39:14 -0500

    [WEB-2921] - Updated copy

--- app/(candidate)/dashboard/voter-records/[type]/components.js/ScheduleFlowStep1.js
+++ app/(candidate)/dashboard/voter-records/[type]/components.js/ScheduleFlowStep1.js
@@ -20,7 +20,7 @@ export default function ScheduleFlowStep1({
   type,
 }) {
   const isTel = type === 'telemarketing';
-  let price = 0.03;
+  let price = 0.35;
   if (type === 'telemarketing') {
     price = 0.04;
     if (voicemailValue) {
