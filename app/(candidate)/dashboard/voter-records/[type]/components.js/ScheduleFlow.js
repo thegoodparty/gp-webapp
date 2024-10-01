ce69c8b97be1fe0cc37fdf6b9735859805593c41
oakinh <nikaohill@gmail.com>
2024-10-01 11:28:45 -0600

    Fix a bug where the campaign type sms/telemarketing, wasn't being passed in updatedState

--- app/(candidate)/dashboard/voter-records/[type]/components.js/ScheduleFlow.js
+++ app/(candidate)/dashboard/voter-records/[type]/components.js/ScheduleFlow.js
@@ -89,6 +89,7 @@ export default function ScheduleFlow(props) {
     const updatedState = {
       ...state,
       voterFileUrl,
+      type,
     };
     await scheduleCampaign(updatedState);
   };
