39b6cc46fafb536f94ad8f05ef3363971c56c7df
tomer-tgp <tomer@goodparty.org>
2024-10-01 11:02:26 -0400

    chore: remove admin access

--- app/candidates/page.js
+++ app/candidates/page.js
@@ -2,7 +2,6 @@ import pageMetaData from 'helpers/metadataHelper';
 import gpApi from 'gpApi';
 import gpFetch from 'gpApi/gpFetch';
 import CandidatesPage from './components/CandidatesPage';
-import { adminAccessOnly } from 'helpers/permissionHelper';
 
 const fetchCount = async () => {
   const api = gpApi.campaign.mapCount;
@@ -22,7 +21,6 @@ export async function generateMetadata({ params, searchParams }) {
 }
 
 export default async function Page({ params, searchParams }) {
-  adminAccessOnly();
   const { count } = await fetchCount();
   const childProps = { count };
   return <CandidatesPage {...childProps} />;
