a93a856eeb397bdd0f43325e003cb80a5e226e28
tomer-tgp <tomer@goodparty.org>
2024-10-01 17:22:28 -0400

    chore: WEB-2980

--- app/shared/layouts/navigation/HeaderLogo.js
+++ app/shared/layouts/navigation/HeaderLogo.js
@@ -2,6 +2,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { useCampaign } from '@shared/hooks/useCampaign';
+import { useCampaignStatus } from '@shared/hooks/useCampaignStatus';
 
 const ProBadge = () => (
   <div
@@ -24,10 +25,18 @@ const ProBadge = () => (
 
 export const HeaderLogo = () => {
   const [campaign] = useCampaign();
+  const [campaignStatusClient] = useCampaignStatus();
+
   const { isPro } = campaign || {};
+  const { status } = campaignStatusClient || {};
+  const link = status && campaign ? '/dashboard' : '/';
   return (
     <>
-      <Link className="flex items-center no-underline" href="/" id="nav-logo">
+      <Link
+        className="flex items-center no-underline"
+        href={link}
+        id="nav-logo"
+      >
         <Image
           src="/images/heart-hologram.svg"
           data-cy="logo"
