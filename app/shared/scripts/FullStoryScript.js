38e93bfc9f0a76145e71dcd66ddad050f8cc13be
Sanjeev Nayak <snayak00@gmail.com>
2024-10-03 15:47:46 -0400

    WEB-3056: turn on fullstory debugging for ticket branch

--- app/shared/scripts/FullStoryScript.js
+++ app/shared/scripts/FullStoryScript.js
@@ -11,9 +11,10 @@ export default function FullStoryScript() {
       id="fullstory"
       dangerouslySetInnerHTML={{
         __html: `
-        if(window.location.hostname === 'goodparty.org'){
+        if(true || window.location.hostname === 'goodparty.org'){
+        console.log("LOADING FS");
           window['_fs_host'] = 'fullstory.com';
-          window['_fs_script'] = 'edge.fullstory.com/s/fs.js';
+          window['_fs_script'] = 'edge.fullstory.com/s/fs-debug.js';
           window['_fs_org'] = 'TBEDP';
           window['_fs_namespace'] = 'FS';
           !function(m,n,e,t,l,o,g,y){var s,f,a=function(h){
