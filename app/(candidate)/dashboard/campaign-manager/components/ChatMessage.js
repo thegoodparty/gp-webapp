05e63f560549568c52515d0a4ca4d9c964abc785
tomer-tgp <tomer@goodparty.org>
2024-10-06 18:11:19 -0400

    chore: fix copied state

--- app/(candidate)/dashboard/campaign-manager/components/ChatMessage.js
+++ app/(candidate)/dashboard/campaign-manager/components/ChatMessage.js
@@ -5,6 +5,8 @@ import Typewriter from 'typewriter-effect';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { FaRegCopy } from 'react-icons/fa';
 import { useState } from 'react';
+import Subtitle2 from '@shared/typography/Subtitle2';
+import { IoMdCheckmark } from 'react-icons/io';
 
 export default function ChatMessage({
   message,
@@ -51,17 +53,23 @@ export default function ChatMessage({
             ) : (
               <>
                 <div dangerouslySetInnerHTML={{ __html: content }} />
-                <div className="flex items-center border-b border-black/[0.12] w-[250px] pb-1 mb-4">
+                <div className="flex items-center border-b border-black/[0.12] w-[250px] pb-4 mb-4">
                   <div>
-                    <CopyToClipboard
-                      text={content.replace(/<[^>]*>/g, '')}
-                      onCopy={handleCopy}
-                    >
-                      <FaRegCopy className="cursor-pointer" size={16} />
-                    </CopyToClipboard>
-                    <div className={`mt-2 ${copied ? '' : 'opacity-0'}`}>
-                      Copied!
-                    </div>
+                    {copied ? (
+                      <div className="px-2 py-1 bg-primary/[0.08] flex items-center rounded-full">
+                        <IoMdCheckmark size={16} className="mr-2" />
+                        <Subtitle2>Copied</Subtitle2>
+                      </div>
+                    ) : (
+                      <div className="py-1">
+                        <CopyToClipboard
+                          text={content.replace(/<[^>]*>/g, '')}
+                          onCopy={handleCopy}
+                        >
+                          <FaRegCopy className="cursor-pointer" size={16} />
+                        </CopyToClipboard>
+                      </div>
+                    )}
                   </div>
                 </div>
               </>
