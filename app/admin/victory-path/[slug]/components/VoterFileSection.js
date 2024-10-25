9d474ea469929d132a285466dbea49e6ec8eac25
Taylor Murray <taylor@goodparty.org>
2024-10-25 11:24:33 -0700

    feat: Do not allow user to rerun P2V when user has not selected office

--- app/admin/victory-path/[slug]/components/VoterFileSection.js
+++ app/admin/victory-path/[slug]/components/VoterFileSection.js
@@ -7,18 +7,29 @@ export default function VoterFileSection() {
   return (
     <div className="bg-indigo-50 rounded border border-slate-300 p-4 my-12">
       <div>
-        {campaign.pathToVictory?.p2vStatus === 'Waiting' ? (
+        {campaign?.details?.raceId === undefined ? (
+          <div className="my-4">
+            This campaign is not eligible for an Automatic Path To Victory
+            because the user manually selected an office.
+          </div>
+        ) : null}
+
+        {campaign.pathToVictory?.p2vStatus === 'Waiting' &&
+        campaign?.details?.raceId ? (
           <div className="my-4">
             Path To Victory is processing... Please wait a few minutes and
             refresh the page.
           </div>
-        ) : (
+        ) : null}
+
+        {campaign.pathToVictory?.p2vStatus === 'Complete' &&
+        campaign?.details?.raceId ? (
           <div className="my-4">
             If you change the Election Type or Election Location you will need
             to rerun the Path To Victory.
           </div>
-        )}
-        <RerunP2V />
+        ) : null}
+        {campaign?.details?.raceId ? <RerunP2V /> : null}
       </div>
     </div>
   );
