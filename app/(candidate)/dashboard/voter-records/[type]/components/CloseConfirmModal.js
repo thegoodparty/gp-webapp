361e9b1f9da44dd225008e4c6dcdb6d263c88992
Sanjeev Nayak <snayak00@gmail.com>
2024-10-25 13:57:31 -0400

    WEB-3094: add close confirmation modal to schedule campaign flow

---
+++ app/(candidate)/dashboard/voter-records/[type]/components/CloseConfirmModal.js
@@ -0,0 +1,26 @@
+import Button from '@shared/buttons/Button';
+import Body1 from '@shared/typography/Body1';
+import H1 from '@shared/typography/H1';
+import Modal from '@shared/utils/Modal';
+
+export default function CloseConfirmModal({ open, onConfirm, onCancel }) {
+  return (
+    <Modal open={open} preventBackdropClose preventEscClose hideClose>
+      <div className="p-6 text-center">
+        <H1 className="mb-4">Are you sure you want to exit?</H1>
+        <Body1 className="mb-10">
+          Leaving now means you&apos;ll need to start over next time you
+          schedule a text campaign.
+        </Body1>
+        <div className="flex justify-center gap-4">
+          <Button color="neutral" size="large" onClick={onConfirm}>
+            Exit Without Saving
+          </Button>
+          <Button size="large" onClick={onCancel}>
+            Continue Scheduling
+          </Button>
+        </div>
+      </div>
+    </Modal>
+  );
+}
