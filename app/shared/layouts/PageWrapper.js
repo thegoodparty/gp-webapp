9b7ca5f0518cb64332ecfdeed4fdb6078af34cdf
Matthew Marcus <lurchbulldog@gmail.com>
2024-10-25 16:36:37 -0500

    [WEB-3144] - Hide nav to fix double nav in Product Tour

--- app/shared/layouts/PageWrapper.js
+++ app/shared/layouts/PageWrapper.js
@@ -1,3 +1,4 @@
+import { headers } from 'next/headers';
 import Snackbar from '@shared/utils/Snackbar';
 import { Suspense } from 'react';
 import Footer from 'app/shared/layouts/footer/Footer';
@@ -11,34 +12,40 @@ import { CampaignProvider } from '@shared/user/CampaignProvider';
 import { InsecureCookieClearer } from '@shared/layouts/InsecureCookieClearer';
 import { ImpersonateUserProvider } from '@shared/user/ImpersonateUserProvider';
 
-export default function PageWrapper({ children, hideFooter }) {
-  return (
-    <InsecureCookieClearer>
-      <UserProvider>
-        <ImpersonateUserProvider>
-          <CampaignProvider>
-            <CampaignStatusProvider>
-              <NavigationProvider>
-                <div className="overflow-x-hidden">
-                  <JsonLdSchema />
-                  <Nav />
-                  {children}
-                  {!hideFooter && (
-                    <Suspense>
-                      <Footer />
-                    </Suspense>
-                  )}
-                  <Snackbar />
+const isProductTourRequest = () => {
+  const headersList = headers();
+  const activePath = headersList.get('x-pathname');
+  return activePath === '/product-tour';
+};
 
+const PageWrapper = ({ children, hideFooter }) => (
+  <InsecureCookieClearer>
+    <UserProvider>
+      <ImpersonateUserProvider>
+        <CampaignProvider>
+          <CampaignStatusProvider>
+            <NavigationProvider>
+              <div className="overflow-x-hidden">
+                <JsonLdSchema />
+                {!isProductTourRequest() && <Nav />}
+                {children}
+                {!hideFooter && (
                   <Suspense>
-                    <CookiesSnackbar />
+                    <Footer />
                   </Suspense>
-                </div>
-              </NavigationProvider>
-            </CampaignStatusProvider>
-          </CampaignProvider>
-        </ImpersonateUserProvider>
-      </UserProvider>
-    </InsecureCookieClearer>
-  );
-}
+                )}
+                <Snackbar />
+
+                <Suspense>
+                  <CookiesSnackbar />
+                </Suspense>
+              </div>
+            </NavigationProvider>
+          </CampaignStatusProvider>
+        </CampaignProvider>
+      </ImpersonateUserProvider>
+    </UserProvider>
+  </InsecureCookieClearer>
+);
+
+export default PageWrapper;
