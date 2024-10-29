08e578a52ced9176da3b657905e3f4da6b64cd5f
Sanjeev Nayak <oldfashionedrobot@users.noreply.github.com>
2024-10-29 12:18:42 -0400

    WEB-3149: enable users to edit opponents (#334)
    Co-authored-by: Sanjeev Nayak <snayak00@gmail.com>

--- app/(candidate)/dashboard/campaign-details/components/RunningAgainstSection.js
+++ app/(candidate)/dashboard/campaign-details/components/RunningAgainstSection.js
@@ -1,38 +1,93 @@
 'use client';
 
+import { useState } from 'react';
 import H3 from '@shared/typography/H3';
-import RunningAgainstModule from './RunningAgainstModule';
 import { updateCampaign } from 'app/(candidate)/onboarding/shared/ajaxActions';
+import PrimaryButton from '@shared/buttons/PrimaryButton';
+import RunningAgainstForm from './RunningAgainstForm';
+import RunningAgainstCard from './RunningAgainstCard';
 
-export default function RunningAgainstSection(props) {
-  const { campaign, nextCallback, header } = props;
-  const handleSave = async (againstState) => {
-    let newAgainst = [...againstState.runningAgainst];
-    if (againstState.newName && againstState.newDesc) {
-      newAgainst.push({
-        name: againstState.newName,
-        description: againstState.newDesc,
-        party: againstState.newParty,
-      });
-    }
+export default function RunningAgainstSection({
+  campaign,
+  nextCallback,
+  header,
+}) {
+  const [isSaving, setIsSaving] = useState(false);
+  const [showNew, setShowNew] = useState(false);
+  const [runningAgainst, setRunningAgainst] = useState(
+    campaign?.details?.runningAgainst || [],
+  );
 
+  const handleSave = async () => {
+    setIsSaving(true);
     await updateCampaign([
-      { key: 'details.runningAgainst', value: newAgainst },
+      { key: 'details.runningAgainst', value: runningAgainst },
     ]);
+
+    setIsSaving(false);
+
     if (nextCallback) {
       nextCallback();
     } else {
       window.location.reload();
     }
   };
+
+  const handleDelete = (index) => {
+    setRunningAgainst((current) => current.toSpliced(index, 1));
+  };
+
+  const handleUpdate = (index, newValues) => {
+    setShowNew(false);
+    setRunningAgainst((current) => {
+      const runningAgainst = [...current];
+
+      if (typeof index === 'number') {
+        runningAgainst[index] = newValues;
+      } else {
+        runningAgainst.push(newValues);
+      }
+      return runningAgainst;
+    });
+  };
+
   return (
     <section className={` pt-6  ${header ? '' : 'border-t  border-gray-600'}`}>
       {header ? <>{header}</> : <H3>Who You&apos;re Running Against</H3>}
-      <RunningAgainstModule
-        campaign={campaign}
-        handleSave={handleSave}
-        smallButton
-      />
+
+      {runningAgainst.map((against, index) => {
+        return (
+          <RunningAgainstCard
+            key={index}
+            {...against}
+            onDelete={() => handleDelete(index)}
+            onUpdate={(newValues) => handleUpdate(index, newValues)}
+          />
+        );
+      })}
+
+      <div className="my-6 border rounded-xl p-4 border-gray-300 flex justify-between">
+        {showNew ? (
+          <RunningAgainstForm
+            onCancel={() => setShowNew(false)}
+            onSave={(newValues) => handleUpdate(null, newValues)}
+          />
+        ) : (
+          <PrimaryButton size="medium" onClick={() => setShowNew(true)}>
+            Add New Opponent
+          </PrimaryButton>
+        )}
+      </div>
+
+      <div className="flex justify-end mb-6">
+        <PrimaryButton
+          loading={isSaving}
+          disabled={isSaving}
+          onClick={handleSave}
+        >
+          Save
+        </PrimaryButton>
+      </div>
     </section>
   );
 }
