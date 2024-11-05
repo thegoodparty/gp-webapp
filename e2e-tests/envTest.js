749a444ea9112b9075d23e749e36faf3ca40f831
Dustin Sison <dsison34@gmail.com>
2024-11-04 19:32:03 -0800

    Updated API KEY

---
+++ e2e-tests/envTest.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+console.log(process.env.TESTRAIL_URL);
+console.log(process.env.TESTRAIL_PROJECT_ID);
+console.log(process.env.TESTRAIL_USERNAME);
+console.log(process.env.TESTRAIL_API_KEY);
