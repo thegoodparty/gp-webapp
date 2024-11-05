2c0713d5c8f672ade171a07adec9a1eec4d69eea
Sanjeev Nayak <oldfashionedrobot@users.noreply.github.com>
2024-11-05 13:26:58 -0500

    WEB-3268: Fix map winners list bugs (#345)
    * WEB-3268: fix map winners list bugs
    
    * WEB-3268: finish comment
    
    * WEB-3268: add padding to scroll window calculation
    
    ---------
    
    Co-authored-by: Sanjeev Nayak <snayak00@gmail.com>

--- app/candidates/components/winners/WinnerListSection.js
+++ app/candidates/components/winners/WinnerListSection.js
@@ -8,171 +8,18 @@ import { memo, useEffect, useState } from 'react';
 import FilteredWinnerList from './FilteredWinnerList';
 import { useMapCampaigns } from '@shared/hooks/useMapCampaigns';
 
-// const tempCampaigns = [
-//   {
-//     slug: 'tomer-almog1',
-//     didWin: true,
-//     office: 'Flat Rock Village Mayor',
-//     state: 'NC',
-//     ballotLevel: 'CITY',
-//     zip: '28739',
-//     party: 'independent',
-//     firstName: 'Tomer',
-//     lastName: 'Almog',
-//     avatar: false,
-//     geoLocation: { lat: 35.4119, lng: -82.46711, geoHash: 'dnm2gkm6' },
-//   },
-//   {
-//     slug: 'tomer-almog',
-//     didWin: null,
-//     office: 'Flat Rock Village Mayor',
-//     state: 'NC',
-//     ballotLevel: 'CITY',
-//     zip: '28739',
-//     party: 'independent',
-//     firstName: 'Tomer',
-//     lastName: 'Almog',
-//     avatar: false,
-//     geoLocation: { lat: 35.3119, lng: -82.46701, geoHash: 'dnm2gkm6' },
-//   },
-//   {
-//     slug: 'tomer-almog2',
-//     didWin: true,
-//     office: 'Flat Rock Village Mayor2',
-//     state: 'NC',
-//     ballotLevel: 'CITY',
-//     zip: '28739',
-//     party: 'independent',
-//     firstName: 'Tomer',
-//     lastName: 'Almog',
-//     avatar: false,
-//     geoLocation: { lat: 35.3119, lng: -82.46701, geoHash: 'dnm2gkm6' },
-//   },
-//   {
-//     slug: 'tomer-almog3',
-//     didWin: true,
-//     office: 'Flat Rock Village Mayor2',
-//     state: 'NC',
-//     ballotLevel: 'CITY',
-//     zip: '28739',
-//     party: 'independent',
-//     firstName: 'Tomer3',
-//     lastName: 'Almog',
-//     avatar: false,
-//     geoLocation: { lat: 35.3119, lng: -82.46721, geoHash: 'dnm2gkm6' },
-//   },
-//   {
-//     slug: 'tomer-almog4',
-//     didWin: true,
-//     office: 'Flat Rock Village Mayor2',
-//     state: 'NC',
-//     ballotLevel: 'CITY',
-//     zip: '28739',
-//     party: 'independent',
-//     firstName: 'Tomer4',
-//     lastName: 'Almog',
-//     avatar: false,
-//     geoLocation: { lat: 35.3119, lng: -82.46721, geoHash: 'dnm2gkm6' },
-//   },
-
-//   {
-//     slug: 'tomer-almog5',
-//     didWin: true,
-//     office: 'Flat Rock Village Mayor2',
-//     state: 'NC',
-//     ballotLevel: 'COUNTY',
-//     zip: '28739',
-//     party: 'independent',
-//     firstName: 'Tomer4',
-//     lastName: 'Almog',
-//     avatar: false,
-//     geoLocation: { lat: 35.3119, lng: -82.46721, geoHash: 'dnm2gkm6' },
-//   },
-
-//   {
-//     slug: 'tomer-almog6',
-//     didWin: true,
-//     office: 'Flat Rock Village Mayor2',
-//     state: 'NC',
-//     ballotLevel: 'COUNTY',
-//     zip: '28739',
-//     party: 'independent',
-//     firstName: 'Tomer4',
-//     lastName: 'Almog',
-//     avatar: false,
-//     geoLocation: { lat: 35.3119, lng: -82.46721, geoHash: 'dnm2gkm6' },
-//   },
-//   {
-//     slug: 'tomer-almog7',
-//     didWin: true,
-//     office: 'state Flat Rock Village Mayor2',
-//     state: 'NC',
-//     ballotLevel: 'STATE',
-//     zip: '28739',
-//     party: 'independent',
-//     firstName: 'Tomer7',
-//     lastName: 'Almog',
-//     avatar: false,
-//     geoLocation: { lat: 35.3119, lng: -82.46721, geoHash: 'dnm2gkm6' },
-//   },
-//   {
-//     slug: 'tomer-almog8',
-//     didWin: true,
-//     office: 'state Flat Rock Village Mayor2',
-//     state: 'NC',
-//     ballotLevel: 'STATE',
-//     zip: '28739',
-//     party: 'independent',
-//     firstName: 'Tomer8',
-//     lastName: 'Almog',
-//     avatar: false,
-//     geoLocation: { lat: 35.3119, lng: -82.46721, geoHash: 'dnm2gkm6' },
-//   },
-//   {
-//     slug: 'tomer-almog9',
-//     didWin: true,
-//     office: 'state Flat Rock Village Mayor2',
-//     state: 'NC',
-//     ballotLevel: 'STATE',
-//     zip: '28739',
-//     party: 'independent',
-//     firstName: 'Tomer9',
-//     lastName: 'Almog',
-//     avatar: false,
-//     geoLocation: { lat: 35.3119, lng: -82.46721, geoHash: 'dnm2gkm6' },
-//   },
-//   {
-//     slug: 'tomer-almog10',
-//     didWin: true,
-//     office: 'state Flat Rock Village Mayor2',
-//     state: 'NC',
-//     ballotLevel: 'STATE',
-//     zip: '28739',
-//     party: 'independent',
-//     firstName: 'Tomer10',
-//     lastName: 'Almog',
-//     avatar: false,
-//     geoLocation: { lat: 35.3119, lng: -82.46721, geoHash: 'dnm2gkm6' },
-//   },
-// ];
+const WINNER_FILTER = { results: true };
 
 export default memo(function WinnerListSection() {
-  const { campaigns: allCampaigns } = useMapCampaigns(null);
+  const { campaigns: allCampaigns } = useMapCampaigns(WINNER_FILTER);
   const [campaigns, setCampaigns] = useState([]);
   const [offices, setOffices] = useState([]);
 
   useEffect(() => {
-    let winners = [];
-    (allCampaigns || []).forEach((campaign) => {
-      if (campaign.didWin === true) {
-        winners.push(campaign);
-      }
-    });
-
-    const allOffices = winners.map((campaign) => campaign.office);
+    const allOffices = allCampaigns.map((campaign) => campaign.office);
     const offices = [...new Set(allOffices)]; // dedupe
 
-    setCampaigns(winners);
+    setCampaigns(allCampaigns);
     setOffices(offices);
   }, [allCampaigns]);
 
@@ -183,11 +30,10 @@ export default memo(function WinnerListSection() {
   });
 
   const onChangeFilters = (key, value) => {
-    setFilters({ ...filters, [key]: value });
+    setFilters({ [key]: value });
 
     const filteredCampaigns = allCampaigns.filter((campaign) => {
       return (
-        campaign.didWin === true &&
         (key === 'state' ? campaign.state === value || value === '' : true) &&
         (key === 'office' ? campaign.office === value || value === '' : true) &&
         (key === 'level'
@@ -219,7 +65,11 @@ export default memo(function WinnerListSection() {
         <MarketingH4 className="mt-8 text-center mb-12">
           Learn who is leading the charge for independents in local elections
         </MarketingH4>
-        <WinnerFilters onChangeFilters={onChangeFilters} offices={offices} />
+        <WinnerFilters
+          filters={filters}
+          onChangeFilters={onChangeFilters}
+          offices={offices}
+        />
         <FilteredWinnerList campaigns={campaigns} />
       </MaxWidth>
     </div>
