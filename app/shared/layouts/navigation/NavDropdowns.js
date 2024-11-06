69cd9667c529ff6395f9eb30293eeb72021d0d81
Dustin Sison <20826079+dustinsison@users.noreply.github.com>
2024-11-06 09:38:18 -0800

    QA Automation Test ID Tags (#349)
    * Added test-id in Nav.js and fixed id typo in Pricing.js
    
    * Added tags for navbar

--- app/shared/layouts/navigation/NavDropdowns.js
+++ app/shared/layouts/navigation/NavDropdowns.js
@@ -4,10 +4,11 @@ import { useNav } from '@shared/layouts/navigation/useNav';
 export const NavDropdowns = () => {
   const { dropdowns, openStates, toggle, closeAll } = useNav();
 
-  return dropdowns.map(({ id, label, links }, index) => (
+  return dropdowns.map(({ id, label, links, dataTestId = '' }, index) => (
     <NavDropdown
       key={index}
       id={id}
+      dataTestId={dataTestId}
       label={label}
       links={links}
       open={openStates[index]}
