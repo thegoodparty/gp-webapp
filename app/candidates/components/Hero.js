13b1c29d25a7215dbf4c96c31e04d191d3b4cfce
Sanjeev Nayak <snayak00@gmail.com>
2024-11-08 13:42:31 -0500

    Candidates Map header responsive fix

--- app/candidates/components/Hero.js
+++ app/candidates/components/Hero.js
@@ -9,7 +9,7 @@ export default memo(function Hero({ count = 0, longState }) {
   return (
     <div className="bg-primary-dark py-8 lg:py-24 text-white text-center ">
       <MaxWidth>
-        <MarketingH1 className="text-center">
+        <MarketingH1 className="text-center !text-4xl md:!text-5xl lg:!text-6xl xl:!text-7xl">
           We Empowered&nbsp;
           {numberFormatter(count)}
           <Image
@@ -17,13 +17,13 @@ export default memo(function Hero({ count = 0, longState }) {
             width={80}
             height={80}
             alt="gp.org"
-            className="mx-3 static inline-block w-12 h-12 lg:w-20 lg:h-20"
+            className="mx-3 static inline-block w-10 h-10 md:w-14 md:h-14 lg:w-18 lg:h-18 xl:w-20 xl:h-20"
             priority
           />
           Independent Victories Across {longState ? longState : 'the U.S'}. 🎉
         </MarketingH1>
 
-        <MarketingH4 className="mt-8">
+        <MarketingH4 className="mt-8 !text-2xl md:!text-3xl lg:!text-4xl">
           See where GoodParty.org empowered independent candidates won their
           elections to transform civic leadership in their communities.
         </MarketingH4>
