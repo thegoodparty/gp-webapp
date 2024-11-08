8cef82741b8d0d63536fc719dc01ad1d026f0aa5
tomer-tgp <tomer@goodparty.org>
2024-11-08 17:32:51 -0500

    chore: header change

--- app/shared/layouts/navigation/Nav.js
+++ app/shared/layouts/navigation/Nav.js
@@ -5,6 +5,7 @@ import RightSideMobile from './RightSideMobile';
 import { HeaderLogo } from '@shared/layouts/navigation/HeaderLogo';
 import { getServerToken, getServerUser } from 'helpers/userServerHelper';
 import { setUserCookie } from 'helpers/cookieHelper';
+import Body2 from '@shared/typography/Body2';
 
 export default async function Nav() {
   let campaignStatus = false;
@@ -22,9 +23,15 @@ export default async function Nav() {
     <>
       <div className="fixed w-screen h-14 z-50">
         <div className="relative bg-indigo-50 lg:block border-solid border-b border-zinc-200 px-5 lg:px-8 z-50 h-14">
-          <div className="flex justify-between items-center h-14" data-testid="navbar">
+          <div
+            className="flex justify-between items-center h-14"
+            data-testid="navbar"
+          >
             <div className="flex items-center">
               <HeaderLogo />
+              <Body2 className="pl-2 italic xl:block lg:hidden md:block ">
+                empowering independents to run , win and serve!
+              </Body2>
               <LeftSide />
             </div>
             <RightSide campaignStatus={campaignStatus} />
