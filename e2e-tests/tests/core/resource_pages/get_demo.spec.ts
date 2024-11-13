b48c1c32e8c80f9c28b02c1f0e4c61284cda2333
Dustin Sison <dsison34@gmail.com>
2024-11-12 20:10:01 -0800

    Adjusted demo tests
    Original commit: https://github.com/thegoodparty/test-automation/commit/98b4e48fca8307c678e7f046cc28b1c2d597b8f3

--- e2e-tests/tests/core/resource_pages/get_demo.spec.ts
+++ e2e-tests/tests/core/resource_pages/get_demo.spec.ts
@@ -42,11 +42,11 @@ test('Verify Get a Demo page', async ({ page }) => {
 
         // Click the first available date within the calendar
         await frame.locator(`text="${calendarDateTitle}"`).isVisible();
-        await frame.locator('[data-test-id="available-date"]').first().click();
+        await frame.locator('[data-test-id="available-date"]').first().click({ timeout: 10000 });
         
         // click first available time
         await frame.locator(`text="${calendarTimeTitle}"`).isVisible();
-        await frame.locator('[data-test-id="time-picker-btn"]').first().click();
+        await frame.locator('[data-test-id="time-picker-btn"]').first().click({ timeout: 10000 });
 
         // Verify "Your Information" page
         await frame.locator(`text="${calendarInfoTitle}"`).isVisible();
