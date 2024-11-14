71f44fbd2bc37a2e51bde1b93c69d2847c1e351a
Sanjeev Nayak <snayak00@gmail.com>
2024-11-14 10:48:22 -0500

    WEB-3215: added shareable components not in shared folder

---
+++ app/shared/unshared/TealButton.stories.js
@@ -0,0 +1,17 @@
+import TealButton from 'app/candidate/[name]/[office]/components/TealButton';
+
+export default {
+  title: 'Unshared/TealButton',
+  component: TealButton,
+  tags: ['autodocs'],
+  render: (args) => {
+    return (
+      <>
+        <strong>(Used in candidate pages)</strong>
+        <TealButton {...args}>Teal Button</TealButton>
+      </>
+    );
+  },
+};
+
+export const Default = {};
