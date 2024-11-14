0d9bb7f1efc722b6eb49a986ae0fecf28fb60ec3
dustinsison <dsison34@gmail.com>
2024-11-14 14:53:46 -0800

    Added variable for specific test run naming

--- e2e-tests/globalSetup.js
+++ e2e-tests/globalSetup.js
@@ -5,7 +5,7 @@ import "dotenv/config";
 module.exports = async () => {
   const testRunName = process.env.TEST_RUN_NAME || "Playwright Test Run";
   const testRunId = await createTestRun(
-    "Playwright Test Run",
+    testRunName,
     [1, 2, 4, 5, 7, 8, 12, 16, 17]
   );
   fs.writeFileSync("testRunId.txt", testRunId.toString());
