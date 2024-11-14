6206402e048012de05f98c483bebb7b877a14f67
Sanjeev Nayak <snayak00@gmail.com>
2024-11-14 14:38:57 -0500

    Revert "Hardcode candidate win number on Home and Candidates pages (#360)"
    This reverts commit e7d9ed6201db477df1833cfc4ec6134fe82f7278.

--- app/homepage/Hero.js
+++ app/homepage/Hero.js
@@ -13,12 +13,18 @@ const fetchWinnerCount = async () => {
 };
 
 export default async function Hero() {
+  const { count } = await fetchWinnerCount();
+
+  // fallback to hardcoded 1,000 in case of bad api call
+  const winnerCount =
+    typeof count === 'number' && count > 0 ? count.toLocaleString() : '1,000';
+
   return (
     <MaxWidth>
       <div className="grid grid-cols-12 gap-4 lg:gap-8 md:justify-items-center pt-20 bg-indigo-50 items-stretch sm:p-8 md:px-10 lg:p-16 xl:px-0">
         <div className="col-span-12 lg:col-span-6">
           <h1 className="text-4xl leading-tight font-semibold xs:font-bold md:text-5xl md:leading-tight xl:text-6xl xl:leading-snug">
-            We empowered 3,120 Independent wins in 2024 🎉
+            We empowered {winnerCount} Independent wins in 2024 🎉
           </h1>
           <h2 className="text-lg font-sfpro font-normal leading-6 mt-8">
             GoodParty.org is empowering civic heroes to run, win, and serve as
