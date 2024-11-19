a6e9968b533f0489d258cfba50de387a876e4e8b
Dustin Sison <20826079+dustinsison@users.noreply.github.com>
2024-11-19 11:32:07 -0800

    Added data-testid tags for login page (#367)

--- app/shared/inputs/EmailInput.js
+++ app/shared/inputs/EmailInput.js
@@ -54,6 +54,9 @@ export default function EmailInput({
             }
           : {}
       }
+    inputProps={{
+    'data-testid': restProps['data-testid'],
+    }}
       {...restProps}
     />
   );
