3b4dbe562866e8e0c10aa573eeff9009c3640bd9
Dustin Sison <dsison34@gmail.com>
2024-11-25 13:00:21 -0800

    Updated config to work with existing workflow

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -13,23 +13,36 @@ export default defineConfig({
     ["json", { outputFile: "test-results/playwright-results.json" }],
   ],
   use: {
+    baseURL: process.env.BASE_URL || "http://localhost:4000/", // Fallback to default URL if not provided
     trace: "on-first-retry",
   },
-
   projects: [
-    {
-      name: 'Local',
-      use: {
-        baseURL: 'http://localhost:3000',
-        ...devices['Desktop Chrome'],
-      },
+  {
+    name: "chromium",
+    use: { ...devices["Desktop Chrome"], baseURL: process.env.BASE_URL },
+  },
+  {
+    name: "firefox",
+    use: { ...devices["Desktop Firefox"], baseURL: process.env.BASE_URL },
+  },
+  {
+    name: "webkit",
+    use: { ...devices["Desktop Safari"], baseURL: process.env.BASE_URL },
+  },
+  // The following projects should only be run explicitly with the --project flag
+  {
+    name: "Local",
+    use: {
+      baseURL: "http://localhost:3000",
+      ...devices["Desktop Chrome"],
     },
-    {
-      name: 'QA',
-      use: {
-        baseURL: 'https://qa.goodparty.org',
-        ...devices['Desktop Chrome'],
-      },
+  },
+  {
+    name: "QA",
+    use: {
+      baseURL: "https://qa.goodparty.org",
+      ...devices["Desktop Chrome"],
     },
+  },
   ],
 });
