a78ec62af61570638d321a4c9b393390a44d4fd3
tomer-tgp <52380560+tomer-tgp@users.noreply.github.com>
2024-12-05 12:41:51 -0500

    Qa to master (#382)
    * chore: kill endtest
    
    * Updates deploy.yml to use GitHub Secrets for test credentials (#378)
    
    * chore: text change
    
    * fix: lowercase of undefined
    
    * Merge dev to QA (#380)
    
    * chore: kill endtest
    
    * Updates deploy.yml to use GitHub Secrets for test credentials
    
    * Updates deploy.yml to use GitHub Secrets for test credentials (#378)
    
    * Renamed account variables
    
    * Added additional test accounts
    
    * chore: text change
    
    * fix: lowercase of undefined
    
    ---------
    
    Co-authored-by: Dustin Sison <dsison34@gmail.com>
    Co-authored-by: Dustin Sison <20826079+dustinsison@users.noreply.github.com>
    
    * merge dev to qa (#381)
    
    * chore: kill endtest
    
    * Updates deploy.yml to use GitHub Secrets for test credentials
    
    * Updates deploy.yml to use GitHub Secrets for test credentials (#378)
    
    * Renamed account variables
    
    * Added additional test accounts
    
    * chore: text change
    
    * fix: lowercase of undefined
    
    * fix: typo
    
    ---------
    
    Co-authored-by: Dustin Sison <dsison34@gmail.com>
    Co-authored-by: Dustin Sison <20826079+dustinsison@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Dustin Sison <20826079+dustinsison@users.noreply.github.com>
    Co-authored-by: Dustin Sison <dsison34@gmail.com>

--- app/(landing)/run-for-office/components/WhyUse.js
+++ app/(landing)/run-for-office/components/WhyUse.js
@@ -27,10 +27,10 @@ export default function WhyUse() {
   return (
     <section className="bg-primary-dark text-white pt-16 px-8 pb-24 text-center">
       <MarketingH3 className="mb-4 !text-4xl md:!text-5xl">
-        Why use GoodParty.org
+        Why use GoodParty.org?
       </MarketingH3>
       <Body1>
-        How we’re leveling the playing field for non-partisan, independent, and
+        We&apos;re leveling the playing field for non-partisan, independent, and
         third-party candidates.
       </Body1>
       <div className="flex flex-col md:flex-row px-8 md:px-0 gap-8 mt-12 mx-auto max-w-4xl">
