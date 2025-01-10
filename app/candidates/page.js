ef773c4139fcc91af9833193557de87aff4ff5a9
Sanjeev Nayak <snayak00@gmail.com>
2025-01-10 14:40:22 -0500

    bump win count

--- app/candidates/page.js
+++ app/candidates/page.js
@@ -4,7 +4,7 @@ import gpFetch from 'gpApi/gpFetch';
 import CandidatesPage from './components/CandidatesPage';
 import { numberFormatter } from 'helpers/numberHelper';
 
-export const WINNER_COUNT = 3446;
+export const WINNER_COUNT = 3448;
 
 const fetchCount = async (onlyWinners = false) => {
   const api = gpApi.campaign.mapCount;
