5604d04c54242846b8f860d621365973ddf06c36
Dustin Sison <20826079+dustinsison@users.noreply.github.com>
2025-01-15 09:13:24 -0800

    Added test for admin AI content page (#32)

--- e2e-tests/globalSetup.js
+++ e2e-tests/globalSetup.js
@@ -5,7 +5,7 @@ import "dotenv/config";
 module.exports = async () => {
   const testRunName = process.env.TEST_RUN_NAME || "Playwright Test Run";
   let testCaseIds = [
-    1, 2, 4, 5, 7, 8, 12, 16, 17, 18, 19, 20, 21, 22, 23, 24, 26, 33, 34, 35, 36, 37,
+    1, 2, 4, 5, 7, 8, 12, 16, 17, 18, 19, 20, 21, 22, 23, 24, 26, 29, 33, 34, 35, 36, 37,
     38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 53, 54
   ];
   const testRunId = await createTestRun(testRunName, testCaseIds);
