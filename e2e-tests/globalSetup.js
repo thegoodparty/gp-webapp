0b60ac8e533bff1d21f0fa19f02ce4a58de2005a
Dustin Sison <20826079+dustinsison@users.noreply.github.com>
2025-01-28 16:46:26 -0800

    Added style guide button tests (#34)
    * Added style guide button tests
    
    * Added efficient cleaner code updates
    
    Original commit: https://github.com/thegoodparty/test-automation/commit/3448dbdb09b108193ca0f126936fa2aa5b1df849

--- e2e-tests/globalSetup.js
+++ e2e-tests/globalSetup.js
@@ -6,7 +6,8 @@ module.exports = async () => {
   const testRunName = process.env.TEST_RUN_NAME || "Localized Test Run (Debugging)";
   let testCaseIds = [
     1, 2, 4, 5, 7, 8, 12, 16, 17, 18, 19, 20, 21, 22, 23, 24, 26, 29, 33, 34, 35, 36, 37,
-    38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 53, 54
+    38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 53, 54, 55, 56, 57, 58, 59, 
+    60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70
   ];
   const testRunId = await createTestRun(testRunName, testCaseIds);
   fs.writeFileSync("testRunId.txt", testRunId.toString());
