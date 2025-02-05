6c00be9b4c529e3632be91228629591ba9f02f83
Dustin Sison <20826079+dustinsison@users.noreply.github.com>
2025-02-05 14:33:33 -0800

    Added test for Admin Top Issues page (#40)
    Original commit: https://github.com/thegoodparty/test-automation/commit/8c3815a50e7cc0505f97d98a5e9e55e8bca2d046

---
+++ e2e-tests/tests/admin/top_issues.spec.ts
@@ -0,0 +1,62 @@
+import 'dotenv/config';
+import { test } from '@playwright/test';
+import { addTestResult, skipNonQA } from 'helpers/testrailHelper';
+import * as fs from 'fs';
+import { loginAccount } from 'helpers/accountHelpers';
+const runId = fs.readFileSync('testRunId.txt', 'utf-8');
+
+test('Verify admin user can access Top Issues page', async ({page}) => {
+    const caseId = 27;
+    await skipNonQA(test);
+
+    const testAdmin = process.env.TEST_USER_ADMIN;
+    const testAdminPassword = process.env.TEST_USER_ADMIN_PASSWORD;
+
+    try {
+        await loginAccount(page, testAdmin, testAdminPassword);
+        await page.waitForLoadState('networkidle');
+        await page.goto('/admin');
+        await page.getByRole('button', { name: 'Top Issues' }).isVisible();
+        await page.getByRole('button', { name: 'Top Issues' }).click();
+
+        // Add a new issue
+        await page.getByRole('button', { name: 'Add a Top Issue' }).click();
+        await page.getByLabel('Top Issue Name').fill('Test Issue');
+        await page.getByRole('button', { name: 'Save' }).click();
+
+        // Add a new position
+        await page.getByRole('button', { name: /Add a position for/}).first().click();
+        await page.getByLabel('Position Name').fill('Test Position');
+        await page.getByRole('button', { name: 'Save New Position' }).click();
+
+        // Refresh page
+        await page.reload({ waitUntil: 'domcontentloaded' });
+
+        // Verify new issue and position is saved
+        await page.getByRole('button', { name: 'Add a position for Test Issue' }).isVisible();
+        await page.getByText('Test Position').isVisible();
+
+        // Delete issue and position
+        await page.locator('div.flex.items-center')
+            .filter({ hasText: 'Test Issue' })
+            .locator('div.text-red-600.inline-block')
+            .click();
+        await page.getByRole('heading', { name: 'Delete Issue?' }).isVisible();
+        await page.getByRole('button', { name: 'Proceed' }).click();
+        await page.getByText('Deleting...').isVisible();
+
+        await page.reload({ waitUntil: 'domcontentloaded' });
+
+        // Verify new issue and position is deleted
+        await page.getByRole('button', { name: 'Add a position for Test Issue' }).isHidden();
+        await page.getByText('Test Position').isHidden();
+
+        // Report test results
+        await addTestResult(runId, caseId, 1, 'Test passed');
+
+    } catch (error) {
+
+        // Report test results
+        await addTestResult(runId, caseId, 5, `Test failed: ${error.stack}`);
+    }
+});
