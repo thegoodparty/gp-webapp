ec73e55212e1cef59483e7e617008b11718399cc
Sanjeev Nayak <snayak00@gmail.com>
2025-02-06 17:06:05 -0500

    WEB-3605: revert vercel env var

--- gpApi/clientFetch.js
+++ gpApi/clientFetch.js
@@ -1,5 +1,16 @@
 import { compile, parse } from 'path-to-regexp';
-import { apiUrl } from './routes';
+
+// CI environment variable is a flag provided by Vercel CI/CD to indicate runtime is during build.
+//   If CI is true, then the API base is set to the NEXT_PUBLIC_API_BASE environment variable since
+//   the Next.js app is currently being built and cannot be talked to, so build requests for static content
+//   data should be directed to the API base, not the Next.js application proxy
+export const apiBase = Boolean(process.env.CI)
+  ? process.env.NEXT_PUBLIC_API_BASE
+  : process.env.NEXT_PUBLIC_APP_BASE;
+
+const versionBase = '/api/v1';
+
+export const apiUrl = apiBase + versionBase;
 
 const IS_LOCAL_ENVIRONMENT =
   Boolean(
