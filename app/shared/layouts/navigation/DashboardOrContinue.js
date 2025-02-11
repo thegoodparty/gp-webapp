edc44893d7758a1b84bfeae0a0ad044d04a42a26
tomer-tgp <tomer@goodparty.org>
2025-02-11 11:53:14 -0500

    fix: 404 for account type

--- app/shared/layouts/navigation/DashboardOrContinue.js
+++ app/shared/layouts/navigation/DashboardOrContinue.js
@@ -13,7 +13,7 @@ export default function DashboardOrContinue({ closeAll, campaignStatus }) {
   if (!status) {
     return (
       <Button
-        href={`/onboarding/${step || 'account-type'}`}
+        href={`/onboarding/${step || '1'}`}
         onClick={closeAll}
         id="nav-continue-setup"
         color="secondary"
