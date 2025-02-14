63d6386d621e01ffc5b7b7113b76ddeec1f8acf7
Matthew Marcus <lurchbulldog@gmail.com>
2025-02-14 12:31:03 -0600

    Revert "Qa"

--- app/(candidate)/onboarding/[slug]/[step]/components/ballotOffices/BallotRaces.js
+++ app/(candidate)/onboarding/[slug]/[step]/components/ballotOffices/BallotRaces.js
@@ -154,7 +154,7 @@ export default function BallotRaces(props) {
         </div>
       ) : (
         <div className="mt-6">
-          {Array.isArray(races) &&
+          {races &&
             races.map((race, index) => (
               <RaceCard
                 key={index}
