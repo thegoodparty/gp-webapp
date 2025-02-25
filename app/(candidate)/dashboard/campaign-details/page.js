52569466887f08b762c8a326b9f9bff8d4b14c7a
oakinh <nikaohill@gmail.com>
2025-02-25 16:04:10 -0600

    Force certain variables like candidatePositions to default to an array

--- app/(candidate)/dashboard/campaign-details/page.js
+++ app/(candidate)/dashboard/campaign-details/page.js
@@ -19,10 +19,14 @@ export default async function Page({ params, searchParams }) {
   await candidateAccess();
 
   const campaign = await fetchUserCampaign();
-  const candidatePositions = await serverLoadCandidatePosition(campaign.id);
+  let candidatePositions = await serverLoadCandidatePosition(campaign.id);
   const topIssues = await serverFetchIssues();
   const user = getServerUser(); // can be removed when door knocking app is not for admins only
-
+  console.log('candidatePositions from serverLoadCandidatePosition was: ', candidatePositions);
+  if (candidatePositions === undefined || candidatePositions === null) {
+    candidatePositions = []
+  }
+  console.log('candidatePositions is now: ', candidatePositions);
   const childProps = {
     pathname: '/dashboard/campaign-details',
     campaign,
