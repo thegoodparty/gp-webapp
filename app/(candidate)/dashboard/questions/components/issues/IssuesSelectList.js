c7548966faf04fefb7603d6322772d91b7484281
tomer-tgp <tomer@goodparty.org>
2025-02-25 19:28:02 -0500

    fix: question missing issues

--- app/(candidate)/dashboard/questions/components/issues/IssuesSelectList.js
+++ app/(candidate)/dashboard/questions/components/issues/IssuesSelectList.js
@@ -1,6 +1,9 @@
 import { IssueSelectItem } from './IssueSelectItem';
 
-export const IssuesSelectList = ({ issues, handleSelectIssue = () => {} }) =>
+export const IssuesSelectList = ({
+  issues = [],
+  handleSelectIssue = () => {},
+}) =>
   issues.map((issue) => (
     <IssueSelectItem
       key={issue.id}
