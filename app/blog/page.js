22da114306f1eb25105e6097c9d0f1bdec06c8a2
tomer-tgp <tomer@goodparty.org>
2025-02-26 22:22:07 -0500

    fix: blog caching

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,17 +1,15 @@
 import pageMetaData from 'helpers/metadataHelper';
 import { fetchArticlesBySections } from 'app/blog/shared/fetchArticlesBySections';
 import BlogPage from './components/BlogPage';
-import { serverFetch } from 'gpApi/serverFetch';
-import { apiRoutes } from 'gpApi/routes';
+import gpApi from 'gpApi';
+import gpFetch from 'gpApi/gpFetch';
 
 export const fetchTopTags = async () => {
+  const api = gpApi.content.contentByKey;
   const payload = {
-    type: 'blogHome',
+    key: 'blogHome',
   };
-  const resp = await serverFetch(apiRoutes.content.getByType, payload, {
-    revalidate: 3600,
-  });
-  return resp.data;
+  return await gpFetch(api, payload, 3600);
 };
 
 const meta = pageMetaData({
@@ -21,9 +19,22 @@ const meta = pageMetaData({
 });
 export const metadata = meta;
 
+// This makes the page static
+export const dynamic = 'force-static';
+
+// This caches the page on the edge
+export const runtime = 'edge';
+
+export const revalidate = 3600; // revalidate every hour
+
+// If your page accepts dynamic parameters, you need to define them here
+export async function generateStaticParams() {
+  return [{}]; // For a single page with no parameters
+}
+
 export default async function Page({ params, searchParams }) {
   const { sections, hero } = await fetchArticlesBySections();
-  const { tags } = await fetchTopTags();
+  const { content: { tags } = {} } = await fetchTopTags();
 
   return <BlogPage sections={sections} hero={hero} topTags={tags} />;
 }
