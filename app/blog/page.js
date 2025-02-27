882b498feb3ea89ffc3cb0b53ee0517608436b7c
tomer-tgp <tomer@goodparty.org>
2025-02-26 22:34:36 -0500

    fix: top tags

--- app/blog/page.js
+++ app/blog/page.js
@@ -4,13 +4,13 @@ import BlogPage from './components/BlogPage';
 import gpApi from 'gpApi';
 import gpFetch from 'gpApi/gpFetch';
 
-export const fetchTopTags = async () => {
-  const api = gpApi.content.contentByKey;
-  const payload = {
-    key: 'blogHome',
-  };
-  return await gpFetch(api, payload, 3600);
-};
+// export const fetchTopTags = async () => {
+//   const api = gpApi.content.contentByKey;
+//   const payload = {
+//     key: 'blogHome',
+//   };
+//   return await gpFetch(api, payload, 3600);
+// };
 
 const meta = pageMetaData({
   title: 'Blog | GoodParty.org',
@@ -34,7 +34,7 @@ export async function generateStaticParams() {
 
 export default async function Page({ params, searchParams }) {
   const { sections, hero } = await fetchArticlesBySections();
-  const { content: { tags } = {} } = await fetchTopTags();
+  // const { content: { tags } = {} } = await fetchTopTags();
 
-  return <BlogPage sections={sections} hero={hero} topTags={tags} />;
+  return <BlogPage sections={sections} hero={hero} />;
 }
