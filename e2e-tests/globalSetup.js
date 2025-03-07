8f7d65d70e20413a634d7014957b1845b723ae22
Dustin Sison <dsison34@gmail.com>
2025-03-06 17:44:13 -0800

    Minor test updates

--- e2e-tests/globalSetup.js
+++ e2e-tests/globalSetup.js
@@ -6,7 +6,7 @@ import "dotenv/config";
 module.exports = async () => {
   const testRunName = process.env.TEST_RUN_NAME || "Localized Test Run (Debugging)";
   let testCaseIds = [
-    1, 2, 4, 5, 7, 8, 12, 16, 17, 
+    1, 4, 5, 7, 8, 12, 16, 17, 
     18, 19, 22, 
     24, 25, 26, 27, 29, 30, 31, 32, 33, 34, 35, 36, 37,
     38, 39, 40, 41, 42, 43, 46, 47, 48, 49, 50, 53, 54, 75,
