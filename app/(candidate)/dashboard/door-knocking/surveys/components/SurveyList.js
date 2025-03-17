adf8187bbd11276eaf32eb0d516910b7b0d61c5c
tomer-tgp <tomer@goodparty.org>
2025-03-17 14:02:27 -0400

    fix: is array

--- app/(candidate)/dashboard/door-knocking/surveys/components/SurveyList.js
+++ app/(candidate)/dashboard/door-knocking/surveys/components/SurveyList.js
@@ -27,14 +27,15 @@ export default function SurveyList(props) {
         <CreateSurvey teams={props.teams} createCallback={reFetchSurveys} />
       </div>
       <div className="grid grid-cols-12 gap-4 mt-8">
-        {surveys?.map((survey) => (
-          <div
-            className="col-span-12 md:col-span-6 lg:col-span-4"
-            key={survey.id}
-          >
-            <SurveyCard survey={survey} />
-          </div>
-        ))}
+        {Array.isArray(surveys) &&
+          surveys.map((survey) => (
+            <div
+              className="col-span-12 md:col-span-6 lg:col-span-4"
+              key={survey.id}
+            >
+              <SurveyCard survey={survey} />
+            </div>
+          ))}
       </div>
     </>
   );
