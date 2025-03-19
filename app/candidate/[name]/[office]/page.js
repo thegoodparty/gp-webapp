cae850308b55e55d4597d247142c0e782b8602bd
tomer-tgp <tomer@goodparty.org>
2025-03-19 12:15:30 -0400

    chore: replace redirect with permanent redirect

--- app/candidate/[name]/[office]/page.js
+++ app/candidate/[name]/[office]/page.js
@@ -2,7 +2,7 @@ import pageMetaData from 'helpers/metadataHelper';
 import CandidatePage from './components/CandidatePage';
 import gpApi from 'gpApi';
 import gpFetch from 'gpApi/gpFetch';
-import { redirect } from 'next/navigation';
+import { permanentRedirect } from 'next/navigation';
 import CandidateSchema from './components/CandidateSchema';
 
 export const revalidate = 3600;
@@ -45,54 +45,9 @@ export default async function Page({ params, searchParams }) {
     bustCache === 'true',
   );
   if (!candidate) {
-    redirect('/candidates');
+    permanentRedirect('/candidates');
   }
 
-  // const candidate = {
-  //   firstName: 'John',
-  //   lastName: 'Doe',
-  //   office: 'City Council, District 7',
-  //   party: 'Independent',
-  //   city: 'Manchester',
-  //   state: 'NH',
-  //   votesNeeded: 5402,
-  //   votersCount: 25321,
-  //   independents: 8414,
-  //   republicans: 9623,
-  //   democrats: 7685,
-  //   about: (
-  //     <div>
-  //       Will Lorem Ipsum SEO, Combined SEO AI, Information We combine using
-  //       ballot data, and L2, and then we ask chatgpt to combine and create an
-  //       about, and this section will probably be totally wrong but it&apos;s
-  //       okay.
-  //       <br />
-  //       <br />
-  //       Will Lorem Ipsum SEO, Combined SEO AI, Information We combine using
-  //       ballot data, and L2, and then we ask chatgpt to combine and create an
-  //       about, and this section will probably be totally wrong but it&apos;s
-  //       okay.
-  //     </div>
-  //   ),
-  //   jobHistory: 'Physics Teacher at Manchester High',
-  //   education: 'BS Physics from University of Chicago, Illinois',
-  //   militaryService: 'None',
-  //   previouslyInOffice: 'Yes',
-  //   priorRoles: [
-  //     { office: 'City Clerk', years: '2022 - 2024' },
-  //     { office: 'Alderman', years: '2020 - 2022' },
-  //   ],
-  //   yearsInOffice: 4,
-  //   officeDescription:
-  //     'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.',
-  //   term: '4 Years',
-  //   salary: 'Average $82,000/ year',
-  //   topIssues: [
-  //     'Lorem ipsum dolor sit amet.',
-  //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
-  //   ],
-  // };
   const childProps = { candidate };
   return (
     <>
