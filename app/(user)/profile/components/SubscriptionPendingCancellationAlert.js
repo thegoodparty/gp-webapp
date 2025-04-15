c8a08b891c0c582082723edee2788394d715bb62
Matthew Marcus <lurchbulldog@gmail.com>
2025-04-15 13:56:44 -0500

    [WEB-3975] - Update cancellation message to support both auto cancel date and cacnellation requested by user

--- app/(user)/profile/components/SubscriptionPendingCancellationAlert.js
+++ app/(user)/profile/components/SubscriptionPendingCancellationAlert.js
@@ -1,6 +1,5 @@
 'use client'
 import { InfoAlert } from '@shared/alerts/InfoAlert'
-import H4 from '@shared/typography/H4'
 import Body2 from '@shared/typography/Body2'
 import { dateUSClientLocaleHelper } from 'helpers/dateHelper'
 
@@ -10,9 +9,8 @@ export const SubscriptionPendingCancellationAlert = ({
   <InfoAlert className="mb-4" variant="outlined" severity="info">
     <div className="flex flex-col flex-grow lg:justify-between lg:flex-row">
       <div className="p-2 mr-2 lg:max-w-[73%]">
-        <H4 className="mb-2">Cancellation Received</H4>
         <Body2 className="">
-          You still have access to pro features until{' '}
+          Your subscription will end on{' '}
           <strong>{dateUSClientLocaleHelper(subscriptionCancelAt)}</strong>
         </Body2>
       </div>
