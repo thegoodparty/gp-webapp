bbdad457407d411c958c64ee10b969c88f47feaa
tomer-tgp <tomer@goodparty.org>
2025-05-09 13:26:00 -0400

    chore: handle no about section

--- app/candidate/[name]/[office]/components/ContentSection.js
+++ app/candidate/[name]/[office]/components/ContentSection.js
@@ -5,16 +5,19 @@ import TopIssuesCard from './TopIssuesCard'
 
 export default function ContentSection(props) {
   const { candidate } = props
+  const { about } = candidate
 
   return (
     <div className="grid grid-cols-12 gap-4 mb-4 pt-5">
       <div className="col-span-12">
         <FreeToolsCard />
       </div>
-      <div className="col-span-12 md:col-span-6">
-        <AboutCard {...props} />
-      </div>
-      <div className="col-span-12 md:col-span-6">
+      {about && (
+        <div className="col-span-12 md:col-span-6">
+          <AboutCard {...props} />
+        </div>
+      )}
+      <div className={`col-span-12  ${about ? 'md:col-span-6' : ''}`}>
         <OfficeCard {...props} />
         <TopIssuesCard candidate={candidate} maxToShow={3} />
       </div>
