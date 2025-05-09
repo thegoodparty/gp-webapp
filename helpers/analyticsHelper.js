f5c85684cd127d3e6c54ab4ef204aeb66c814a3c
oakinh <nikaohill@gmail.com>
2025-05-09 10:30:47 -0500

    Remove console.debug

--- helpers/analyticsHelper.js
+++ helpers/analyticsHelper.js
@@ -348,7 +348,6 @@ export const EVENTS = {
 export const trackEvent = (name, properties) => {
   // TODO: Repurpose this file and function for Segment when we get the green light to rip out FS
   try {
-    console.debug('[TRACKING]', name, properties)
     // Segment has different environments, and should run even when FS is disabled
     segmentTrackEvent(name, properties)
     if (typeof FS === 'undefined') {
