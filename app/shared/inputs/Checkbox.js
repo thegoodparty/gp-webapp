3c63d9ccf82d84aaf2fee5f4690903f94fadda78
tomer-tgp <tomer@goodparty.org>
2025-05-13 05:48:28 -0400

    chore: pr review

--- app/shared/inputs/Checkbox.js
+++ app/shared/inputs/Checkbox.js
@@ -4,17 +4,10 @@ import MuiCheckbox from '@mui/material/Checkbox'
 
 const Checkbox = (props) => {
   const { label, ...restProps } = props
-  return (
-    <>
-      {label ? (
-        <FormControlLabel
-          label={label}
-          control={<MuiCheckbox {...restProps} />}
-        />
-      ) : (
-        <MuiCheckbox variant="outlined" {...props} />
-      )}
-    </>
+  return label ? (
+    <FormControlLabel label={label} control={<MuiCheckbox {...restProps} />} />
+  ) : (
+    <MuiCheckbox variant="outlined" {...props} />
   )
 }
 
