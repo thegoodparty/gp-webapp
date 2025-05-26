a442b9c41b015205971f95c8d15d423532b78825
tomer-tgp <tomer@goodparty.org>
2025-05-26 19:45:54 -0400

    chore: hide pagination

--- app/shared/utils/Table.js
+++ app/shared/utils/Table.js
@@ -185,7 +185,7 @@ export default function Table({
           ))}
         </tbody>
       </table>
-      {showPagination && (
+      {showPagination && table.getRowModel().rows?.length > 0 && (
         <div className="flex items-center justify-center my-4">
           <button
             className="px-2 py-1 mx-1 bg-slate-600 text-white font-black rounded"
