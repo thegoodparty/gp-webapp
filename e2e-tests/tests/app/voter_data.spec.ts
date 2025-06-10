fcf94c0b942de33254ad028eb5ea4d03a0a5a590
dustinsison <dsison34@gmail.com>
2025-06-10 16:04:28 -0700

    Disabled pro upgrade test

--- e2e-tests/tests/app/voter_data.spec.ts
+++ e2e-tests/tests/app/voter_data.spec.ts
@@ -7,36 +7,36 @@ import { documentReady } from 'helpers/domHelpers';
 const runId = fs.readFileSync('testRunId.txt', 'utf-8');
 
 test.use({
-    storageState: 'auth.json',
+  storageState: 'auth.json',
 });
 
 test.beforeEach(async ({ page }) => {
-    await page.goto("/dashboard/voter-records");
-    await documentReady(page);
+  await page.goto("/dashboard/voter-records");
+  await documentReady(page);
 });
 
 test.describe.serial('Voter data pro features', () => {
-    test.skip(
-        !process.env.BASE_URL?.match(/^https?:\/\/(dev\.|qa\.|)goodparty\.org/),
-        'Skipping GoodParty.org PRO / voter data tests on non-production environments'
-    );
+  test.skip(
+    !process.env.BASE_URL?.match(/^https?:\/\/(dev\.|qa\.|)goodparty\.org/),
+    'Skipping GoodParty.org PRO / voter data tests on non-production environments'
+  );
 
-    test('Voter Data shows Upgrade to Pro prompt for free users', async ({ page }) => {
-        const caseId = 41;
+  test('Voter Data shows Upgrade to Pro prompt for free users', async ({ page }) => {
+    const caseId = 41;
 
-        try {
-            // Verify user is on voter data (free) page
-            await expect(page.getByRole('heading', { name: 'Why pay more for less?' })).toBeVisible();
-            await page.getByRole('link', { name: 'Start today for just $10/month.' }).click();
+    try {
+      // Verify user is on voter data (free) page
+      await expect(page.getByRole('heading', { name: 'Why pay more for less?' })).toBeVisible();
+      await page.getByRole('link', { name: 'Start today for just $10/month.' }).click();
 
-            // Report test results
-            await addTestResult(runId, caseId, 1, 'Test passed');
-        } catch (error) {
-            await handleTestFailure(page, runId, caseId, error);    
-        }
-    });
+      // Report test results
+      await addTestResult(runId, caseId, 1, 'Test passed');
+    } catch (error) {
+      await handleTestFailure(page, runId, caseId, error);
+    }
+  });
 
-  test('Upgrade user to Pro', async ({ page }) => {
+  test.skip('Upgrade user to Pro', async ({ page }) => {
     const caseId = 42;
     try {
       await upgradeToPro(page);
@@ -44,7 +44,7 @@ test.describe.serial('Voter data pro features', () => {
       // Report test results
       await addTestResult(runId, caseId, 1, 'Test passed');
     } catch (error) {
-      await handleTestFailure(page, runId, caseId, error);    
+      await handleTestFailure(page, runId, caseId, error);
     }
   });
 
@@ -64,7 +64,7 @@ test.describe.serial('Voter data pro features', () => {
       // Report test results
       await addTestResult(runId, caseId, 1, 'Test passed');
     } catch (error) {
-      await handleTestFailure(page, runId, caseId, error);    
+      await handleTestFailure(page, runId, caseId, error);
     }
   });
 });
