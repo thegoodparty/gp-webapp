2ec5fbd33499442fa71f9eb52bdc2ca9a158d23d
oakinh <nikaohill@gmail.com>
2025-06-18 10:41:58 -0500

    .

--- app/(candidate)/onboarding/[slug]/[step]/components/ballotOffices/CustomOfficeForm.js
+++ app/(candidate)/onboarding/[slug]/[step]/components/ballotOffices/CustomOfficeForm.js
@@ -9,6 +9,9 @@ import {
 import Button from '@shared/buttons/Button'
 import H1 from '@shared/typography/H1'
 import Body2 from '@shared/typography/Body2'
+import { useAnalytics } from '@shared/hooks/useAnalytics'
+import { trackEvent } from 'helpers/analyticsHelper'
+import { useUser } from '@shared/hooks/useUser'
 
 const fields = [
   {
@@ -65,6 +68,8 @@ export default function CustomOfficeForm({ campaign, onSave, onBack }) {
     ballotOffice: campaign.details?.ballotOffice || false,
     electionDate: campaign.details?.electionDate || '',
   })
+  const user = useUser()
+  const analytics = useAnalytics()
   const now = new Date()
   const selectedDate = dateFromNonStandardUSFormatString(state['electionDate'])
   const error =
@@ -100,6 +105,16 @@ export default function CustomOfficeForm({ campaign, onSave, onBack }) {
       electionDate: state.electionDate,
     }
     onSave(updated)
+
+    const trackingProperties = {
+      state: state.state,
+      officeMunicipality: state.city,
+      officeName: state.office,
+      officeElectionDate: state.electionDate,
+      officeManuallyInput: true,
+    }
+    analytics.identify(user.id, trackingProperties)
+    trackEvent('Onboarding - Candidate Office Completed', trackingProperties)
   }
 
   return (
