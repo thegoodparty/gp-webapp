40a6e62835caa3d96426be01c9f1d5a4663d5b56
Sanjeev Nayak <snayak00@gmail.com>
2025-06-19 14:02:14 -0400

    WEB-4208: part 1 changes

---
+++ app/(candidate)/dashboard/website/components/EmptyState.js
@@ -0,0 +1,53 @@
+import H1 from '@shared/typography/H1'
+import Body2 from '@shared/typography/Body2'
+import Paper from '@shared/utils/Paper'
+import IntroSteps from './IntroSteps'
+import Button from '@shared/buttons/Button'
+import Caption from '@shared/typography/Caption'
+
+const STEPS = [
+  {
+    number: 1,
+    title: 'Pick your domain',
+    description: 'Choose your built-in GoodParty.org domain',
+  },
+  {
+    number: 2,
+    title: 'Design your site',
+    description: 'Customize content to make it uniquely yours',
+  },
+  {
+    number: 3,
+    title: 'Publish & share',
+    description: 'Go live and start reaching voters',
+  },
+]
+
+export default function EmptyState({ onClickCreate, createLoading }) {
+  return (
+    <div className="pt-4 px-2 text-left md:text-center">
+      <H1>Create your free website</H1>
+      <Body2 className="mt-2">
+        Design a professional website to showcase your campaign, connect with
+        voters, and share your vision for the community.
+      </Body2>
+      <Paper className="mt-8">
+        <div className="flex flex-col gap-8 py-2">
+          <IntroSteps steps={STEPS} />
+        </div>
+      </Paper>
+      <Button
+        color="secondary"
+        className="mt-12 w-full md:w-auto px-10"
+        onClick={onClickCreate}
+        disabled={createLoading}
+        loading={createLoading}
+      >
+        {createLoading ? 'Creating...' : 'Create your website'}
+      </Button>
+      <Caption className="mt-4 text-center font-medium text-gray-700">
+        Estimated time: 10 minutes
+      </Caption>
+    </div>
+  )
+}
