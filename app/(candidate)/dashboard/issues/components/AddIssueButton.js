d929adec22b0ddb4dac85bc3d79708d269a9ff25
Tomer Almog <tomeralmog@gmail.com>
2025-07-01 15:51:19 -0400

    chore: move href to button

--- app/(candidate)/dashboard/issues/components/AddIssueButton.js
+++ app/(candidate)/dashboard/issues/components/AddIssueButton.js
@@ -1,16 +1,13 @@
 import Button from '@shared/buttons/Button'
-import Link from 'next/link'
 import { FaPlus } from 'react-icons/fa'
 
 export default function AddIssueButton() {
   return (
-    <Link href="/dashboard/issues/add-issue">
-      <Button color="info">
-        <div className="flex items-center gap-2">
-          <FaPlus />
-          Add an issue
-        </div>
-      </Button>
-    </Link>
+    <Button color="info" href="/dashboard/issues/add-issue">
+      <div className="flex items-center gap-2">
+        <FaPlus />
+        Add an issue
+      </div>
+    </Button>
   )
 }
