af26ef0aca2082161811c48934a9a2e1686cc5f3
Collin-GP <collin@goodparty.org>
2025-07-03 15:15:20 -0600

    WEB-4244 update regex so we capture digits as well as the letters in a camel case. before: one4You would return one4you but now it will return one4-you on the return trip to go back to camel case the original would turn into one4you, the second case would reform the original one4You (#744)
    Co-authored-by: Collin Park <collinpark@Collins-MacBook-Air.local>

--- helpers/stringHelper.js
+++ helpers/stringHelper.js
@@ -38,7 +38,7 @@ export function camelToKebab(camelCase) {
   if (!camelCase || camelCase == '') {
     return '';
   }
-  return camelCase.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+  return camelCase.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase();
 }
 
 export function toTitleCase(str) {
