7c459fbe0dbb98840fdfab679f848fe5b92ef006
Tomer Almog <tomer@goodparty.org>
2025-07-07 14:03:35 -0400

    chore: more PR comments

--- app/(candidate)/dashboard/issues/components/search/ViewModeToggle.js
+++ app/(candidate)/dashboard/issues/components/search/ViewModeToggle.js
@@ -3,22 +3,23 @@ import { ButtonGroup } from '@mui/material'
 import Button from '@shared/buttons/Button'
 import { FaListUl } from 'react-icons/fa'
 import { TbLayoutGrid } from 'react-icons/tb'
+import { VIEW_MODES } from '../../shared/constants'
 
 export default function ViewModeToggle({ value, onChange }) {
   return (
     <div className="flex bg-neutral-light p-1 rounded-md">
       <ButtonGroup variant="outlined" size="small">
         <Button
-          onClick={() => onChange('list')}
-          color={value === 'list' ? 'white' : 'neutral'}
+          onClick={() => onChange(VIEW_MODES.LIST)}
+          color={value === VIEW_MODES.LIST ? 'white' : 'neutral'}
           className="mr-2 flex items-center"
         >
           <FaListUl />
           <span className="ml-2">List</span>
         </Button>
         <Button
-          onClick={() => onChange('board')}
-          color={value === 'board' ? 'white' : 'neutral'}
+          onClick={() => onChange(VIEW_MODES.BOARD)}
+          color={value === VIEW_MODES.BOARD ? 'white' : 'neutral'}
           className="flex items-center"
         >
           <TbLayoutGrid />
