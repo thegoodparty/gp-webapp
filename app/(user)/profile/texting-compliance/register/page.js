cfc199e5d3c4534475656bea2e2ac938ec3affe1
Matthew Marcus <lurchbulldog@gmail.com>
2025-07-21 23:47:55 -0500

    [WEB-4341] - added missing files to commit

---
+++ app/(user)/profile/texting-compliance/register/page.js
@@ -0,0 +1,15 @@
+import pageMetaData from 'helpers/metadataHelper'
+
+const meta = pageMetaData({
+  title: 'Register - Texting Compliance | GoodParty.org',
+  description: 'Register for texting compliance.',
+})
+export const metadata = meta
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Register for Texting Compliance</h1>
+    </div>
+  )
+} 
---
+++ app/(user)/profile/texting-compliance/submit-pin/page.js
@@ -0,0 +1,15 @@
+import pageMetaData from 'helpers/metadataHelper'
+
+const meta = pageMetaData({
+  title: 'Submit PIN - Texting Compliance | GoodParty.org',
+  description: 'Submit PIN for texting compliance verification.',
+})
+export const metadata = meta
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Submit PIN</h1>
+    </div>
+  )
+} 
