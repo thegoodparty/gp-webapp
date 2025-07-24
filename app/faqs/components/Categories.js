6266fff1f24de7de09c3df1448679d13672594bf
Israel Vicars <israelvicars@gmail.com>
2025-07-24 01:26:43 -0400

    feat: order faq articles within faq categories

--- app/faqs/components/Categories.js
+++ app/faqs/components/Categories.js
@@ -19,19 +19,31 @@ export default function Categories({ content }) {
               {category.fields.name}
             </div>
             {category.articles &&
-              category.articles.map((article) => (
-                <div data-cy="faq-article" key={article.id}>
-                  <Link
-                    href={faqArticleRoute(article)}
-                    data-cy="faq-article-link"
-                    className="block"
+              category.articles
+                .sort((a, b) => (a.order || 9999) - (b.order || 9999))
+                .map((article) => (
+                  <div
+                    data-cy="faq-article"
+                    key={article.id}
+                    data-order={article.order || 'unordered'}
                   >
-                    <div className="py-2 pl-10" data-cy="faq-article-title">
-                      {article.title}
-                    </div>
-                  </Link>
-                </div>
-              ))}
+                    <Link
+                      href={faqArticleRoute(article)}
+                      data-cy="faq-article-link"
+                      className="block"
+                    >
+                      <div className="py-2 pl-10" data-cy="faq-article-title">
+                        {process.env.NODE_ENV === 'development' &&
+                          article.order && (
+                            <span className="text-xs text-gray-400 mr-2">
+                              [{article.order}]
+                            </span>
+                          )}
+                        {article.title}
+                      </div>
+                    </Link>
+                  </div>
+                ))}
           </div>
         ))}
     </div>
