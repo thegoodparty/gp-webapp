8c055d6f27125b499db27f3df4c2d9a26df15e27
Tomer Almog <tomer@goodparty.org>
2025-07-30 06:38:09 -0400

    chore: cleanup

--- app/(candidate)/dashboard/website/util/domainFetch.util.js
+++ app/(candidate)/dashboard/website/util/domainFetch.util.js
@@ -5,18 +5,6 @@ export function searchDomains(domain) {
   return clientFetch(apiRoutes.domain.search, { domain })
 }
 
-export function registerDomain(domain) {
-  return clientFetch(apiRoutes.domain.register, { domain })
-}
-
-export function completeRegistration() {
-  return clientFetch(apiRoutes.domain.completeRegistration)
-}
-
 export function deleteDomain() {
   return clientFetch(apiRoutes.domain.delete)
-}
-
-export function getDomainStatus() {
-  return clientFetch(apiRoutes.domain.status)
 }
