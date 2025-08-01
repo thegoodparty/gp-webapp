2327205dd0fb041c60053929b86f3c92651df201
Tomer Almog <tomer@goodparty.org>
2025-08-01 16:38:39 -0400

    fix: copy

--- app/(candidate)/dashboard/outreach/components/CopyScriptActionOption.js
+++ app/(candidate)/dashboard/outreach/components/CopyScriptActionOption.js
@@ -1,20 +1,32 @@
+'use client'
+import { useCampaign } from '@shared/hooks/useCampaign'
 import CopyToClipboard from '@shared/utils/CopyToClipboard'
 import { OutreachActionWrapper } from 'app/(candidate)/dashboard/outreach/components/OutreachActionWrapper'
+import { htmlToPlainText } from 'helpers/stringHelper'
 import { MdContentCopy } from 'react-icons/md'
 
 export const CopyScriptActionOption = ({
   outreach = {},
   onCopy = () => {},
-}) => (
-  <CopyToClipboard
-    {...{
-      text: outreach?.script || '',
-      onCopy,
-    }}
-  >
-    <OutreachActionWrapper>
-      <MdContentCopy className="mr-2 inline-block" />
-      Copy Script
-    </OutreachActionWrapper>
-  </CopyToClipboard>
-)
+}) => {
+  const [campaign] = useCampaign()
+
+  const text =
+    campaign?.aiContent && outreach && campaign?.aiContent[outreach?.script]
+      ? htmlToPlainText(campaign?.aiContent[outreach?.script].content)
+      : outreach?.script
+
+  return (
+    <CopyToClipboard
+      {...{
+        text,
+        onCopy,
+      }}
+    >
+      <OutreachActionWrapper>
+        <MdContentCopy className="mr-2 inline-block" />
+        Copy Script
+      </OutreachActionWrapper>
+    </CopyToClipboard>
+  )
+}
