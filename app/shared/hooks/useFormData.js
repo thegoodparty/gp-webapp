011d28e1eb7d85191be16500ef9b34a7c583afdc
Matthew Marcus <lurchbulldog@gmail.com>
2025-08-12 10:32:51 -0500

    [WEB-4433] - minor cleanup

--- app/shared/hooks/useFormData.js
+++ app/shared/hooks/useFormData.js
@@ -1,7 +1,7 @@
 'use client'
 import { createContext, useContext, useState } from 'react'
 
-const FormDataContext = createContext()
+const FormDataContext = createContext({})
 
 export const FormDataProvider = ({
   children,
