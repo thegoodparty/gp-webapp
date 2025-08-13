1ec8f3b5d81f10aadddbd6060fd009852055a667
Tomer Almog <tomer@goodparty.org>
2025-08-13 05:11:14 -0400

    chore: claimed heart

--- app/candidate/[name]/[office]/components/CandidatImage.js
+++ app/candidate/[name]/[office]/components/CandidatImage.js
@@ -3,15 +3,17 @@
 import { usePublicCandidate } from './PublicCandidateProvider'
 import Image from 'next/image'
 import { IoPersonSharp } from 'react-icons/io5'
+import ClaimedHeart from './ClaimedHeart'
 
 export default function CandidateImage() {
   const [candidate] = usePublicCandidate()
-  const { firstName, lastName, image } = candidate
+  const { firstName, lastName, image, claimed } = candidate
+  console.log('claimed', claimed)
 
   return (
     <div className="lg:-mb-40">
       {image ? (
-        <div className="bg-indigo-400 inline-block border border-white rounded-full relative w-40 h-40 lg:w-60 lg:h-60">
+        <div className="bg-indigo-400 inline-block border-2 lg:border-4 border-white rounded-full relative w-40 h-40 lg:w-60 lg:h-60">
           <Image
             src={image}
             fill
@@ -20,10 +22,12 @@ export default function CandidateImage() {
             unoptimized
             className="rounded-full w-40 h-40 lg:w-60 lg:h-60 object-cover object-center"
           />
+          <ClaimedHeart />
         </div>
       ) : (
-        <div className="bg-indigo-400  border border-white rounded-full p-4 w-40 h-40 lg:w-60 lg:h-60 flex items-center justify-center text-7xl lg:text-9xl">
+        <div className="bg-indigo-400  border-2 lg:border-4 border-white rounded-full p-4 w-40 h-40 lg:w-60 lg:h-60 flex items-center justify-center text-7xl lg:text-9xl relative">
           <IoPersonSharp />
+          <ClaimedHeart />
         </div>
       )}
     </div>
