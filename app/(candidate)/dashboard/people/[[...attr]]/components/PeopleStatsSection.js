09c5d3f74a28cfe0cf4d50941621375bf10db7fe
Tomer Almog <tomer@goodparty.org>
2025-08-30 15:30:50 -0400

    chore: match stats design to Figma

--- app/(candidate)/dashboard/people/[[...attr]]/components/PeopleStatsSection.js
+++ app/(candidate)/dashboard/people/[[...attr]]/components/PeopleStatsSection.js
@@ -2,7 +2,7 @@ import { numberFormatter } from 'helpers/numberHelper'
 import { LuVote } from 'react-icons/lu'
 import { SlPeople } from 'react-icons/sl'
 import Paper from '@shared/utils/Paper'
-import Body1 from '@shared/typography/Body1'
+import Body2 from '@shared/typography/Body2'
 
 const fetchPeopleStats = async () => {
   return {
@@ -27,7 +27,7 @@ export default async function PeopleStatsSection() {
       label: 'Total Constituents',
       value: peopleStats.total ? numberFormatter(peopleStats.total) : 'N/A',
       icon: (
-        <div className="text-2xl text-slate-600 bg-slate-100 rounded flex items-center justify-center h-10 w-10">
+        <div className="text-lg text-slate-600 ">
           <SlPeople />
         </div>
       ),
@@ -39,7 +39,7 @@ export default async function PeopleStatsSection() {
         ? `${peopleStats.politicalMakeup.republican}% Republicans`
         : 'N/A',
       icon: (
-        <div className="text-2xl text-purple-600 bg-purple-100 rounded flex items-center justify-center h-10 w-10">
+        <div className="text-lg text-slate-600 ">
           <LuVote />
         </div>
       ),
@@ -51,7 +51,7 @@ export default async function PeopleStatsSection() {
         ? `${peopleStats.largestCommunity}`
         : 'N/A',
       icon: (
-        <div className="text-2xl text-blue-600 bg-blue-100 rounded flex items-center justify-center h-10 w-10">
+        <div className="text-lg text-slate-600 ">
           <SlPeople />
         </div>
       ),
@@ -63,7 +63,7 @@ export default async function PeopleStatsSection() {
         ? `${peopleStats.largestAgeGroupPercentage}% are ${peopleStats.largestAgeGroup} years old`
         : 'N/A',
       icon: (
-        <div className="text-2xl text-blue-600 bg-blue-100 rounded flex items-center justify-center h-10 w-10">
+        <div className="text-lg text-slate-600 ">
           <SlPeople />
         </div>
       ),
@@ -74,13 +74,12 @@ export default async function PeopleStatsSection() {
     <section className="mt-4 mb-6 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4 ">
       {cards.map((card) => (
         <Paper key={card.key}>
-          <div className="flex items-center gap-4">
+          <div className="flex items-center gap-4 justify-between">
+            <Body2 className="font-medium">{card.label}</Body2>
             <div>{card.icon}</div>
-            <div className="flex flex-col">
-              <Body1 className="text-slate-500">{card.label}</Body1>
-              <Body1 className="font-semibold">{card.value}</Body1>
-            </div>
           </div>
+
+          <h4 className="font-bold mt-1 text-2xl">{card.value}</h4>
         </Paper>
       ))}
     </section>
