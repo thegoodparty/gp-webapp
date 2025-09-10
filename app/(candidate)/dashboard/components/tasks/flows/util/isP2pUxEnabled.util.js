26887402255b0e53142603ba24db6e5656a71f18
Matthew Marcus <lurchbulldog@gmail.com>
2025-09-10 15:16:47 -0500

    fixes P2P feature flag problem (#951)

--- app/(candidate)/dashboard/components/tasks/flows/util/isP2pUxEnabled.util.js
+++ app/(candidate)/dashboard/components/tasks/flows/util/isP2pUxEnabled.util.js
@@ -1,6 +1,6 @@
 import { NEXT_PUBLIC_P2P_CUTOFF_DATETIME } from 'appEnv'
 import { isInvalidDateObject } from 'helpers/dateHelper'
-import { isBefore } from 'date-fns'
+import { isAfter } from 'date-fns'
 
 export const p2pCutoffDatetime = new Date(NEXT_PUBLIC_P2P_CUTOFF_DATETIME)
 
@@ -11,4 +11,4 @@ if (NEXT_PUBLIC_P2P_CUTOFF_DATETIME && isInvalidDateObject(p2pCutoffDatetime)) {
 export const isP2pUxEnabled = (proUpdatedAtDate) =>
   !NEXT_PUBLIC_P2P_CUTOFF_DATETIME ||
   (!isInvalidDateObject(proUpdatedAtDate) &&
-    isBefore(proUpdatedAtDate, p2pCutoffDatetime))
+    isAfter(proUpdatedAtDate, p2pCutoffDatetime))
