8cbee17c8a936d57989c99d250470bfc9cc258e6
Stephen <stephen.tanguis@gmail.com>
2025-09-26 14:22:28 -0500

    Add shared components for serve onboarding
    Add new components to shared directory, which are used in serve onboarding but available for all to use.

---
+++ app/shared/charts/InsightPieChart.js
@@ -0,0 +1,50 @@
+'use client'
+import { PieChart, Pie, Legend, ResponsiveContainer, Cell } from 'recharts'
+import { numberFormatter } from 'helpers/numberHelper'
+import { COLORS } from './constants'
+
+export const InsightPieChart = ({ data, percentage }) => {
+
+    const renderLegend = () => {
+        return (
+            <div className="w-full px-2 text-black" style={{width: '100%'}}>
+                {data.map((item, index) => (
+                    <div key={`legend-row-${item.name}`} className="flex items-center justify-between w-full py-1">
+                        <div className="flex items-center min-w-0">
+                            <span
+                                className="inline-block mr-2 rounded-full"
+                                style={{ backgroundColor: COLORS[index % COLORS.length], width: 8, height: 8 }}
+                            />
+                            <span className="truncate text-xs text-muted-foreground font-normal">{item.name}</span>
+                        </div>
+                        <span className="text-xs font-semibold ml-4">{numberFormatter(item.value)}{percentage ? '%' : ''}</span>
+                    </div>
+                ))}
+            </div>
+        );
+    };
+
+    return (
+        <ResponsiveContainer width="100%" height={350}>
+            <PieChart width={400} height={350} margin={{ top: 0, right: 0, bottom: 24, left: 0 }}>
+                <Pie
+                    data={data}
+                    cx="50%"
+                    cy="45%"
+                    labelLine={false}
+                    label={null}
+                    outerRadius={100}
+                    fill="#8884d8"
+                    stroke="none"
+                    strokeWidth={0}
+                    dataKey="value"
+                >
+                    {data.map((entry, index) => (
+                        <Cell key={`cell-${entry.name}`} fill={COLORS[index % COLORS.length]} />
+                    ))}
+                </Pie>
+                <Legend width="100%" verticalAlign="bottom" align="center" layout="vertical" content={renderLegend} />
+            </PieChart>
+        </ResponsiveContainer>
+    );
+}
---
+++ app/shared/charts/InsightVerticalBarChart.js
@@ -0,0 +1,101 @@
+'use client'
+import { BarChart, XAxis, YAxis, Bar, ResponsiveContainer, Cell, LabelList, CartesianGrid } from 'recharts'
+import { numberFormatter } from 'helpers/numberHelper'
+import { useTailwindBreakpoints } from '../hooks/useTailwindBreakpoints'
+import { COLORS } from './constants'
+
+export const InsightVerticalBarChart = ({ data, percentage }) => {
+
+    const tailwindBreakpoint = useTailwindBreakpoints();
+    const isMobile = tailwindBreakpoint === 'sm' || tailwindBreakpoint === 'xs';
+
+    const renderCustomBar = (props) => {
+        const { fill, x, y, width, height } = props;
+        const radius = 8;
+        const path = `M ${x} ${y + height} 
+                     L ${x} ${y + radius} 
+                     Q ${x} ${y} ${x + radius} ${y} 
+                     L ${x + width - radius} ${y} 
+                     Q ${x + width} ${y} ${x + width} ${y + radius} 
+                     L ${x + width} ${y + height} Z`;
+        
+        return (
+            <path
+                d={path}
+                fill={fill}
+            />
+        );
+    };
+
+    const renderLegend = () => {
+        return (
+            <div className="w-full px-2 text-black pb-4">
+                <div className="flex items-center justify-center flex-wrap gap-4">
+                    {data.map((item, index) => (
+                        <div key={`legend-item-${item.name}`} className="flex items-center">
+                            <span
+                                className="inline-block mr-2 rounded-sm"
+                                style={{ backgroundColor: COLORS[index % COLORS.length], width: 8, height: 8 }}
+                            />
+                            <span className="text-xs text-muted-foreground font-normal">{item.name}</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        );
+    };
+
+    return (
+        <div className="w-full">
+            <ResponsiveContainer width="100%" height={350}>
+                <BarChart 
+                    data={data} 
+                    height={350}
+                    margin={{ top: 20, right: 0, left: 0, bottom: isMobile ? 0 : 20 }}
+                    barCategoryGap={2}
+                    barGap={0}
+                >
+                <XAxis 
+                    dataKey="name" 
+                    axisLine={false}
+                    tickLine={false}
+                    tick={ isMobile ? false : { fontSize: 12, dy: 10, dx: 50}}
+                    domain={['dataMin', 'dataMax']}
+                    type="category"
+                    scale="band"
+                    padding={{ left: 0, right: 0 }}
+                    interval={0}
+                />
+                <YAxis 
+                    dataKey="value" 
+                    axisLine={false}
+                    tickLine={false}
+                    tick={{ fontSize: 0, fill: 'transparent' }}
+                    domain={[0, (dataMax) => Math.ceil(dataMax * 1.1)]}
+                    tickCount={5}
+                    width={0}
+                />
+                <CartesianGrid stroke="#e5e7eb" horizontal={true} vertical={false} />
+                <Bar 
+                    dataKey="value" 
+                    fill="#8884d8" 
+                    shape={renderCustomBar}
+                    maxBarSize={105}
+                >
+                    {data.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                    ))}
+                    <LabelList 
+                        dataKey="value" 
+                        position="top" 
+                        dy={-4}
+                        formatter={(value) => (numberFormatter(value) + (percentage ? '%' : ''))}
+                        style={{ fontSize: 12, fill: '#374151' }}
+                    />
+                </Bar>
+            </BarChart>
+        </ResponsiveContainer>
+        {isMobile && renderLegend()}
+        </div>
+    );
+}
