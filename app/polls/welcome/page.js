0780484d957f6a59e74b9ad96d634cc920477dbc
Stephen <stephen.tanguis@gmail.com>
2025-09-26 12:13:45 -0500

    Update polls route and cleanup markup
    Update polls route and cleanup markup

--- app/polls/welcome/page.js
+++ app/polls/welcome/page.js
@@ -1,12 +1,11 @@
 import pageMetaData from 'helpers/metadataHelper'
-
-import ServePage from './components/ServePage'
+import PollWelcomePage from './components/PollWelcomePage'
 import candidateAccess from 'app/(candidate)/dashboard/shared/candidateAccess'
 
 const meta = pageMetaData({
   title: 'Welcome to GoodParty.org Serve',
   description: 'Welcome to GoodParty.org Serve',
-  slug: '/serve',
+  slug: '/polls',
 })
 
 export const metadata = meta
@@ -15,5 +14,5 @@ export default async function Page({ searchParams }) {
   await candidateAccess()
   const childProps = {}
 
-  return <ServePage {...childProps} />
+  return <PollWelcomePage {...childProps} />
 }
