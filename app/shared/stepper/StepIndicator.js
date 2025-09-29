d78b7f1356434dc74f31021b0b46c6a2b916765b
Stephen <stephen.tanguis@gmail.com>
2025-09-29 11:53:58 -0500

    fix stepper onclick
    fix stepper onclick to handle null

--- app/shared/stepper/StepIndicator.js
+++ app/shared/stepper/StepIndicator.js
@@ -1,11 +1,11 @@
-export const StepIndicator = ({ numberOfSteps, currentStep, onClick }) => {
+export const StepIndicator = ({ numberOfSteps, currentStep, onClick = null }) => {
   const steps = Array.from({ length: numberOfSteps }, (_, index) => index + 1)
   return (
     <div className="flex items-center w-full gap-2 px-1">
       {steps.map((step) => (
         <div
           key={step}
-          onClick={() => onClick(step)}
+          onClick={() => onClick && onClick(step)}
           className={`rounded-full h-2 min-h-[5px] flex-1 ${step === currentStep ? 'bg-blue-500' : 'bg-slate-300 hover:bg-slate-400 cursor-pointer '}`}
         />
       ))}
