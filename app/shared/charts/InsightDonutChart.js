0cbb3a867bc418a3931aa191b4a5265f2f248f78
Stephen <stephen.tanguis@gmail.com>
2025-09-29 09:27:34 -0500

    Add null data handling to charts
    Add null data handling to charts

--- app/shared/charts/InsightDonutChart.js
+++ app/shared/charts/InsightDonutChart.js
@@ -3,7 +3,7 @@ import { PieChart, Pie, Legend, ResponsiveContainer, Cell } from 'recharts'
 import { numberFormatter } from 'helpers/numberHelper'
 import { COLORS } from './constants'
 
-export const InsightDonutChart = ({ data, percentage }) => {
+export const InsightDonutChart = ({ data = [], percentage = false }) => {
 
     const renderLegend = () => {
         return (
