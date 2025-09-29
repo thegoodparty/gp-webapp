046cfd4d05cc8637b50b8a0c19a41e543f223636
Stephen <stephen.tanguis@gmail.com>
2025-09-29 08:47:51 -0500

    fix responsiveness

--- app/polls/onboarding/components/OnboardingPage.js
+++ app/polls/onboarding/components/OnboardingPage.js
@@ -102,7 +102,7 @@ export default function OnboardingPage({ pathname }) {
     <div className="fixed inset-x-0 bottom-0 top-14 flex flex-col bg-muted">
      
      <main className="flex-1 overflow-y-auto">
-        <section className="max-w-screen-md mx-auto bg-white md:border md:border-slate-200 md:rounded-xl md:mt-12 xs:mt-4 md:mb-16">
+        <section className="max-w-screen-md mx-auto bg-white md:border md:border-slate-200 md:rounded-xl md:mt-12 xs:pt-4 md:mb-16">
           <div className="p-4 sm:p-8 lg:p-16 lg:pb-4">
             {currentStep.name === 'Insights' && (
               <InsightsStep />
@@ -126,7 +126,7 @@ export default function OnboardingPage({ pathname }) {
            </div>
         </section>
       </main>
-      <div className="block md:hidden w-full px-4 sm:px-8">
+      <div className="block md:hidden bg-white w-full px-4 sm:px-8">
         {currentStep.showFooter && (
           <StepFooter numberOfSteps={5} currentStep={currentStep.stepIndex} onBack={currentStep.allowBack ? handleBack : null} onBackText={currentStep.backLabel} disabledNext={isNextDisabled} onNext={handleNext} onNextText={currentStep.nextLabel} />
         )}
