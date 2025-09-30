2fd3040e4f40d304d399e8b01aed2ec804bcae45
Matthew Marcus <lurchbulldog@gmail.com>
2025-09-30 16:31:37 -0500

    [ENG-4063] - Adjusts feature flag to allow P2P flow access for campaigns that are in "approved" compliance status (#993)

--- app/(candidate)/dashboard/components/tasks/flows/hooks/P2pUxEnabledProvider.js
+++ app/(candidate)/dashboard/components/tasks/flows/hooks/P2pUxEnabledProvider.js
@@ -5,29 +5,32 @@ import {
   isP2pUxEnabled,
   p2pCutoffDatetime,
 } from 'app/(candidate)/dashboard/components/tasks/flows/util/isP2pUxEnabled.util'
+import { useTcrComplianceCheck } from 'app/polls/onboarding/hooks/useTcrComplianceCheck'
 
 export const P2pUxEnabledContext = createContext({
   p2pCutoffDatetime: null,
   proUpdatedAtDate: null,
   p2pUxEnabled: false,
+  tcrCompliant: false,
   resetP2pUxEnabled: () => {},
 })
 export const P2pUxEnabledProvider = ({ children }) => {
   const [campaign] = useCampaign()
+  const [tcrCompliant] = useTcrComplianceCheck()
   const { details: campaignDetails } = campaign || {}
   const { isProUpdatedAt } = campaignDetails || {}
   const proUpdatedAtDate = new Date(isProUpdatedAt)
   const [p2pUxEnabled, setP2pUxEnabled] = useState(
-    isP2pUxEnabled(proUpdatedAtDate),
+    isP2pUxEnabled(proUpdatedAtDate, tcrCompliant),
   )
 
   useEffect(() => {
-    setP2pUxEnabled(isP2pUxEnabled(proUpdatedAtDate))
-  }, [proUpdatedAtDate])
+    setP2pUxEnabled(isP2pUxEnabled(proUpdatedAtDate, tcrCompliant))
+  }, [proUpdatedAtDate, tcrCompliant])
 
   const resetP2pUxEnabled = useMemo(
-    () => () => setP2pUxEnabled(isP2pUxEnabled(proUpdatedAtDate)),
-    [proUpdatedAtDate],
+    () => () => setP2pUxEnabled(isP2pUxEnabled(proUpdatedAtDate, tcrCompliant)),
+    [proUpdatedAtDate, tcrCompliant],
   )
 
   return (
@@ -37,6 +40,7 @@ export const P2pUxEnabledProvider = ({ children }) => {
         p2pCutoffDatetime,
         proUpdatedAtDate,
         p2pUxEnabled,
+        tcrCompliant,
       }}
     >
       {children}
--- app/(candidate)/dashboard/components/tasks/flows/util/isP2pUxEnabled.util.js
+++ app/(candidate)/dashboard/components/tasks/flows/util/isP2pUxEnabled.util.js
@@ -8,7 +8,8 @@ if (NEXT_PUBLIC_P2P_CUTOFF_DATETIME && isInvalidDateObject(p2pCutoffDatetime)) {
   throw new Error('NEXT_PUBLIC_P2P_CUTOFF_DATETIME is not a valid date')
 }
 
-export const isP2pUxEnabled = (proUpdatedAtDate) =>
+export const isP2pUxEnabled = (proUpdatedAtDate, tcrCompliant) =>
+  tcrCompliant ||
   !NEXT_PUBLIC_P2P_CUTOFF_DATETIME ||
   (!isInvalidDateObject(proUpdatedAtDate) &&
     isAfter(proUpdatedAtDate, p2pCutoffDatetime))
