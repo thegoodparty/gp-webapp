2752ea3678837be8ddaf5c0a54217cb6c95f16d5
Stephen <stephen.tanguis@gmail.com>
2025-10-09 09:59:14 -0500

    Use user firstName and lastName instead of name on polls onboarding
    Use user firstName and lastName instead of name on polls onboarding

--- app/polls/hooks/useOnboarding.js
+++ app/polls/hooks/useOnboarding.js
@@ -27,7 +27,7 @@ export const useOnboarding = () => {
     [campaign]
   )
   
-  const userName = useMemo(() => user?.name, [user])
+  const userName = useMemo(() => user?.firstName + ' ' + user?.lastName, [user])
   
   // Demo message text is used for the preview step and strategy step (It includes a constituency name for demo purposes)
   const demoMessageText = useMemo(() => 
