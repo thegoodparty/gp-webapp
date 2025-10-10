cb61b8d8ef17af03b27851d0850eeb299f6f0f79
Tomer Almog <tomer@goodparty.org>
2025-10-10 16:00:25 -0400

    chore: more cleanup

--- app/(candidate)/dashboard/polls/[id]/components/ConfidenceAlert.js
+++ app/(candidate)/dashboard/polls/[id]/components/ConfidenceAlert.js
@@ -4,10 +4,28 @@ import { BsExclamationCircle } from 'react-icons/bs'
 import { usePoll } from '../hooks/PollProvider'
 import { Alert, AlertTitle } from 'goodparty-styleguide'
 import Body2 from '@shared/typography/Body2'
+
 export default function ConfidenceAlert() {
   const [poll] = usePoll()
   const { lowConfidence } = poll || {}
-  if (!lowConfidence) {
+  if (lowConfidence) {
+    return (
+      <Alert variant="destructive">
+        <AlertTitle>
+          <div className="flex gap-2 text-red-500">
+            <BsExclamationCircle className="mt-0.5" />
+            <div>
+              <div className="font-medium">Poll Confidence: Low</div>
+              <Body2>
+                The results of your poll are not conclusive. We recommend you
+                run the same poll to a larger sample of residents.
+              </Body2>
+            </div>
+          </div>
+        </AlertTitle>
+      </Alert>
+    )
+  } else {
     return (
       <Alert variant="success">
         <AlertTitle>
@@ -25,20 +43,4 @@ export default function ConfidenceAlert() {
       </Alert>
     )
   }
-  return (
-    <Alert variant="destructive">
-      <AlertTitle>
-        <div className="flex gap-2 text-red-500">
-          <BsExclamationCircle className="mt-0.5" />
-          <div>
-            <div className="font-medium">Poll Confidence: Low</div>
-            <Body2>
-              The results of your poll are not conclusive. We recommend you run
-              the same poll to a larger sample of residents.
-            </Body2>
-          </div>
-        </div>
-      </AlertTitle>
-    </Alert>
-  )
 }
