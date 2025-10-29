7539ee4df7a20f0d47f7f517ea5ff30edfc15f02
Stephen <stephen.tanguis@gmail.com>
2025-10-29 17:56:57 -0500

    remove snap
    remove snap

--- app/(candidate)/dashboard/election-result/layout.js
+++ app/(candidate)/dashboard/election-result/layout.js
@@ -1,9 +1,6 @@
-import UserSnapScript from '@shared/scripts/UserSnapScript'
-
 export default function ElectionResultLayout({ children }) {
   return (
     <div className="min-h-screen bg-white md:bg-muted">
-      <UserSnapScript />
       <main className="flex-1">{children}</main>
     </div>
   )
