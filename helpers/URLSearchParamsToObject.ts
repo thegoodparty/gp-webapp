7d74d7b64d1cff2005246e50d4a304ae7cdbb525
Matthew Marcus <lurchbulldog@gmail.com>
2025-11-11 17:13:16 -0600

    First pass at converting some "leaf" files in the codebase from JS -> TS (#1132)
    * First pass at converting some "leaf" files in the codebase from JS -> TS
    
    * Fixes production build, I hope 🤞
    
    * Remove duplicate routes file and refactor user-related types into a new shared types file
    
    * Removing unused code - https://github.com/thegoodparty/gp-webapp/pull/1132/files#r2516069308
    
    ---------
    
    Co-authored-by: daniel <5633757+dealvz@users.noreply.github.com>

---
+++ helpers/URLSearchParamsToObject.ts
@@ -0,0 +1,12 @@
+export const URLSearchParamsToObject = (params: URLSearchParams): Record<string, string | string[]> => {
+  const obj: Record<string, string | string[]> = {}
+  for (const [key, value] of params) {
+    obj[key] = Object.hasOwn(obj, key)
+      ? Array.isArray(obj[key])
+        ? [...(obj[key] as string[]), value]
+        : [obj[key] as string, value]
+      : value
+  }
+  return obj
+}
+
--- helpers/videoHelper.js
+++
@@ -1,9 +0,0 @@
-export function youtubeParser(url) {
-  if (!url || typeof url !== 'string') {
-    return false;
-  }
-  var regExp =
-    /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
-  var match = url.match(regExp);
-  return match && match[7].length == 11 ? match[7] : false;
-}
