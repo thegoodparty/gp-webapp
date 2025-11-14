1ecb3b64be8a87fff927d611c8d569ba081cebb2
Matthew Marcus <lurchbulldog@gmail.com>
2025-11-14 11:28:49 -0600

    [ENG-4387] - Started conversions on shared folder

--- app/shared/utils/appendParam.js
+++
@@ -1,8 +0,0 @@
-const appendParam = (router, searchParams, key, value) => {
-  const params = new URLSearchParams(searchParams.toString())
-  params.delete(key)
-  params.append(key, value)
-  router.push(`?${params.toString()}`)
-}
-
-export default appendParam
--- app/shared/utils/getReqPathname.js
+++
@@ -1,6 +0,0 @@
-import { headers } from 'next/headers'
-
-export const getReqPathname = async () => {
-  const headersList = await headers()
-  return headersList.get('x-pathname')
-}
