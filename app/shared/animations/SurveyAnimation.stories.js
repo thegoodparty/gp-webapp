e7974ad148c0d523866e4ffd6b8bfa123eb60f87
Matthew Marcus <lurchbulldog@gmail.com>
2025-11-25 13:41:48 -0600

    [ENG-4387] - Remove all Storybook resources (#1180)

--- app/shared/animations/SurveyAnimation.stories.js
+++
@@ -1,9 +0,0 @@
-import SurveyAnimation from './SurveyAnimation'
-export default {
-  title: 'Animations/SurveyAnimation',
-  component: SurveyAnimation,
-  tags: ['autodocs'],
-  args: {},
-}
-
-export const Default = {}
