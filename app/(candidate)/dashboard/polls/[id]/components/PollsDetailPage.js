115961036f9108afa7ab1a731eb5c5caa8208465
Stephen <stephtea@users.noreply.github.com>
2025-12-02 16:49:51 -0600

    [ENG-5874] usersnap poll survey (#1186)
    Add logic for usersnap poll survey
    https://github.com/thegoodparty/gp-webapp/pull/1186

--- app/(candidate)/dashboard/polls/[id]/components/PollsDetailPage.js
+++ app/(candidate)/dashboard/polls/[id]/components/PollsDetailPage.js
@@ -7,14 +7,31 @@ import PollsContent from './PollsContent'
 import { useEffect } from 'react'
 import { EVENTS, trackEvent } from 'helpers/analyticsHelper'
 import { usePoll } from '../../shared/hooks/PollProvider'
+import { POLL_STATUS } from '../../shared/constants'
+import { waitForUsersnap } from '@shared/scripts/UserSnapScript'
+
+const showSurvey = async () => {
+  try {
+    const api = await waitForUsersnap()
+    api.logEvent('serve-product-market-fit')
+  } catch (error) {
+    console.error('Failed to load Usersnap API', error)
+  }
+}
 
 export default function PollsDetailPage({ pathname }) {
   const [poll] = usePoll()
   const [campaign] = useCampaign()
 
+  const pollStatus = poll?.status
   useEffect(() => {
-    trackEvent(EVENTS.polls.resultsViewed, { status: poll?.status })
-  }, [])
+    if (pollStatus) {
+      trackEvent(EVENTS.polls.resultsViewed, { status: pollStatus })
+      if (pollStatus === POLL_STATUS.COMPLETED) {
+        showSurvey()
+      }
+    }
+  }, [pollStatus])
 
   return (
     <DashboardLayout pathname={pathname} campaign={campaign} showAlert={false}>
