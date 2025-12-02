6f83b99fef01b9ff73f852600529089e8ea08af4
Stephen <stephen.tanguis@gmail.com>
2025-12-02 12:31:05 -0600

    add error logic for e2e tests
    add error logic for e2e tests

--- app/shared/scripts/UserSnapScript.tsx
+++ app/shared/scripts/UserSnapScript.tsx
@@ -75,18 +75,36 @@ export default function UserSnapScript(): React.JSX.Element {
         })
       })
     }
+
+    return () => {
+      if (window.onUsersnapLoad) {
+        window.onUsersnapLoad = undefined
+      }
+    }
   }, [])
   return (
     <Script
       strategy="afterInteractive"
       type="text/javascript"
       id="usersnap"
+      onError={() => {
+        if (window.__usersnapResolve) {
+          window.__usersnapReady = undefined
+          window.__usersnapResolve = undefined
+        }
+      }}
       dangerouslySetInnerHTML={{
         __html: `
     // usersnap.com
     
     var script = document.createElement('script');
     script.defer = 1;
+    script.onerror = function() {
+      if (window.__usersnapResolve) {
+        window.__usersnapReady = undefined;
+        window.__usersnapResolve = undefined;
+      }
+    };
     script.src = 'https://widget.usersnap.com/global/load/ffda1fce-d2f7-4471-b118-050ae883436b?onload=onUsersnapLoad';
     document.getElementsByTagName('head')[0].appendChild(script);
     `,
