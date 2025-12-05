3d82c79845f1f93433beff59566e4621f711b173
Swain Molster <swain.molster@gmail.com>
2025-12-05 11:45:21 -0500

    ENG-6135 [part 4]: remove references to the 'expanding' status

--- app/(candidate)/dashboard/polls/shared/constants.ts
+++ app/(candidate)/dashboard/polls/shared/constants.ts
@@ -1,14 +1,12 @@
 export const POLL_STATUS = {
   COMPLETED: 'completed',
   IN_PROGRESS: 'in_progress',
-  EXPANDING: 'expanding',
   SCHEDULED: 'scheduled',
 }
 
 export const POLL_STATUS_LABELS = {
   [POLL_STATUS.COMPLETED]: 'Completed',
   [POLL_STATUS.IN_PROGRESS]: 'In Progress',
-  [POLL_STATUS.EXPANDING]: 'In Progress',
   [POLL_STATUS.SCHEDULED]: 'Scheduled',
 }
 
