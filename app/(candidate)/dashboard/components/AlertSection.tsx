bc7fcbd4204c45c0dba6875c13e72d7d0681de5b
Matthew Marcus <lurchbulldog@gmail.com>
2025-12-16 14:46:25 -0600

    [ENG-4387] - fixed type cast

--- app/(candidate)/dashboard/components/AlertSection.tsx
+++ app/(candidate)/dashboard/components/AlertSection.tsx
@@ -5,16 +5,6 @@ import { CompleteProSignUpAlert } from './CompleteProSignUpAlert'
 import { PendingProSubscriptionAlert } from './PendingProSignUpAlert'
 import { DemoAccountWarningAlert } from '../shared/DemoAccountWarningAlert'
 
-interface UserMetaData {
-  checkoutSessionId?: string
-  customerId?: string
-  demoPersona?: string
-}
-
-interface User {
-  metaData?: UserMetaData
-}
-
 interface CampaignDetails {
   subscriptionId?: string
 }
@@ -28,19 +18,19 @@ interface AlertSectionProps {
   campaign: Campaign
 }
 
-export default function AlertSection(props: AlertSectionProps): React.JSX.Element {
+export default function AlertSection(
+  props: AlertSectionProps,
+): React.JSX.Element {
   const [user] = useUser()
-  const { metaData: userMetaData } = (user as User) || {}
+  const { metaData: userMetaData } = user || {}
   const { checkoutSessionId, customerId, demoPersona } = userMetaData || {}
 
   const { campaign } = props
   const { isPro, details } = campaign
   const { subscriptionId } = details || {}
 
-  const startedProCheckout =
-    checkoutSessionId && !customerId && !subscriptionId
-  const subscriptionPending =
-    checkoutSessionId && customerId && !subscriptionId
+  const startedProCheckout = checkoutSessionId && !customerId && !subscriptionId
+  const subscriptionPending = checkoutSessionId && customerId && !subscriptionId
 
   const showCompleteProSignUpAlert = startedProCheckout
   const showSubscriptionPendingAlert = subscriptionPending
