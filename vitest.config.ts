1ac81ad7c225b0a541da8fb9448a31b5a733e6ab
Collin Park <collin@goodparty.org>
2026-02-05 16:41:23 -0700

    we already brought in vitest, that we had missed, handled it for bot so we dont have conflict

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,18 +1,12 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   test: {
     environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
     globals: true,
-    setupFiles: './vitest.setup.ts',
-  },
-  resolve: {
-    alias: {
-      '@shared': path.resolve(__dirname, './app/shared'),
-      helpers: path.resolve(__dirname, './helpers'),
-    },
   },
 })
