abb6af47395ba1045ef004f2d416816998a5f993
Swain Molster <swain.molster@gmail.com>
2026-02-10 18:14:43 -0500

    remove dead code

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,7 +1,1 @@
 import '@testing-library/jest-dom/vitest'
-import { router } from 'helpers/test-utils/router-mocking'
-import { vi } from 'vitest'
-
-vi.mock('next/navigation', () => ({
-  useRouter: vi.fn(() => router),
-}))
