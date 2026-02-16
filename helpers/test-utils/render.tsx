6d03b8ddd4dea12006a7248ce8f4b3a6e0618ccd
Swain Molster <swain.molster@gmail.com>
2026-02-16 09:29:18 -0800

    clear queryclient

--- helpers/test-utils/render.tsx
+++ helpers/test-utils/render.tsx
@@ -1,12 +1,17 @@
 import { queryClient } from '@shared/query-client'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { render as _render, RenderOptions } from '@testing-library/react'
+import { beforeEach } from 'vitest'
 
 const TestProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   )
 }
+
+beforeEach(() => {
+  queryClient.clear()
+})
 
 export const render = (
   ui: React.ReactNode,
