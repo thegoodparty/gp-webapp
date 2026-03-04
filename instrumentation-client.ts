ae4ad01393410c63ebee635b9d3ab06e3d450cd0
Swain Molster <swain.molster@gmail.com>
2026-03-04 18:32:02 -0500

    revert enabling logs

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -5,7 +5,6 @@ import { isProductRoute } from 'app/shared/utils/isProductRoute'
 Sentry.init({
   dsn: 'https://6a3734c28a66ca850443447cd6d1f691@o4510915540025344.ingest.us.sentry.io/4510915613884416',
   enabled: !process.env.NEXT_PUBLIC_API_BASE?.includes('localhost'),
-  enableLogs: true,
 
   tracesSampler: ({ parentSampled }) => {
     if (parentSampled !== undefined) return parentSampled
@@ -14,7 +13,6 @@ Sentry.init({
   },
 
   integrations: [
-    Sentry.consoleIntegration(),
     Sentry.replayIntegration({
       maskAllText: false,
       maskAllInputs: true,
