0427e1f46c3b6ca805df58cb3a1afa459fa9913b
Matthew Marcus <lurchbulldog@gmail.com>
2026-03-13 15:14:23 -0500

    [ENG-6923] - https://github.com/thegoodparty/gp-webapp/pull/1553#discussion_r2933295531 and https://github.com/thegoodparty/gp-webapp/pull/1553#discussion_r2933377602

--- gpApi/gpFetch.ts
+++ gpApi/gpFetch.ts
@@ -108,10 +108,7 @@ const fetchCall = async <T = Partial<Record<string, unknown>>>(
   }
   try {
     const isSuccessfulResponseStatus = res.status >= 200 && res.status <= 299
-    if (isSuccessfulResponseStatus) {
-      return res.json()
-    }
-    return res
+    return isSuccessfulResponseStatus ? await res.json() : false
   } catch (e) {
     console.error('error in fetchCall catch', e)
     return false
