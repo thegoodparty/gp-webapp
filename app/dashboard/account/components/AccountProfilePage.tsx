5287bd90fae9425651ba93ccb082f23d63155250
Tomer Almog <tomer@goodparty.org>
2026-03-18 13:01:42 -0500

    chore: css change

--- app/dashboard/account/components/AccountProfilePage.tsx
+++ app/dashboard/account/components/AccountProfilePage.tsx
@@ -5,7 +5,7 @@ import { UserProfile } from '@clerk/nextjs'
 export default function AccountProfilePage(): React.JSX.Element {
   return (
     <div className="bg-indigo-100 min-h-[calc(100vh-60px)]">
-      <div className="max-w-screen-md mx-auto px-4 py-4 xl:p-0 xl:pt-4">
+      <div className="max-w-md mx-auto px-4 py-4 xl:p-0 xl:pt-4">
         <UserProfile
           routing="hash"
           appearance={{
