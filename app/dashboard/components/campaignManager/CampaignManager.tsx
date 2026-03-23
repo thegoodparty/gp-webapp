89df7a9bbe529d7e31f65c436d1ac857abf64225
Tomer Almog <tomer@goodparty.org>
2026-03-23 14:09:33 -0400

    feat: wrap components in CampaignUpdateHistoryProvider

--- app/dashboard/components/campaignManager/CampaignManager.tsx
+++ app/dashboard/components/campaignManager/CampaignManager.tsx
@@ -6,6 +6,7 @@ import HeaderSection from './HeaderSection'
 import { useCampaign } from '@shared/hooks/useCampaign'
 import ProgressSection from './ProgressSection'
 import { VoterContactsProvider } from '@shared/hooks/VoterContactsProvider'
+import { CampaignUpdateHistoryProvider } from '@shared/hooks/CampaignUpdateHistoryProvider'
 
 export default function CampaignManager({ pathname }: { pathname: string }) {
   const [campaign] = useCampaign()
@@ -15,9 +16,11 @@ export default function CampaignManager({ pathname }: { pathname: string }) {
   return (
     <DashboardLayout pathname={pathname} campaign={campaign}>
       <VoterContactsProvider>
-        <HeaderSection />
-        <ProgressSection />
-        <LoadingState />
+        <CampaignUpdateHistoryProvider>
+          <HeaderSection />
+          <ProgressSection />
+          <LoadingState />
+        </CampaignUpdateHistoryProvider>
       </VoterContactsProvider>
     </DashboardLayout>
   )
