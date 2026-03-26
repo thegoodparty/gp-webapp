8ba36a4c902983ce1cfafa21fd289eca6ed29e9c
Swain Molster <swain.molster@gmail.com>
2026-03-26 15:59:15 -0400

    fix a couple organization bugs

--- app/shared/layouts/navigation/Nav.tsx
+++ app/shared/layouts/navigation/Nav.tsx
@@ -10,15 +10,16 @@ import { useFlagOn } from '@shared/experiments/FeatureFlagsProvider'
 const Nav = (): React.JSX.Element => {
   const pathname = usePathname()
   const isDashboardPath = pathname?.startsWith('/dashboard')
-  const { on: useNewNav } = useFlagOn('win-serve-split')
+  const { ready, on: useNewNav } = useFlagOn('win-serve-split')
   const dashboardNewNav = isDashboardPath && useNewNav
+  const hideWhileLoading = isDashboardPath && !ready
 
   return (
     <>
       <div
         id="top-nav"
         className={`fixed w-screen h-14 z-50${
-          dashboardNewNav ? ' hidden' : ''
+          dashboardNewNav || hideWhileLoading ? ' hidden' : ''
         }`}
       >
         <div className="relative bg-indigo-50 lg:block border-solid border-b border-zinc-200 px-5 lg:px-8 z-50 h-14">
@@ -38,7 +39,9 @@ const Nav = (): React.JSX.Element => {
       {!dashboardNewNav && <RightSideMobile />}
       <div
         id="top-nav-spacer"
-        className={`h-14 relative${dashboardNewNav ? ' hidden' : ''}`}
+        className={`h-14 relative${
+          dashboardNewNav || hideWhileLoading ? ' hidden' : ''
+        }`}
       >
         &nbsp;
       </div>
