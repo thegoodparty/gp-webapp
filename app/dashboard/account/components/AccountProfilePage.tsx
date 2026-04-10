087e44a06c86a8f9d80521ed6f76f65f9dcc1f0a
Matthew Marcus <lurchbulldog@gmail.com>
2026-04-10 16:50:15 -0500

    [ENG-6260][ENG-7265] - https://github.com/thegoodparty/gp-webapp/pull/1693#discussion_r3066863237 and https://github.com/thegoodparty/gp-webapp/pull/1693#discussion_r3066863240

--- app/dashboard/account/components/AccountProfilePage.tsx
+++ app/dashboard/account/components/AccountProfilePage.tsx
@@ -1,42 +1,29 @@
 'use client'
 
-import { useClerk, UserProfile } from '@clerk/nextjs'
+import { UserProfile } from '@clerk/nextjs'
 import DashboardLayout from 'app/dashboard/shared/DashboardLayout'
-import { useEffect } from 'react'
 
-export default function AccountProfilePage(): React.JSX.Element {
-  const clerk = useClerk()
-  let removeListener: () => void
-
-  useEffect(() => {
-    removeListener = clerk.addListener((event) => {
-      console.log('event', event)
-    })
-    return () => {
-      removeListener()
-    }
-  }, [])
-
-  return (
-    <DashboardLayout pathname="/dashboard/account">
-      <div className="-m-2 md:-m-4 h-full">
-        <UserProfile
-          {...{
-            routing: 'hash',
-            appearance: {
-              variables: {
-                borderRadius: 'none',
-                colorBackground: '#f5f5f5',
-              },
-              elements: {
-                rootBox: 'w-full! h-full! border-none!',
-                cardBox: 'w-full! h-full!border-none! shadow-none!',
-                navbar: 'hidden',
-              },
+const AccountProfilePage = (): React.JSX.Element => (
+  <DashboardLayout pathname="/dashboard/account">
+    <div className="-m-2 md:-m-4 h-full">
+      <UserProfile
+        {...{
+          routing: 'hash',
+          appearance: {
+            variables: {
+              borderRadius: 'none',
+              colorBackground: '#f5f5f5',
             },
-          }}
-        />
-      </div>
-    </DashboardLayout>
-  )
-}
+            elements: {
+              rootBox: 'w-full! h-full! border-none! max-w-full!',
+              cardBox: 'w-full! h-full! border-none! shadow-none! max-w-full!',
+              navbar: 'hidden',
+            },
+          },
+        }}
+      />
+    </div>
+  </DashboardLayout>
+)
+
+export default AccountProfilePage
