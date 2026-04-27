5828352807af542c513bd69c9f022fa231ce7e65
Tomer Almog <tomer@goodparty.org>
2026-04-26 21:35:45 -0400

    feat: TextingComplianceFeatureFlag

---
+++ app/dashboard/profile/texting-compliance-agentic/components/TextingComplianceAgentic.tsx
@@ -0,0 +1,8 @@
+import { Card } from '@styleguide'
+import { TextingComplianceProps } from 'app/dashboard/profile/texting-compliance/components/TextingCompliance'
+
+export default function TextingComplianceAgentic(
+  _props: TextingComplianceProps,
+): React.JSX.Element {
+  return <Card className="p-4 mt-4">TextingCompliance</Card>
+}
---
+++ app/dashboard/profile/texting-compliance-agentic/components/TextingComplianceFeatureFlag.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { useFlagOn } from '@shared/experiments/FeatureFlagsProvider'
+import TextingCompliance, {
+  TextingComplianceProps,
+} from 'app/dashboard/profile/texting-compliance/components/TextingCompliance'
+import TextingComplianceAgentic from './TextingComplianceAgentic'
+
+const TEXT_COMPLIANCE_FEATURE_FLAG_KEY = 'pro-upgrade1'
+
+export default function TextingComplianceFeatureFlag(
+  props: TextingComplianceProps,
+): React.JSX.Element {
+  const { ready, on: textComplianceFeatureFlagEnabled } = useFlagOn(
+    TEXT_COMPLIANCE_FEATURE_FLAG_KEY,
+  )
+
+  if (!ready || !textComplianceFeatureFlagEnabled) {
+    return <TextingCompliance {...props} />
+  }
+
+  return <TextingComplianceAgentic {...props} />
+}
